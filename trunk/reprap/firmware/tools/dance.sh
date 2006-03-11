25094db63abc49b574c19c1aae515894059a9de1
vik <vik@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2006-03-11 00:24:02 +0000

    Added -cooked mode to brevent port munting binary data.

--- trunk/reprap/firmware/tools/dance.sh
+++ trunk/reprap/firmware/tools/dance.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 echo "Makes the X Stepper dance back & forth ad infinitum"
+export pokeDev=/dev/ttyUSB0
 # Add your "poke" command here.
-export poke="./poke -d 2 -t /dev/ttyUSB0 -v "
+export poke="./poke -d 2 -t $pokeDev -v "
+# Ensure port is raw data friendly.
+stty -F $pokeDev -echo -cooked
 echo 3 0 0 | $poke
 while true; do
 	echo 5 200 30 1 | $poke
