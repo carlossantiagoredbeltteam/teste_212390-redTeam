30f602e92601952b80be44ea62ddf8f248d1a2d6
vik <vik@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2006-08-25 22:05:33 +0000

    Updated script so it doesn't permanently lock the serial port.

--- trunk/reprap/firmware/tools/check_loop.sh
+++ trunk/reprap/firmware/tools/check_loop.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 # Does lots of pokes, hopefully getting a response at some point.
-stty -F /dev/ttyUSB0 -echo -cooked
+export serialport=/dev/ttyUSB0
+echo Using $serialport - edit the script to change this.
+stty -F $serialport -echo -cooked
 while (true)
 do
-	echo 6|./poke -d 8 -t /dev/ttyUSB0 -v -w &
-	sleep 2
-	killall -KILL poke
+	echo 6|./poke -d 8 -t $serialport -v &
+	sleep 4
+	killall poke 2>/dev/null
+	sleep 1
+	killall poke 2>/dev/null
 done
