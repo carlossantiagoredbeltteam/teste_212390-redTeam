2f986c00ebe44521da3f1578655a124f20994977
adrian-bowyer <adrian-bowyer@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2007-03-01 22:27:39 +0000

    Bug in extruder code for UNIVERSAL_PCB fixed.

--- reprap/firmware/devices/extruder/extruder2.c
+++ reprap/firmware/devices/extruder/extruder2.c
@@ -54,7 +54,7 @@ volatile static byte heatCounter = 0;
 static byte temperatureLimit0 = 0;
 static byte temperatureLimit1 = 0;
 
-volatile static byte delay_counter;
+//volatile static byte delay_counter;
 static byte lastTemperature = 0;
 static byte lastTemperatureRef = 0;
 
@@ -114,11 +114,13 @@ void extruder_reverse()
 }
 void heater_off()
 {
-	PORTA3 = 0;
+	portaval &= BIN(11110111);
+	PORTA = portaval;
 }
 void heater_on()
 {
-	PORTA3 = 1;
+	portaval |= BIN(00001000);
+	PORTA = portaval;
 }
 
 #pragma save
@@ -128,24 +130,22 @@ void change_log()
   char changes, current;
   extrude_click = 0;
   material_click = 0;
-  current = PORTB;  // Store so it doesn't change half way through processing
-  PORTB = current;  // properly reset change ??
+  current = PORTB1;  // Store so it doesn't change half way through processing
   changes = lastPortB ^ current;
 
-  if (changes & 0x01) {
+  if (changes) {
     // Our opto-marker changed
-    if (current & 0x01) {
+    if (current) {
     	extrude_click = 1;
     }
   }
   lastPortB = current;
   
-  current = PORTA;  // Store so it doesn't change half way through processing
-  PORTA = current;  // properly reset change ??
+  current = PORTA5;  // Store so it doesn't change half way through processing
   changes = lastPortA ^ current;  
-  if (changes & 0x20) {
+  if (changes) {
     // Material detector changed
-    if (!PORTA5 && seekNotify != 255) {
+    if (!current && seekNotify != 255) {
 		material_click = 1;
     }
   }
@@ -158,7 +158,7 @@ void set_cooler(byte b)
     if (b)
       portaval |= BIN(00000001);
     else
-      portaval &= BIN(11111110);
+	  portaval &= BIN(11111110);
     PORTA = portaval;
 }
 #pragma restore
@@ -208,7 +208,7 @@ void change_log()
   }
   if (changes & 0x40) {
     // Material detector changed
-    if (!PORTB6 && seekNotify != 255) {
+    if (!PORTB6 && seekNotify != 255) { //***AB Shouldn't this be (!(current & 0x40) && seekNotify != 255)
 		material_click = 1;
     }
   }
@@ -248,39 +248,51 @@ void init2()
   lastTemperatureRef = 0;
   temperatureVRef = 3;
   portaval = 0;
+  PORTA = portaval;
   TMR1H = HEATER_PWM_PERIOD;
   TMR1L = 0;
 }
 
 #pragma save
 #pragma nooverlay
-void setSpeed(byte speed, byte direction)
+void pwmSet()
 {
-  if (speed == 0) {
-    extruder_stop();
-    // Also turn off PWM completely
-    CCP1CON = BIN(00000000);
-    PR2 = 255;
-    PORTB3 = 0;
-    CCPR1L = 0;
-    return;
-  } else {
-    if (direction == 0) {
-      // Set forward output enable
-      extruder_forward();
-    } else {
-      // Set reverse output enable
-      extruder_reverse();
-    }
-    // Turn on PWM if it wasn't already
     CCP1CON = BIN(00111100);
+    CCPR1L = seekSpeed;
+  	if (seekSpeed == 255)
+    	PR2 = 0;
+  	else
+    	PR2 = PWMPeriod;
+}
+#pragma restore
+
+//#pragma save
+//#pragma nooverlay
+//void pwmOff()
+//{
+//    CCP1CON = BIN(00000000);
+//    PR2 = 255;
+//    PORTB3 = 0;
+//    CCPR1L = 0;
+//}
+//#pragma restore
+
+#pragma save
+#pragma nooverlay
+void setSpeed(byte direction)
+{
+  if (seekSpeed == 0) 
+  {
+    extruder_stop();
+    return;
+  } else 
+  {
+    if (direction == 0) 
+      extruder_forward();
+    else 
+      extruder_reverse();
   }
-  CCPR1L = speed;
-  if (speed == 255)
-    PR2 = 0;
-  else
-    PR2 = PWMPeriod;
-    
+  pwmSet();    
   currentDirection = direction;
  _asm  /// @todo Remove when sdcc bug fixed
   BANKSEL _currentPosition
@@ -342,14 +354,8 @@ void motorTick()
 	currentPosition.ival++;
       
       if (seekSpeed != 0 && currentPosition.ival == seekPosition.ival) {
-	// Set speed to 0 and turn off motor
-	seekSpeed = 0;
+	// Turn off motor
 	extruder_stop();
-	// Also turn off PWM
-	CCP1CON = BIN(00000000);
-	PR2 = 255;
-	PORTB3 = 0;
-	CCPR1L = 0;
     }
   }
   if (material_click) {
@@ -365,20 +371,20 @@ void motorTick()
 }
 #pragma restore
 
+
 void delay_10us()
 {
 _asm
-  BANKSEL _delay_counter
-DELAY_10US_1:
-  CLRWDT
   NOP
   NOP
   NOP
   NOP
   NOP
   NOP
-  DECFSZ _delay_counter, F
-  GOTO DELAY_10US_1
+  NOP
+  NOP
+  NOP
+  NOP
 _endasm;
 }
 
@@ -389,7 +395,8 @@ void checkTemperature()
   // A7 is thermistor
   // A1 is comparator
 
-  byte val = 0;
+  // Remember what PORTA should be
+  byte val = portaval;
 
   // Assume capacitor is discharged from previous round or powerup
 
@@ -403,10 +410,15 @@ void checkTemperature()
   delay_10us();
 
   T0IF = 0;
-  // Set A6 to high, float others
-  TRISA = BIN(10000010) | PORTATRIS;
-  PORTA = portaval | BIN(01000000);
 
+  // Set A6 to high, float others
+  // Don't allow interrupts to mess with these
+  GIE = 0;   
+  TRISA = BIN(10000010) | PORTATRIS;
+  portaval = val | BIN(01000000);
+  PORTA = portaval;
+  GIE = 1;
+ 	
   // Wait for cap to reach vref
   TMR0 = 0;
   while (C2OUT)
@@ -418,9 +430,12 @@ void checkTemperature()
 
   // Discharge cap
   // Set A1 to low, float others
+  GIE = 0;
+  portaval = val;
   PORTA = portaval;
   TRISA = BIN(11000000) | PORTATRIS;
-
+  GIE = 1;
+  
   // Set vref to low
   VRCON = BIN(10100001); // should be 1010xxxx to not output value
   delay_10us();
@@ -436,9 +451,12 @@ void checkTemperature()
 
   T0IF = 0;
   // Set A7 to high, float others
-  PORTA = portaval | BIN(10000000);
+  GIE = 0;
+  portaval = val | BIN(10000000);
+  PORTA = portaval;
   TRISA = BIN(01000010) | PORTATRIS;
-
+  GIE = 1;
+  
   // Use 8 bit Timer0 to measure time
   // Wait for cap to reach vref
   TMR0 = 0;
@@ -451,8 +469,12 @@ void checkTemperature()
 
   // Discharge cap
   // Set A1 to low, float others
+  // Restore PORTA to its original value
+  GIE = 0;
+  portaval = val;
   PORTA = portaval;
   TRISA = BIN(11000000) | PORTATRIS;
+  GIE = 1;
 
   // Set vref to low
   VRCON = BIN(10100001); // should be 1010xxxx to not output value
@@ -482,14 +504,15 @@ void processCommand()
     endMessage();
     break;
 
+// Extrude speed takes precedence over fan speed
   case CMD_FORWARD:
-    // Forward speed
-    setSpeed(buffer[1], 0);
+    seekSpeed = buffer[1];
+    setSpeed(0);
     break;
 
   case CMD_REVERSE:
-    // Reverse speed
-    setSpeed(buffer[1], 1);
+    seekSpeed = buffer[1];
+    setSpeed(1);
     break;
 
   case CMD_SETPOS:
@@ -515,9 +538,9 @@ void processCommand()
     if (seekPosition.ival != currentPosition.ival) {
       seekSpeed = buffer[1];
       if (currentPosition.ival > seekPosition.ival)
-	setSpeed(seekSpeed, 1);
+	setSpeed(1);
       else
-	setSpeed(seekSpeed, 0);
+	setSpeed(0);
     }
 
     break;
@@ -525,7 +548,7 @@ void processCommand()
   case CMD_FREE:
     // Free motor.  There is no torque hold for a DC motor,
     // so all we do is switch off
-    setSpeed(0, 0);
+    extruder_stop();
     break;
 
   case CMD_NOTIFY:
@@ -561,6 +584,13 @@ void processCommand()
 
   case CMD_SETCOOLER:
   	set_cooler(buffer[1]);
+#ifdef UNIVERSAL_PCB
+	if(buffer[1] && !seekSpeed)
+	{
+		seekSpeed = buffer[1];
+    	pwmSet();
+    }
+#endif
     break;
 
 // "Hidden" low level commands
