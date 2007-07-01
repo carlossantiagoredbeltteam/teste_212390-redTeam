009e2d92ed961cd8e618a0139e1724076f9d20e7
hoekstar <hoekstar@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2007-07-01 23:34:34 +0000

    did a bunch of preparatory work to actually make it a real site

---
+++ trunk/reprap/web/hoekens-bom/template.php
@@ -0,0 +1,81 @@
+<?
+	function drawHeader($title)
+	{ 
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+	<head>
+		<title><?=$title?> - RepRap Bill of Materials</title>
+		<link rel="stylesheet" type="text/css" href="/css/style.css" />
+		<meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
+		<script>
+			if (parent.frames.length > 0)
+			{
+			    parent.location.href = self.document.location;
+			}
+		</script>
+	</head>
+	<body>
+		<ul id="nav">
+			<li><a href="/">Home</a></li>
+			<li><a href="http://forums.reprap.org/">Forums</a></li>
+			<li><a href="http://blog.reprap.org/">Blog</a></li>
+			<li><a href="http://builders.reprap.org/">Builders</a></li>
+			<li><a href="http://www.reprap.org/">RepRap.org</a></li>
+			<li><a href="http://parts.rrrf.org/">Store</a></li>
+			<li><a href="http://reprap.org/bin/view/Main/RepRapOneDarwin">HOWTO Build It</a></li>
+		</ul>
+		<div id="main">
+			<a name="top" class="nodisplay"></a>
+			<div id="header">
+				<h1 class="shad"><em>RepRap BOM Generator</em></h1>
+				<h1><em>RepRap BOM Generator</em></h1>
+				<div class="gear"> </div>
+			</div>
+
+			<div id="body">
+				<div id="content">
+					<h2><?=$title?></h2>
+<?
+	}
+
+	function drawSideBar()
+	{
+?>
+				<div id="sidebar">
+					<div style="clear: both;"> </div>
+				</div>
+<?
+	}
+
+	function drawFooter()
+	{
+?>
+				</div>
+				<?//drawSideBar();?>
+			</div>
+			<div id="footer">
+				Copyright &copy; 2006-<?=date('Y')?> <a href="http://www.reprap.org">RepRap.org</a>.
+			</div>
+		</div>
+
+		<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+		</script>
+		<script type="text/javascript">
+			_uacct = "UA-1381371-7";
+			urchinTracker();
+		</script>
+		
+		<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+		</script>
+		<script type="text/javascript">
+			_uacct = "UA-1381371-8";
+			urchinTracker();
+		</script>
+		<script src="http://stats.reprap.org/?js" type="text/javascript"></script>
+	</body>
+</html>
+<?
+	}
+}
+?>
