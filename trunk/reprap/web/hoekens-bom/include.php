a8419b78e4f18d8d201cea2e317456b2c252fca4
hoekstar <hoekstar@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2007-07-01 23:24:56 +0000

    added some minor changes

--- trunk/reprap/web/hoekens-bom/include.php
+++ trunk/reprap/web/hoekens-bom/include.php
@@ -97,7 +97,7 @@
 		
 		public function __construct($name, $type, $quantity = 1, $suppliers = array())
 		{
-			$this->name = $name;
+			$this->name = trim($name);
 			$this->type = $type;
 			$this->quantity = $quantity;
 			$this->addSuppliers($suppliers);
@@ -154,7 +154,7 @@
 			$key = $this->hasPart($part);
 			
 			//if we found it, simply add more quantity in.
-			if ($key)
+			if ($key !== false)
 				$this->parts[$part->type][$key]->quantity += $part->quantity;
 			//otherwise, just add our part itself.
 			else
@@ -206,6 +206,12 @@
 							break;
 
 						case 'belt':
+						
+							if (!$beltSuppliers)
+								$beltSuppliers = $part->suppliers;
+							else
+								$part->suppliers = $beltSuppliers;
+								
 							preg_match("/\d+/", $part->name, $matches); // match the length
 							$part->name = "{$matches[0]}mm belt (Length 950 pitch 2.5 width 6 thick ~1.3)";
 							$part->quantity *= $subqty;
@@ -213,19 +219,23 @@
 							
 						case 'rod':
 						case 'stud':
-						
+								
 							if (preg_match("/M(\d+)\D*(\d+)/", $part->name, $matches))
-								$part->name = "M{$matches[1]} x {$matches[2]}mm $part->type";
+							{
+								$length = $matches[2];
+								$part->name = "M{$matches[1]} x $length $part->type";
+							}
 							else
 							{
 								preg_match("/(\d+)/", $part->name, $matches); // match the length
-						
+								$length = $matches[1];
+								
 								if ($part->type == 'rod')
-									$part->name = "M8 x {$matches[1]}mm $part->type (fixme in BOM!)";
+									$part->name = "M8 x $length $part->type";
 								else if ($part->type == 'stud')
-									$part->name = "M5 x {$matches[1]}mm $part->type (fixme in BOM!)";
+									$part->name = "M5 x $length $part->type";
 							}
-							
+						
 							$part->quantity *= $subqty;
 							break;
 
@@ -233,7 +243,7 @@
 							if (preg_match("/(\d+)\D*(\d+)AWG/", $part->name, $matches))
 								$part->name = "{$matches[1]}mm x {$matches[2]}AWG wire";
 							else if (preg_match("/(\d+)/", $part->name, $matches))
-								$part->name = "{$matches[1]}mm wire (specify AWG in BOM!)";
+								$part->name = "{$matches[1]}mm wire";
 					
 							$part->quantity *= $subqty;
 							break;
@@ -265,7 +275,23 @@
 		
 		public function addSupplier($part)
 		{
-			$this->suppliers[$part->key][] = $part;
+			if ($part->key && $part->part_id)
+			{
+				if (count($this->suppliers[$part->key]))
+				{
+					foreach ($this->suppliers[$part->key] AS $dongle)
+					{
+						if ($dongle->part_id == $part->part_id)
+						{
+							$found = true;
+							$dongle->quantity += $part->quantity;	
+						}
+					}
+				}
+
+			}
+			if (!$found)
+				$this->suppliers[$part->key][] = $part;
 		}
 		
 		public function getParts($key = null)
