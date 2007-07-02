3c05f8bb9f950e0f09153ab145a2baa22e7be8a1
hoekstar <hoekstar@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2007-07-02 01:43:24 +0000

    added the global supplier choice option!

--- trunk/reprap/web/hoekens-bom/uniqueparts.php
+++ trunk/reprap/web/hoekens-bom/uniqueparts.php
@@ -38,16 +38,50 @@
 	function renderUniqueHTML($bom)
 	{
 		drawHeader("Choose Suppliers");
+
+		$types = $bom->getTypes();
+		$suppliers = $bom->getUniqueSuppliers();
 ?>
+	<script>
+		function selectGlobalSupplier(ele)
+		{
+			var supplier_key = ele.value;
+			
+			if (supplier_key)
+			{
+				var selects = Element.getElementsByClassName($('bom_form'), 'supplier_select');
+				
+				for (var i=0; i<selects.length; i++)
+				{
+					var select = selects[i];
+					
+					var options = Element.immediateDescendants(select);
+					
+					for (var j=0; j<options.length; j++)
+					{
+						var option = options[j];
+						if (option.innerHTML == supplier_key)
+							option.selected = true;
+					}
+				}
+			}
+		}
+	</script>
 	<ul>
 		<li>Step 1: Choose which assemblies and the quantities of each.</li>
 		<li><b>Step 2: Choose which suppliers you'd like to use.</b></li>
 		<li>Step 3: Order your parts!  Super easy.</li>
 	</ul>
-	<form action="billofmaterials.php" method="post">
+	
+	<h3>Set a global supplier (it will choose this supplier if available below.)</h3>
+	<select name="global_supplier" onchange="selectGlobalSupplier(this)">
+		<option value="">Choose Supplier</option>
+		<? foreach ($suppliers AS $corp): ?>
+			<option value="<?=$corp->name?>"><?=$corp->name?> (<?=implode(", ", $corp->countries)?>)</option>
+		<? endforeach ?>
+	</select>
+	<form action="billofmaterials.php" method="post" id="bom_form">
 <?
-		$types = $bom->getTypes();
-
 		foreach ($types AS $type)
 		{
 			$parts = $bom->getParts($type);
@@ -115,7 +149,7 @@
 
 		if (count($part->suppliers))
 		{
-			echo '<select name="suppliers[]">';
+			echo '<select class="supplier_select" name="suppliers[]">';
 			foreach ($part->suppliers AS $supplier)
 				echo '<option value="' . $supplier->name . '">' . $corps[$supplier->key]->name . '</option>';
 			echo '</select>';
