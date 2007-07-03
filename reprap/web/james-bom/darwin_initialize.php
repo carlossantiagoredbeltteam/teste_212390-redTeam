d6faaac7de05ca05980b41316867e27d1cb61a5e
jvasile <jvasile@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2007-07-03 02:35:23 +0000

    Updated parser to match Hoeken's recent edits to the spreadsheets. Cleaned up a bunch of the code, including splitting some into module.php and part.php. Finished the row hide/show buttons. Added a supplier view. Combined some columns. Separted code from the html. Switched to MDB2 because Pear's DB.php isn't GPL compatible.

--- reprap/web/james-bom/darwin_initialize.php
+++ reprap/web/james-bom/darwin_initialize.php
@@ -3,43 +3,19 @@
 // create db
 // db must be defined before this file is included.
 $db->queries("
-INSERT INTO model (module_id) VALUES (1); /* identify master module */
-
-INSERT INTO tag (name, description) VALUES ('', 'N/A');
-
-INSERT INTO source (name, url, description) VALUES ('RepRap', 'http://reprap.org', 'These are parts that will be fabricated on an already-working RepRap.');
-INSERT INTO source (name, url, description) VALUES ('RepRap Research Foundation', 'http://rrrf.org', 'The Reprap Research Foundation\'s online store');
-INSERT INTO source (name, description) VALUES ('No Source', 'Some parts you\'ll just have to find on your own.');
-INSERT INTO source (name, description, notes) VALUES ('Module', 'This part is a module or assembly.', 'See documentation for building details.');
-INSERT INTO source (name, description) VALUES ('Mold', 'You can to make this part yourself from Friendly Plastic.  See URL for details');
-INSERT INTO source (name, url, notes) VALUES ('RS Electronics', 'http://rselectronics', 'This is not Radio Shack.  It\'s a completely different company.');
-INSERT INTO source (name, url) VALUES ('Farnell Electronics', 'http://farnell.com');
-INSERT INTO source (name, url) VALUES ('Jameco Electronics', 'http://jameco.com');
-INSERT INTO source (name, url) VALUES ('McMaster-Carr', 'http://mcmaster.com');
-INSERT INTO source (name, url) VALUES ('Mouser Electronics', 'http://mouser.com');
-INSERT INTO source (name, url) VALUES ('Radio Shack', 'http://radioshack.com');
-INSERT INTO source (name, url) VALUES ('Greenweld Hobbies', 'http://www.greenweld.co.uk/');
-
-INSERT into module (name) VALUES ('Darwin'); /* master module */
-INSERT into module (name) VALUES ('Cartesian Robot');
-INSERT into module (name) VALUES ('Thermoplast Extruder');
-INSERT into module (name) VALUES ('Material Extruder');
-INSERT into module (name) VALUES ('PowerComms Board');
-INSERT into module (name) VALUES ('Stepper Controller Board');
-INSERT into module (name) VALUES ('Extruder Controller Board');
-INSERT into module (name) VALUES ('Opto Endstop Board');
-INSERT into module (name) VALUES ('Stepper Tester Board');
-INSERT into module (name) VALUES ('Miscellaneous Kit');
-
-INSERT into module_module (supermodule_id, submodule_id, quantity) VALUES (1,2,1);
-INSERT into module_module (supermodule_id, submodule_id, quantity) VALUES (1,3,1);
-INSERT into module_module (supermodule_id, submodule_id, quantity) VALUES (1,4,1);
-INSERT into module_module (supermodule_id, submodule_id, quantity) VALUES (1,5,1);
-INSERT into module_module (supermodule_id, submodule_id, quantity) VALUES (1,6,3);
-INSERT into module_module (supermodule_id, submodule_id, quantity) VALUES (1,7,2);
-INSERT into module_module (supermodule_id, submodule_id, quantity) VALUES (1,8,3);
-INSERT into module_module (supermodule_id, submodule_id, quantity) VALUES (1,9,1);
-INSERT into module_module (supermodule_id, submodule_id, quantity) VALUES (1,10,1);
+INSERT INTO source (name, url, abbreviation, description) VALUES ('RepRap', 'http://reprap.org', 'RR', 'These are parts that will be fabricated on an already-working RepRap.');
+INSERT INTO source (name, url, abbreviation, description) VALUES ('RepRap Research Foundation', 'http://rrrf.org', 'RRRF', 'The Reprap Research Foundation\'s online store');
+INSERT INTO source (name, abbreviation, description, notes) VALUES ('Module', 'Module', 'This part is a module or assembly.', 'See documentation for building details.');
+INSERT INTO source (name, abbreviation, description) VALUES ('Mold', 'Mold', 'You can make this part yourself from Friendly Plastic.  See URL for details');
+INSERT INTO source (name, url, abbreviation, notes) VALUES ('RS Electronics', 'http://rselectronics', 'RS', 'This is not Radio Shack.  It\'s a completely different company.');
+INSERT INTO source (name, url, abbreviation) VALUES ('Farnell', 'http://farnell.com', 'F');
+INSERT INTO source (name, url, abbreviation) VALUES ('Jameco', 'http://jameco.com', 'J');
+INSERT INTO source (name, url, abbreviation) VALUES ('McMaster', 'http://mcmaster.com', 'MCM');
+INSERT INTO source (name, url, abbreviation) VALUES ('Mouser', 'http://mouser.com', 'M');
+INSERT INTO source (name, url, abbreviation) VALUES ('Radio Shack', 'http://radioshack.com', 'RShk');
+INSERT INTO source (name, url, abbreviation) VALUES ('Greenweld', 'http://www.greenweld.co.uk/', 'GH');
+INSERT INTO source (name, url, abbreviation) VALUES ('Sculpt.com', ' http://www.sculpt.com/', 'Sculpt');
+INSERT INTO source (name, abbreviation) VALUES ('Hardware Store', 'H');
 
 
 ");
--- reprap/web/james-bom/parse.php
+++ reprap/web/james-bom/parse.php
@@ -36,31 +36,34 @@ Parsing BOM from Google spreadsheets...
 <?php include("csv.php"); ?>
 <?php
 
-$wget_path = chop(`which wget`); if (!$wget_path){ die ("Can't find wget executable."); }
-$cat_path = chop(`which cat`); if (!$cat_path) { die ("Can't find cat executable."); }
+//$wget_path = chop(`which wget`); if (!$wget_path){ die ("Can't find wget executable."); }
+//$cat_path = chop(`which cat`); if (!$cat_path) { die ("Can't find cat executable."); }
 
 function get_bom_data() {
+
   // Grabs list of Bills of Materials from google spreadsheets, then
   // gets each spreadsheet as csv
 
-  $master_bom = get_csv(8); // retrieve master BOM
-  //  echo "$master_bom";
-  $pages = explode("\n", $master_bom);
-  array_splice($pages, 5,0,array("Universal Controller,3,'Universal controller board on which the stepper and extruder controller boards are based',1"));
-  //TODO: compensate for faking the universal controller as module of darwin
+  echo "Getting BOM data from Google or cache.\n";
+
+  $pages = get_csv(8); // retrieve master BOM
+
+  //  $pages[count($pages)] = array(array("Universal Controller","3",'Universal controller board on which the stepper and extruder controller boards are based',"1"));
+  array_splice($pages, 8,0,array(array("Universal Controller","3",'Universal controller board on which the stepper and extruder controller boards are based',"1")));
 
   // Which field contains the id of the spreadsheet page we need?
-  $fields = parse_csv( $pages[1] );
+  //  $fields = parse_csv( $pages[1] );
+  $fields = $pages[1];
   $bom_field=-1;
   for ($i=0; $i < sizeof($fields); $i++) {
     if (!strcasecmp($fields[$i], "BOM Id")){$bom_field=$i;}
     if (!strcasecmp($fields[$i], "Module")){$module_field=$i;}
   }
   if ($bom_field==-1) { die("Couldn't find BOM_Id field in master bom spreadsheet."); }
-  
+
   // Get list of modules and their BOM Ids
   for ($p=2; $p < sizeof($pages);$p++) {
-    $data = parse_csv($pages[$p]);
+    $data = $pages[$p];
     $modules[ $data[$module_field] ] = $data[$bom_field];
   }
   
@@ -101,7 +104,7 @@ function discover_field_names ($fields) {
 }
 
 function parse_fields ($line, $field_str='', $module_name='') {
-  $fields = parse_csv($line);
+  $fields = $line;
   if (strcasecmp($field_str, join("|", $fields))) {
         echo ("$module_name fields have changed to:\n".join("|", $fields)."\n\nSkipping parsing\n");
         return NULL;
@@ -112,39 +115,39 @@ function parse_fields ($line, $field_str='', $module_name='') {
 function parse_supplier_columns($vals, $f, $part_id) {
   // returns -1 if there's nothing in the supplier columns
   // otherwise, returns 1
-
   global $db;
 
   $success = -1;
 
-  foreach ($f["source"] as $s) {  //iterate over source columns
-    if (preg_match("/^R\d+/", $vals[$s])) {
-      $db->query("INSERT into source_part (part_id, source_id) VALUES ($part_id, 6)"); // rs electronics
-      $success = 1;
-    } elseif (preg_match("/^RS:\d+/", $vals[$s])) {
-      $db->query("INSERT into source_part (part_id, source_id) VALUES ($part_id, 6)"); // rs electronics
-      $success = 1;
-    } elseif (preg_match("/^F\d+/", $vals[$s])) {
-      $db->query("INSERT into source_part (part_id, source_id) VALUES ($part_id, 7)"); // Farnell
-      $success = 1;
-    } elseif (preg_match("/http\:\/\/parts\.rrrf\.org/", $vals[$s])) {
-      $db->query("INSERT into source_part (part_id, source_id, url) VALUES ($part_id, 2, \"$vals[$s]\")"); // rrrf
-      $success = 1;
-    } elseif (($f["mouser"] == $s) and ($vals[$s] != NULL)) {
-      $db->query("INSERT into source_part (part_id, source_id, vendor_part_id) VALUES ($part_id, 10, \"$vals[$s]\")"); // mouser
-      $success = 1;
-    } elseif (preg_match("/greenweld\.co\.uk/", $vals[$s])) {
-      $db->query("INSERT into source_part (part_id, source_id, url) VALUES ($part_id, 12, \"$vals[$s]\")"); // greenweld
-      $success = 1;
-    } elseif (preg_match("/farnell.com/", $vals[$s])) {
-      $db->query("INSERT into source_part (part_id, source_id, url) VALUES ($part_id, 7, \"$vals[$s]\")"); // Farnell
-      $success = 1;
-    } elseif (!strcmp("-", $vals[$s])) { //ignore
-    } elseif ($vals[$s]) {
-      echo "Could not parse supply column $s: $vals[$s]\n";
+  $sid = $db->get_source_abbreviation_id_hash();
+
+  for ($s=$f["source"][0]; $s<count($vals); $s++) {  //iterate over source columns
+    if (preg_match('/:/', $vals[$s])) {
+      list($code, $id) = explode(':', $vals[$s], 2);
+      if (!strcasecmp($code, 'http')) {
+        if (preg_match("/sculpt\.com/", $vals[$s])) {
+          $db->query("INSERT into source_part (part_id, source_id, url) VALUES ($part_id, ".$sid['Sculpt'].", \"$vals[$s]\")"); // Farnell
+          $success = 1;
+        } else {
+          echo "Don't know how to parse url source: $vals[$s].\n";
+        }
+      } else {
+        $q = $db->query("SELECT id FROM source WHERE abbreviation=\"$code\"");
+        $row = $q->fetchRow();
+        if ($row['id']) {
+          $q = $db->query("SELECT source_id, part_id FROM source_part where source_id=".$row['id']." AND part_id=$part_id");
+          if (!$q->fetchRow()) { $db->query("INSERT into source_part (part_id, source_id, vendor_part_id) VALUES ($part_id, ".$row['id'].", \"$id\")"); }
+          $success = 1;
+        } else {
+          echo "Unknown source abbreviation: $code in $vals[$s]\n";
+        }
+      }
+    } else {
+      if ($val[$s]) {
+        echo "Malformed source: $vals[$s]\n";
+      }
     }
   }
-
   return $success;
 }
 
@@ -160,10 +163,11 @@ function set_module_part ($quantity, $part_id, $module_id=0, $description='', $s
 
 function parse_name_quantity_type_description ($vals, $f, $parent_module_id=0, $module_id=0) {
   global $db;
+
   if (preg_match("/assembly/i", $vals[$f["type"]])) {
     $module_id = $db->create_module($vals[$f["name"]], $parent_module_id, $vals[$f["quantity"]], $vals[$f["note"]]);
     $db->tag_module($module_id, $vals[$f["type"]]);
-    //echo "New module ($module_id): ".$vals[$f["name"]]."\n";
+    // echo "New module ($module_id): ".$vals[$f["name"]]."\n";
     return $module_id;
   }
 
@@ -183,162 +187,186 @@ function parse_name_quantity_type_description ($vals, $f, $parent_module_id=0, $
   set_module_part($vals[$f["quantity"]], $part_id, $module_id, $vals[$f["description"]], $vals[$f["schematic"]], $vals[$f["note"]]);
   
   // Source
-  if (!strcasecmp($vals[$f["type"]], 'rp')) {
-    $db->query("INSERT into source_part (part_id, source_id) VALUES ($part_id, 1)"); // reprap
-  } elseif (!strcasecmp($vals[$f["type"]], 'assembly')) {
-    $db->query("INSERT into source_part (part_id, source_id) VALUES ($part_id, 4)"); // module
-  } elseif (!strcasecmp($vals[$f["type"]], 'mold')) {
-    $db->query("INSERT into source_part (part_id, source_id) VALUES ($part_id, 5)"); // mold it yourself
-  } elseif (!strcasecmp($vals[$f["type"]], 'electronic')) {
-    $db->query("INSERT into source_part (part_id, source_id) VALUES ($part_id, 4)"); // electronic
-  } elseif (!strcasecmp($vals[$f["type"]], 'fast') || !strcasecmp($vals[$f["type"]], 'rod') || !strcasecmp($vals[$f["type"]], 'stud')) {
-    $db->query("INSERT into source_part (part_id, source_id) VALUES ($part_id, 9)"); // mcmaster carr
-  } elseif (parse_supplier_columns($vals, $f, $part_id) != -1) {
-  } else {
-    $db->query("INSERT into source_part (part_id, source_id) VALUES ($part_id, 3)"); // no source
-  }
+  parse_supplier_columns($vals, $f, $part_id);
 
   return $module_id;
 }
 
 
-function parse_spreadsheet($module_name, $lines, $field_str) {
+function parse_spreadsheet($module_name, $lines, $field_str, $parent_module_id=1) {
   global $db;
 
   $fields = parse_fields($lines[1], $field_str, $module_name);
-  if (!$fields) { return;}
+   if (!$fields) { return; }
 
   $f = discover_field_names($fields);
+  //  var_dump ($f); exit;
 
-  $parent_module_id = $db->get_module_id_by_name($module_name);
   $module_id = $parent_module_id;  // just to start out
 
   for ($i = 2; $i < count($lines); $i++) {
-    $vals = parse_csv($lines[$i]);
+    $vals = $lines[$i];
     if ($vals[$f["name"]] == NULL) { continue; }
     $module_id = parse_name_quantity_type_description ($vals, $f, $parent_module_id, $module_id);
   }
 }
 
+function dump_spreadsheet($sheet) {
+  foreach ($sheet as $row) {
+    if ($row) {
+      foreach ($row as $cell) {
+        echo "$cell\t";
+      }
+      echo "\n";
+    }
+  }
+}
+function dump_spreadsheets($sheets) {
+  foreach ($sheets as $sheet)  {
+    if ($sheet) {
+      dump_spreadsheet($sheet);
+    }
+  }
+}
+
 ////////////////////////////////////////////////////////////////////////
 
-$modules = get_bom_data();
-$db = new mydb();
+$suppliers = get_csv(12); //TODO parse suppliers
 
+$modules = get_bom_data(); // get the modules as an array of arrays of rows of cells
+//dump_spreadsheets(&$modules);
+
+$db = new mydb( array('no_use_db' => 1));
 // drop and recreate database to start clean
 $db->queries("DROP DATABASE if exists bom;".
-             "CREATE DATABASE ".$db->name.";".
-             "USE ".$db->name.";");
+             "CREATE DATABASE ".$db->db_name.";".
+             "USE ".$db->db_name.";");
 include ("tables.mysql");          // create tables
-include("darwin_initialize.php");  // load the darwin data into the fresh db
+$darwin_module_id = $db->create_model("Darwin");
+$darwin_module_id = $darwin_module_id['module_id'];
 
+include("darwin_initialize.php");  // load the darwin data into the fresh db
 
 // Parse the spreadsheets
 
-while (list($mod, $data) = each($modules)) {
-  // process csv
+while (list($mod, $lines) = each($modules)) {
+  switch ($mod) {
+  case "Cartesian Robot v1.0":  // parse Cartesian Robot
+    echo "Parsing Cartesion Robot\n";
 
-  $lines = explode("\n", $data);
-
-  if (!strcasecmp($mod, "Cartesian Robot")) {        // parse Cartesian Robot
-    // Change jig module to a bunch of parts tagged "jig".  They're not really a separate module
-    for ($l=0; $l < count($lines); $l++) {
-      if (preg_match('/jig/i', $lines[$l])) {
+    $module_id = $db->create_module("Cartesian Robot v1.0", $darwin_module_id);
+    
+    // Change jig module to a bunch of parts tagged "jig".  Jigs are not really a separate module
+    for ($l=2; $l < count($lines); $l++) {
+      if (preg_match('/jig/i', $lines[$l][1])) {
         $jig_line=$l;
         break;
       }
     }
     unset($lines[$jig_line]);
-    for ($l=$jig_line; !preg_match('/assembly/i', $lines[$l]) && $l < count($lines); $l++) {
-      $lines[$l] = preg_replace('/,(rod)/', ",$1\njig,", $lines[$l]);
-      $lines[$l] = preg_replace('/,(rp)/', ",$1\njig,", $lines[$l]);
+    for ($l=$jig_line; !preg_match('/assembly/i', $lines[$l][3]) && $l < count($lines); $l++) {
+      $lines[$l][3] .= "\njig";
+      if (!$lines[$l][4]) { $lines[$l][4] = "This jig is used for lining up and measuring other pieces during assembly."; }
     }
+    
+    parse_spreadsheet($mod, $lines, 'Part Id|Name|Quantity|Type|Description|Suppliers', $module_id);
+    break;
+  
+  case "Thermoplast Extruder v1.0":      // parse extruder
+    echo "Parsing extruder.\n";
+    $module_id = $db->create_module("Thermoplast Extruder v1.0", $darwin_module_id);
+    parse_spreadsheet($mod, $lines, 'Part Id|Name|Quantity|Type|Description|Suppliers', $module_id);
+    break;
 
-    parse_spreadsheet($mod, $lines, 'Assembly / Components|Qty|Type|Supplier (F=Farnell, R=RS)');
-  } elseif (!strcasecmp($mod, "Thermoplast Extruder")) {      // parse extruder
-    parse_spreadsheet($mod, $lines, 'Name|Qty|Type|Description|Part Id|Main Supplier|Alternate Supplier');
-  } elseif (!strcasecmp($mod, "Support Material Extruder")) {
-    // spreadsheet doesn't exist yet
-  } elseif (!strcasecmp($mod, "Universal Controller")) {
-    // disambiguate signal connector and connector housing parts
-    for ($l=0; $l < count($lines); $l++) {
-      if (preg_match('/signal connector/i', $lines[$l]) || preg_match('/connector housing/i', $lines[$l])) {
-        if (preg_match('/\d+ pin/', $lines[$l], $matches)) {
-          $lines[$l] = $matches[0].' '.$lines[$l];
-        }
-      }
-    }
-    
-    parse_spreadsheet($mod, $lines, 'Name|Quantity|Type|Description|Item|Mouser|RRRF');
-    
-    $db->query ("INSERT into module_module (supermodule_id, submodule_id, quantity) ".
-                "VALUES (".$db->get_module_id_by_name("Stepper Controller Board").", ".
-                $db->get_module_id_by_name("Universal Board Component Kit").", 1);");
+  case "Support Extruder Controller Board v1.2":
+    echo "Parsing Support Extruder Controller Board\n";
+    $module_id = $db->create_module("Support Extruder Controller Board v1.2", $darwin_module_id);
+    parse_spreadsheet($mod, $lines, 'Part Id|Name|Quantity|Type|Description|Suppliers', $module_id);
+    break;
+
+  case "Support Material Extruder v1.0":
+    echo "Parsing Support Material Extruder v1.0\n";
+    $module_id = $db->create_module("Support Material Extruder", $darwin_module_id);
+    parse_spreadsheet($mod, $lines, 'Part Id|Name|Quantity|Type|Description|Suppliers', $module_id);
+    break;
+
+  case "Universal Controller":
+    echo "Parsing Universal Controller\n";
+    $module_id = $db->create_module("Universal Controller",  $thermoplast_controller_id);
+    parse_spreadsheet($mod, $lines, 'Part Id|Name|Quantity|Type|Description|Suppliers', $module_id);
     
     $db->query ("INSERT into module_module (supermodule_id, submodule_id, quantity) ".
-                "VALUES (".$db->get_module_id_by_name("Extruder Controller Board").", ".
-                $db->get_module_id_by_name("Universal Board Component Kit").", 1);");
-    
-  } elseif (!strcasecmp($mod, "Stepper Controller Board")) {
-    $lines[1].=",Mouser";
-    unset ($lines[2]);
-    parse_spreadsheet($mod, $lines, 'Name|Quantity|Type|Description|Item|Mouser');
-  } elseif (!strcasecmp($mod, "Extruder Controller Board")) {
-    $lines[1] = preg_replace("/Supplier/i", "Mouser", $lines[1]);
-    unset ($lines[2]);
-    parse_spreadsheet($mod, $lines, 'Name|Quantity|Type|Description|Item|Mouser');
-  } elseif (!strcasecmp($mod, "PowerComms Board")) {
-    
-    // move the note into the rest of the spreadsheet
-    for ($l=0; $l < count($lines); $l++) {
-      if (preg_match("/C2-C5/", $lines[$l])) { $c_item = $l; }
-      if (preg_match("/U2/", $lines[$l])) { $u_item = $l; }
-      if (preg_match("/MAX202/", $lines[$l])) { $note_line = $l; }
-    }
-    $lines[$c_item].=','.$lines[$note_line];
-    $lines[$u_item].=','.$lines[$note_line];
-    unset($lines[19]);
-    $lines[1].=",Note";
-    
-    parse_spreadsheet($mod, $lines, 'Name|Quantity|Type|Item|Description|Mouser|RRRF|Note');
-    
-  } elseif (!strcasecmp($mod, "Opto Endstop Board")) {
-    // split spreadsheet in two
-    $electrical = array($lines[0]);
-    $l=2;
-    do {
-      array_push($electrical, $lines[$l]);
-      $l++;
-    } while ($l < count($lines) && !preg_match("/^Mechanical/i", $lines[$l]));
-    parse_spreadsheet($mod, $electrical, 'Item|Name / Value|Quantity|Description|Mouser|RRRF|Farnell (For European Reprappers)');
-    
-    $l++;
-    $lines[$l] = preg_replace("/Item/i", "Name", $lines[$l]);
-    $mechanical = array($lines[0], $lines[$l++]);
-    do {
-      array_push($mechanical, $lines[$l]);
-      $l++;
-    } while ($l < count($lines));
-    parse_spreadsheet($mod, $mechanical, 'Name|Quantity|Type');
-    
-  } elseif (!strcasecmp($mod, "Stepper Tester Board")) {
-    
-    // remove Mouser BOM
-    $BOM_trigger=0;
-    for ($l=0; $l < count($lines); $l++) {
-      if (preg_match("/BOM Importer/i", $lines[$l])) { $BOM_trigger=1; }
-      if ($BOM_trigger) { unset($lines[$l]); }
-    }
-    
-    parse_spreadsheet($mod, $lines, 'Item|Name / Value|Quantity|Description|Mouser|RRRF');
-  } elseif (!strcasecmp($mod, "Miscellaneous Kit")) {
-    parse_spreadsheet($mod, $lines, 'Name|Quantity|Description');
-  } else {
+                "VALUES ($stepper_controller_id, $module_id, 1);");
+    break;
+
+  case  "Stepper Controller Board v1.2":
+    echo "Parsing Stepper Controller\n";
+    $stepper_controller_id = $db->create_module("Stepper Controller Board v1.2", $darwin_module_id, 3);
+    parse_spreadsheet($mod, $lines, 'Part Id|Name|Quantity|Type|Description|Suppliers', $stepper_controller_id);
+    break;
+
+  case "Thermoplast Extruder Controller Board v1.2":
+    echo "Parsing extruder\n";
+    $thermoplast_controller_id = $db->create_module("Thermoplast Extruder Controller Board v1.2", $darwin_module_id);
+    parse_spreadsheet($mod, $lines, 'Part Id|Name|Quantity|Type|Description|Suppliers',  $thermoplast_controller_id);
+    break;
+
+  case  "PowerComms Board v1.2":
+    echo "Parsing PowerComms board\n";
+    $module_id = $db->create_module("PowerComms Board v1.2", $darwin_module_id);
+    parse_spreadsheet($mod, $lines, 'Part Id|Name|Quantity|Type|Description|Suppliers', $module_id);
+    break;
+
+  case "Opto Endstop Board v1.0":
+    echo "Parsing Opto Endstop Board\n";
+    $module_id = $db->create_module("Opto Endstop Board v1.0", $darwin_module_id, 3);
+    parse_spreadsheet($mod, $lines, 'Part Id|Name|Quantity|Type|Description|Suppliers', $module_id);
+    break;
+
+  case  "Stepper Tester Board v1.0";
+    echo "Parsing Stepper Tester Board\n";
+    $module_id = $db->create_module("Stepper Tester Board v1.0", $darwin_module_id);
+    parse_spreadsheet($mod, $lines, 'Item|Name / Value|Quantity|Type|Description|Suppliers', $module_id);
+    break;
+
+  case "Miscellaneous Kit":
+    echo "Parsing miscellaneous kit\n";
+    parse_spreadsheet($mod, $lines, 'Id|Name|Quantity|Type|Description|Suppliers', $module_id);
+    break;
+
+  default:
     echo ("Couldn't find a parser for $mod\n");
   }
 }
 
 $db->query("UPDATE tag SET name='fastener' WHERE name='fast'");
-$db->query("UPDATE module SET url='http://reprap.org/bin/view/Main/WebHome' WHERE id=1");
+$db->query("UPDATE module SET url='http://reprap.org/bin/view/Main/WebHome' WHERE name=\"Darwin\"");
+
+function set_vendor_part_id_by_part_name($part_name, $vendor_part_id) {
+  // adjusts source_part, creates if needed
+
+  global $db;
+
+  $part_id = $db->get_part_id_by_name($part_name);
+  if ($part_id == 1) {
+    echo "Can't set vendor part id to $vendor_part_id for $part_name.\n";
+  } else {
+    $source_id = $db->get_source_id_by_name("McMaster");
+    $q = $db->query("SELECT id FROM source_part WHERE part_id=$part_id and source_id=$source_id");
+    $row = $q->fetchRow();
+    if ($row['id']) {
+      $db->query("UPDATE source_part SET vendor_part_id='$vendor_part_id' WHERE id=".$row['id']);
+    } else {
+      $db->query("INSERT into source_part (part_id, source_id, vendor_part_id) VALUES ($part_id, $source_id, \"$vendor_part_id\")");
+    }      
+  }
+}
+
+set_vendor_part_id_by_part_name('M3 nut', '90591A121');
+set_vendor_part_id_by_part_name('M3 washer', '91166A210');
+set_vendor_part_id_by_part_name('M3 x 15 cap', '91290A120');
+set_vendor_part_id_by_part_name('M3 x 25 cap', '91290A125');
+set_vendor_part_id_by_part_name('M3 x 30 cap', '91290A130');
+set_vendor_part_id_by_part_name('M3 x 35 cap', '91292A033 ');
 
 echo "\n\ndone.\n";
