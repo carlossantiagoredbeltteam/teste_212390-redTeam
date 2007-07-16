eac0c11f10d3cd981dde16be27443dd39e56ee0b
hoekstar <hoekstar@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2007-07-16 00:55:23 +0000

    added my 2d host branch

---
+++ trunk/users/hoeken/2d-host-branch/src/org/reprap/devices/GenericStepperMotor.java
@@ -0,0 +1,738 @@
+package org.reprap.devices;
+
+import java.io.IOException;
+
+import org.reprap.Device;
+import org.reprap.Preferences;
+import org.reprap.ReprapException;
+import org.reprap.comms.Address;
+import org.reprap.comms.Communicator;
+import org.reprap.comms.IncomingContext;
+import org.reprap.comms.OutgoingMessage;
+import org.reprap.comms.IncomingMessage;
+import org.reprap.comms.IncomingMessage.InvalidPayloadException;
+import org.reprap.comms.messages.IncomingIntMessage;
+import org.reprap.comms.messages.OutgoingAddressMessage;
+import org.reprap.comms.messages.OutgoingBlankMessage;
+import org.reprap.comms.messages.OutgoingByteMessage;
+import org.reprap.comms.messages.OutgoingIntMessage;
+
+/**
+ *
+ */
+public class GenericStepperMotor extends Device {
+
+	/**
+	 * API for firmware
+	 * Activate the stepper motor in forward direction 
+	 */
+	public static final byte MSG_SetForward = 1;
+	
+	/**
+	 * Activate the stepper motor in reverse direction 
+	 */
+	public static final byte MSG_SetReverse = 2;
+	
+	/**
+	 *  Set the stepper motor position (how?)
+	 */
+	public static final byte MSG_SetPosition = 3;
+	
+	/**
+	 * Get the current stepper motor position
+	 */
+	public static final byte MSG_GetPosition = 4;
+	
+	/**
+	 * 
+	 */
+	public static final byte MSG_Seek = 5;	
+	
+	/**
+	 * Set the motor to idle; this turns the torque off whereas speed = 0 keeps torque on 
+	 */
+	public static final byte MSG_SetIdle = 6;		
+	
+	/**
+	 * Set notification (?) 
+	 */
+	public static final byte MSG_SetNotification = 7;
+	
+	/**
+	 * Set the sync mode (?)
+	 */
+	public static final byte MSG_SetSyncMode = 8;
+	
+	/**
+	 * Calibrate (?) 
+	 */
+	public static final byte MSG_Calibrate = 9;
+	
+	/**
+	 * Get the range (?)  
+	 */
+	public static final byte MSG_GetRange = 10;
+	
+	/**
+	 * DDAMaster ?
+	 */
+	public static final byte MSG_DDAMaster = 11;
+	
+	/**
+	 * Move on step in forward direction 
+	 */
+	public static final byte MSG_StepForward = 12;
+	
+	/**
+	 * Move on step in backward direction
+	 */
+	public static final byte MSG_StepBackward = 13;	
+	
+	/**
+	 * Set the power to the stepper motor 
+	 */
+	public static final byte MSG_SetPower = 14;
+	
+	/**
+	 * Homereset(?? 
+	 */
+	public static final byte MSG_HomeReset = 16;
+
+
+	/**
+	 * 
+	 */
+	public static final byte SYNC_NONE = 0;
+	public static final byte SYNC_SEEK = 1;
+	public static final byte SYNC_INC = 2;
+	public static final byte SYNC_DEC = 3;
+	
+	/**
+	 * 
+	 */
+	private boolean haveInitialised = false;
+	private boolean haveSetNotification = false;
+	private boolean haveCalibrated = false;
+	
+	/**
+	 * Power output limiting (0-100 percent)
+	 */
+	private int maxTorque; 
+	
+	/**
+	 * @param communicator
+	 * @param address
+	 * @param prefs
+	 * @param motorId
+	 */
+	public GenericStepperMotor(Communicator communicator, Address address, Preferences prefs, int motorId) {
+		
+		//Address address, int maxTorque
+		super(communicator, address);
+		
+		String axis;
+		switch(motorId)
+		{
+		case 1:
+			axis = "X";
+			break;
+		case 2:
+			axis = "Y";
+			break;
+		case 3:
+			axis = "Z";
+			break;
+		default:
+			axis = "X";
+			System.err.println("GenericStepperMotor - dud axis id: " + motorId);
+				
+		}
+		this.maxTorque = prefs.loadInt(axis + "Axis" + "Torque(%)");
+	}
+
+	/**
+	 * @throws IOException
+	 */
+	private void initialiseIfNeeded() throws IOException {
+		if (!haveInitialised) {
+			haveInitialised = true;
+			setMaxTorque(maxTorque);
+		}
+	}
+	
+	/**
+	 * Dispose of this object
+	 */
+	public void dispose() {
+	}
+	
+	/**
+	 * Set the motor speed (or turn it off) 
+	 * @param speed A value between -255 and 255.
+	 * @throws ReprapException
+	 * @throws IOException
+	 */
+	public void setSpeed(int speed) throws IOException {
+		initialiseIfNeeded();
+		lock();
+		try {
+			OutgoingMessage request = new RequestSetSpeed(speed);
+			sendMessage(request);
+		}
+		finally {
+			unlock();
+		}
+	}
+
+	/**
+	 * @throws IOException
+	 */
+	public void setIdle() throws IOException {
+		initialiseIfNeeded();
+		lock();
+		try {
+			OutgoingMessage request = new RequestSetSpeed();
+			sendMessage(request);
+		}
+		finally {
+			unlock();
+		}
+	}
+	
+	/**
+	 * @throws IOException
+	 */
+	public void stepForward() throws IOException {
+		initialiseIfNeeded();
+		lock();
+		try {
+			OutgoingMessage request = new RequestOneStep(true);
+			sendMessage(request);
+		}
+		finally {
+			unlock();
+		}
+	}
+	
+	/**
+	 * @throws IOException
+	 */
+	public void stepBackward() throws IOException {
+		initialiseIfNeeded();
+		lock();
+		try {
+			OutgoingMessage request = new RequestOneStep(false);
+			sendMessage(request);
+		}
+		finally {
+			unlock();
+		}
+	}
+	
+	/**
+	 * @throws IOException
+	 */
+	public void resetPosition() throws IOException {
+		setPosition(0);
+	}
+	
+	/**
+	 * @param position
+	 * @throws IOException
+	 */
+	public void setPosition(int position) throws IOException {
+		initialiseIfNeeded();
+		lock();
+		try {
+			sendMessage(new RequestSetPosition(position));
+		}
+		finally {
+			unlock();
+		}
+	}
+	
+	/**
+	 * @return current position of the motor
+	 * @throws IOException
+	 */
+	public int getPosition() throws IOException {
+		//System.out.println("get enter");
+		int value;
+		initialiseIfNeeded();
+		lock();
+		try {
+			IncomingContext replyContext = sendMessage(
+					new OutgoingBlankMessage(MSG_GetPosition));
+			
+			IncomingIntMessage reply = new RequestPositionResponse(replyContext);
+			try {
+				value = reply.getValue();
+			}
+			catch (IncomingMessage.InvalidPayloadException ex) {
+				throw new IOException(ex.getMessage());
+			}
+		}
+		finally {
+			unlock();
+		}
+		//System.out.println("get leave");
+		return value;
+	}
+	
+	/**
+	 * @param speed
+	 * @param position
+	 * @throws IOException
+	 */
+	public void seek(int speed, int position) throws IOException {
+		//System.out.println("seek enter");
+		initialiseIfNeeded()	;
+		lock();
+		try {
+			sendMessage(new RequestSeekPosition(speed, position));
+		}
+		finally {
+			unlock();
+			//System.out.println("seek leave");
+		}
+	}
+
+	/**
+	 * @param speed
+	 * @param position
+	 * @throws IOException
+	 */
+	public void seekBlocking(int speed, int position) throws IOException {
+		initialiseIfNeeded();
+		lock();
+		try {
+			setNotification();
+			new RequestSeekResponse(this, new RequestSeekPosition(speed, position), 50000);
+			setNotificationOff();
+		} catch (Exception e) {
+			// TODO: Nasty error. But WTF do we do about it?
+			e.printStackTrace();
+		} finally {
+			unlock();
+		}
+	}
+
+	/**
+	 * @param speed
+	 * @return range of the motor
+	 * @throws IOException
+	 * @throws InvalidPayloadException
+	 */
+	public Range getRange(int speed) throws IOException, InvalidPayloadException {
+		initialiseIfNeeded()	;
+		lock();
+		try {
+			if (haveCalibrated) {
+				IncomingContext replyContext = sendMessage(
+						new OutgoingBlankMessage(MSG_GetRange));
+				RequestRangeResponse response = new RequestRangeResponse(replyContext);
+				return response.getRange();
+			} else {
+				setNotification();
+				IncomingContext replyContext = sendMessage(
+						new OutgoingByteMessage(MSG_Calibrate, (byte)speed));
+				RequestRangeResponse response = new RequestRangeResponse(replyContext);
+				setNotificationOff();
+				return response.getRange();
+			}
+		}
+		finally {
+			unlock();
+		}
+	}
+	
+	/**
+	 * @param speed
+	 * @throws IOException
+	 * @throws InvalidPayloadException
+	 */
+	public void homeReset(int speed) throws IOException, InvalidPayloadException {
+		initialiseIfNeeded()	;
+		lock();
+		try {
+			setNotification();
+			new RequestHomeResetResponse(this, new OutgoingByteMessage(MSG_HomeReset, (byte)speed), 60000);
+			setNotificationOff();
+		} finally {
+			unlock();
+		}
+	}
+	
+	/**
+	 * @param syncType
+	 * @throws IOException
+	 */
+	public void setSync(byte syncType) throws IOException {
+		initialiseIfNeeded()	;
+		lock();
+		try {
+			sendMessage(
+					new OutgoingByteMessage(MSG_SetSyncMode, syncType));
+		}
+		finally {
+			unlock();
+		}
+		
+	}
+	
+	/**
+	 * @param speed
+	 * @param x1
+	 * @param deltaY
+	 * @throws IOException
+	 */
+	public void dda(int speed, int x1, int deltaY) throws IOException {
+		initialiseIfNeeded()	;
+		lock();
+		try {
+			setNotification();
+			
+			new RequestDDAMasterResponse(this, new RequestDDAMaster(speed, x1, deltaY), 60000);
+			
+			setNotificationOff();
+		}
+		finally {
+			unlock();
+		}
+	}
+	
+	/**
+	 * @throws IOException
+	 */
+	private void setNotification() throws IOException {
+		initialiseIfNeeded()	;
+		if (!haveSetNotification) {
+			sendMessage(new OutgoingAddressMessage(MSG_SetNotification,
+					getCommunicator().getAddress()));
+			haveSetNotification = true;
+		}
+	}
+
+	/**
+	 * @throws IOException
+	 */
+	private void setNotificationOff() throws IOException {
+		initialiseIfNeeded()	;
+		if (haveSetNotification) {
+			sendMessage(new OutgoingAddressMessage(MSG_SetNotification, getAddress().getNullAddress()));
+			haveSetNotification = false;
+		}
+	}
+
+	/**
+	 * 
+	 * @param maxTorque An integer value 0 to 100 representing the maximum torque percentage
+	 * @throws IOException
+	 */
+	public void setMaxTorque(int maxTorque) throws IOException {
+		initialiseIfNeeded()	;
+		if (maxTorque > 100) maxTorque = 100;
+		double power = maxTorque * 68.0 / 100.0;
+		byte scaledPower = (byte)power;
+		lock();
+		try {
+			sendMessage(
+					new OutgoingByteMessage(MSG_SetPower, scaledPower));
+		}
+		finally {
+			unlock();
+		}
+		
+	}
+	
+	
+	/**
+	 *
+	 */
+	protected class RequestPositionResponse extends IncomingIntMessage {
+		
+		/**
+		 * @param incomingContext
+		 * @throws IOException
+		 */
+		public RequestPositionResponse(IncomingContext incomingContext) throws IOException {
+			super(incomingContext);
+		}
+		
+		/* (non-Javadoc)
+		 * @see org.reprap.comms.messages.IncomingIntMessage#isExpectedPacketType(byte)
+		 */
+		protected boolean isExpectedPacketType(byte packetType) {
+			return packetType == MSG_GetPosition; 
+		}
+	}
+
+	/**
+	 *
+	 */
+	protected class RequestSeekResponse extends IncomingIntMessage {
+		
+		/**
+		 * @param device
+		 * @param message
+		 * @param timeout
+		 * @throws IOException
+		 */
+		public RequestSeekResponse(Device device, OutgoingMessage message, long timeout) throws IOException {
+			super(device, message, timeout);
+		}
+		
+		/* (non-Javadoc)
+		 * @see org.reprap.comms.messages.IncomingIntMessage#isExpectedPacketType(byte)
+		 */
+		protected boolean isExpectedPacketType(byte packetType) {
+			return packetType == MSG_Seek; 
+		}
+	}
+	
+	/**
+	 *
+	 */
+	protected class RequestSetPosition extends OutgoingIntMessage {
+		/**
+		 * @param position
+		 */
+		public RequestSetPosition(int position) {
+			super(MSG_SetPosition, position);
+		}
+	}
+
+	protected class RequestSetSpeed extends OutgoingMessage {
+
+		/**
+		 * 
+		 */
+		byte [] message;
+		
+		/**
+		 * The empty constructor will create a message to idle the motor
+		 */
+		RequestSetSpeed() {
+			message = new byte [] { MSG_SetIdle }; 
+		}
+		
+		/**
+		 * Create a message for setting the motor speed.
+		 * @param speed The speed to set the motor to.  Note that specifying
+		 * 0 will stop the motor and hold it at 0 and thus still draws
+		 * high current.  To idle the motor, use the message created by the
+		 * empty constructor.
+		 */
+		RequestSetSpeed(int speed) {
+			byte command;
+			if (speed >= 0) {
+				command = MSG_SetForward;
+			} else {
+				command = MSG_SetReverse;
+				speed = -speed;
+			}
+			if (speed > 255) speed = 255;
+			message = new byte[] { command, (byte)speed };
+				
+		}
+		
+		/* (non-Javadoc)
+		 * @see org.reprap.comms.OutgoingMessage#getBinary()
+		 */
+		public byte[] getBinary() {
+			return message;
+		}
+		
+	}
+	
+	/**
+	 *
+	 */
+	protected class RequestOneStep extends OutgoingMessage
+	{
+		/**
+		 * 
+		 */
+		byte [] message;
+		
+		/**
+		 * @param forward
+		 */
+		RequestOneStep(boolean forward) {
+			byte command;
+			if (forward)
+				command = MSG_StepForward;
+			else
+				command = MSG_StepBackward;
+			message = new byte[] { command };
+				
+		}
+		
+		/* (non-Javadoc)
+		 * @see org.reprap.comms.OutgoingMessage#getBinary()
+		 */
+		public byte[] getBinary() {
+			return message;
+		}
+	}
+	
+	/**
+	 *
+	 */
+	protected class RequestSeekPosition extends OutgoingMessage {
+		/**
+		 * 
+		 */
+		byte [] message;
+		
+		/**
+		 * @param speed
+		 * @param position
+		 */
+		RequestSeekPosition(int speed, int position) {
+			message = new byte[] { MSG_Seek,
+					(byte)speed,
+					(byte)(position & 0xff),
+					(byte)((position >> 8) & 0xff)};
+		}
+		
+		/* (non-Javadoc)
+		 * @see org.reprap.comms.OutgoingMessage#getBinary()
+		 */
+		public byte[] getBinary() {
+			return message;
+		}
+		
+	}
+
+	/**
+	 *
+	 */
+	protected class RequestDDAMaster extends OutgoingMessage {
+		byte [] message;
+		
+		/**
+		 * @param speed
+		 * @param x1
+		 * @param deltaY
+		 */
+		RequestDDAMaster(int speed, int x1, int deltaY) {
+			message = new byte[] { MSG_DDAMaster,
+					(byte)speed,
+					(byte)(x1 & 0xff),
+					(byte)((x1 >> 8) & 0xff),
+					(byte)(deltaY & 0xff),
+					(byte)((deltaY >> 8) & 0xff)
+				};
+		}
+		
+		/* (non-Javadoc)
+		 * @see org.reprap.comms.OutgoingMessage#getBinary()
+		 */
+		public byte[] getBinary() {
+			return message;
+		}
+		
+	}
+
+	/**
+	 *
+	 */
+	protected class RequestDDAMasterResponse extends IncomingIntMessage {
+		
+		/**
+		 * @param device
+		 * @param message
+		 * @param timeout
+		 * @throws IOException
+		 */
+		public RequestDDAMasterResponse(Device device, OutgoingMessage message, long timeout) throws IOException {
+			super(device, message, timeout);
+		}
+		
+		/* (non-Javadoc)
+		 * @see org.reprap.comms.messages.IncomingIntMessage#isExpectedPacketType(byte)
+		 */
+		protected boolean isExpectedPacketType(byte packetType) {
+			return packetType == MSG_DDAMaster;
+		}
+	}
+	
+	/**
+	 *
+	 */
+	protected class RequestRangeResponse extends IncomingIntMessage {
+		
+		/**
+		 * @param incomingContext
+		 * @throws IOException
+		 */
+		public RequestRangeResponse(IncomingContext incomingContext) throws IOException {
+			super(incomingContext);
+		}
+		
+		/* (non-Javadoc)
+		 * @see org.reprap.comms.messages.IncomingIntMessage#isExpectedPacketType(byte)
+		 */
+		protected boolean isExpectedPacketType(byte packetType) {
+			// We could get this either as an asynchronous notification
+			// from calibration or by explicit request
+			return packetType == MSG_GetRange || packetType == MSG_Calibrate; 
+		}
+		
+		/**
+		 * @return
+		 * @throws InvalidPayloadException
+		 */
+		public Range getRange() throws InvalidPayloadException {
+		    byte [] reply = getPayload();
+		    if (reply == null || reply.length != 3)
+		    	throw new InvalidPayloadException("Unexpected payload getting range");
+		    Range r = new Range();
+		    r.minimum = 0;
+		    r.maximum = IncomingIntMessage.ConvertBytesToInt(reply[1], reply[2]);
+		    return r;
+		}
+
+	}
+	
+	/**
+	 *
+	 */
+	protected class RequestHomeResetResponse extends IncomingMessage {
+		
+		/**
+		 * @param device
+		 * @param message
+		 * @param timeout
+		 * @throws IOException
+		 */
+		public RequestHomeResetResponse(Device device, OutgoingMessage message, long timeout) throws IOException {
+			super(device, message, timeout);
+		}
+		
+		/* (non-Javadoc)
+		 * @see org.reprap.comms.IncomingMessage#isExpectedPacketType(byte)
+		 */
+		protected boolean isExpectedPacketType(byte packetType) {
+			return packetType == MSG_HomeReset; 
+		}
+	}
+	
+	/**
+	 *
+	 */
+	public class Range {
+		
+		/**
+		 * 
+		 */
+		public int minimum;
+		
+		/**
+		 * 
+		 */
+		public int maximum;
+	}
+
+}
---
+++ trunk/users/hoeken/2d-host-branch/src/org/reprap/geometry/Producer.java
@@ -0,0 +1,343 @@
+package org.reprap.geometry;
+
+import java.util.*;
+import javax.media.j3d.*;
+import javax.vecmath.*;
+import org.reprap.Preferences;
+import org.reprap.Printer;
+import org.reprap.geometry.polygons.*;
+import org.reprap.gui.PreviewPanel;
+import org.reprap.gui.RepRapBuild;
+import org.reprap.machines.MachineFactory;
+import org.reprap.machines.NullCartesianMachine;
+
+public class Producer {
+	
+	/**
+	 * 
+	 */
+	protected Printer reprap;
+	
+	/**
+	 * 
+	 */
+	protected RrHalfPlane oddHatchDirection;
+	
+	/**
+	 * 
+	 */
+	protected RrHalfPlane evenHatchDirection;
+	
+	/**
+	 * 
+	 */
+	protected RepRapBuild bld;
+	
+	/**
+	 * @param preview
+	 * @param builder
+	 * @throws Exception
+	 */
+	public Producer(PreviewPanel preview, RepRapBuild builder) throws Exception {
+		reprap = MachineFactory.create();
+		reprap.setPreviewer(preview);
+		preview.setMachine(reprap);
+		bld = builder;
+		oddHatchDirection = new RrHalfPlane(new Rr2Point(0.0, 0.0), new Rr2Point(1.0, 1.0));
+		evenHatchDirection = new RrHalfPlane(new Rr2Point(0.0, 1.0), new Rr2Point(1.0, 0.0));
+	}
+	
+//	/**
+//	 * @return simple polygon object that represents a 2D square
+//	 */
+//	public RrPolygon square()
+//	{
+//		RrPolygon a = new RrPolygon();
+//		Rr2Point p1 = new Rr2Point(10, 10);
+//		Rr2Point p2 = new Rr2Point(20, 10);
+//		Rr2Point p3 = new Rr2Point(20, 20);
+//		Rr2Point p4 = new Rr2Point(10, 20);
+//		a.add(p1, 1);
+//		a.add(p2, 1);
+//		a.add(p3, 1);
+//		a.add(p4, 1);
+//		return a;
+//	}
+	
+//	/**
+//	 * @return hexagonal object
+//	 */
+//	public RrCSGPolygon hex()
+//	{
+//		double hexSize = 20;
+//		double hexX = 15, hexY = 15;
+//		
+//		RrCSG r = RrCSG.universe();
+//		Rr2Point pold = new Rr2Point(hexX + hexSize/2, hexY);
+//		Rr2Point p;
+//		double theta = 0; 
+//		for(int i = 0; i < 6; i++)
+//		{
+//			theta += Math.PI * 60. / 180.0;
+//			p = new Rr2Point(hexX + Math.cos(theta)*hexSize/2, hexY + Math.sin(theta)*hexSize/2);
+//			r = RrCSG.intersection(r, new RrCSG(new RrHalfPlane(p, pold)));
+//			pold = p;
+//		}
+//		
+//		// Horrid hacks in multipliers next...
+//		return new RrCSGPolygon(r, new RrBox(new Rr2Point(hexX - hexSize*0.57, hexY - hexSize*0.61), 
+//				new Rr2Point(hexX + hexSize*0.537, hexY + hexSize*0.623)));
+//	}
+	
+//	/**
+//	 * @return Adrian's testshape; a 3D object
+//	 */
+//	public RrCSGPolygon adriansTestShape()
+//	{
+//		Rr2Point p = new Rr2Point(3, 5);
+//		Rr2Point q = new Rr2Point(7, 27);
+//		Rr2Point r = new Rr2Point(32, 30);
+//		Rr2Point s = new Rr2Point(31, 1);
+//		
+//		Rr2Point pp = new Rr2Point(12, 21);
+//		Rr2Point qq = new Rr2Point(18, 32);
+//		Rr2Point rr = new Rr2Point(15, 17);    
+//		
+//		RrHalfPlane ph = new RrHalfPlane(p, q);
+//		RrHalfPlane qh = new RrHalfPlane(q, r);
+//		RrHalfPlane rh = new RrHalfPlane(r, s);
+//		RrHalfPlane sh = new RrHalfPlane(s, p);
+//		
+//		RrHalfPlane pph = new RrHalfPlane(pp, qq);
+//		RrHalfPlane qqh = new RrHalfPlane(qq, rr);
+//		RrHalfPlane rrh = new RrHalfPlane(rr, pp);
+//		
+//		RrCSG pc = new RrCSG(ph);
+//		RrCSG qc = new RrCSG(qh);
+//		RrCSG rc = new RrCSG(rh);
+//		RrCSG sc = new RrCSG(sh);
+//		
+//		pc = RrCSG.intersection(pc, qc);
+//		rc = RrCSG.intersection(sc, rc);		
+//		pc = RrCSG.intersection(pc, rc);
+//		
+//		RrCSG ppc = new RrCSG(pph);
+//		RrCSG qqc = new RrCSG(qqh);
+//		RrCSG rrc = new RrCSG(rrh);
+//		
+//		ppc = RrCSG.intersection(ppc, qqc);
+//		ppc = RrCSG.intersection(ppc, rrc);
+//		ppc = RrCSG.difference(pc, ppc);
+//		
+//		pc = ppc.offset(-5);
+//		ppc = RrCSG.difference(ppc, pc);
+//		
+//		RrCSGPolygon result = new RrCSGPolygon(ppc, new 
+//				RrBox(new Rr2Point(0,0), new Rr2Point(110,110)));
+////		result.divide(1.0e-6, 1);
+////		new RrGraphics(result, true);
+//		return result;
+//	}
+	
+	/**
+	 * @throws Exception
+	 */
+//	public void produce(boolean testPiece) throws Exception {
+	public void produce() throws Exception {
+
+        // Fallback defaults
+		//int extrusionSpeed = 200;
+		//int extrusionTemp = 40;
+		//int movementSpeedXY = 230;
+		//int fastSpeedXY = 230;
+		//int movementSpeedZ = 230;
+		
+		//int coolingPeriod = Preferences.loadGlobalInt("CoolingPeriod");
+		boolean subtractive = Preferences.loadGlobalBool("Subtractive");
+		
+//		try {
+//			extrusionSpeed = Preferences.loadGlobalInt("ExtrusionSpeed");
+//			extrusionTemp = Preferences.loadGlobalInt("ExtrusionTemp");
+//			movementSpeedXY = Preferences.loadGlobalInt("MovementSpeed");
+//			fastSpeedXY = Preferences.loadGlobalInt("FastSpeed");
+//			movementSpeedZ = Preferences.loadGlobalInt("MovementSpeedZ");
+//		} catch (Exception ex) {
+//			System.out.println("Warning: could not load ExtrusionSpeed/MovementSpeed, using defaults");
+//		}
+		
+
+//		reprap.setSpeed(movementSpeedXY);
+//		reprap.setFastSpeed(fastSpeedXY);
+//		reprap.setSpeedZ(movementSpeedZ);
+		System.out.println("Intialising reprap");
+		reprap.initialise();
+		System.out.println("Selecting material 0");
+		reprap.selectExtruder(0);
+		//reprap.setExtruderSpeed(extrusionSpeed);
+		System.out.println("Setting temperature");
+		reprap.getExtruder().heatOn();
+		
+		// A "warmup" segment to get things in working order
+		if (!subtractive) {
+			System.out.println("Printing warmup segments, moving to (0,5)");
+			reprap.setSpeed(reprap.getFastSpeed());
+			reprap.moveTo(0, 5, reprap.getExtruder().getExtrusionHeight(), true, false);
+			reprap.setSpeed(reprap.getExtruder().getXYSpeed());
+			System.out.println("Printing warmup segments, printing to (0,20)");
+			reprap.printTo(0, 20, reprap.getExtruder().getExtrusionHeight(), false);
+			System.out.println("Printing warmup segments, printing to (2,20)");
+			reprap.printTo(2, 20, reprap.getExtruder().getExtrusionHeight(), false);
+			System.out.println("Printing warmup segments, printing to (2,5)");
+			reprap.printTo(2, 5, reprap.getExtruder().getExtrusionHeight(), true);
+			reprap.setSpeed(reprap.getFastSpeed());
+		}
+		
+		// This should now split off layers one at a time
+		// and pass them to the LayerProducer.  
+		
+		boolean isEvenLayer = true;
+		STLSlice stlc;
+		double zMax;
+//		if(testPiece)
+//		{
+//			stlc = null;
+//			zMax = 5;
+//		} else
+//		{
+			bld.mouseToWorld();
+			stlc = new STLSlice(bld.getSTLs());
+			zMax = stlc.maxZ();
+			// zMax = 1.6;  // For testing.
+//		}
+		
+		double startZ;
+		double endZ;
+		double stepZ;
+		if (subtractive) {
+			// Subtractive construction works from the top, downwards
+			startZ = zMax;
+			endZ = 0;
+			stepZ = -reprap.getExtruder().getExtrusionHeight();
+			reprap.setZManual(startZ);
+		} else {
+			// Normal constructive fabrication, start at the bottom and work up.
+			// Note that we start extruding one layer off the baseboard...
+			startZ = reprap.getExtruder().getExtrusionHeight();
+			endZ = zMax;
+			stepZ = reprap.getExtruder().getExtrusionHeight();
+		}
+		
+		for(double z = startZ; subtractive ? z > endZ : z < endZ; z += stepZ) {
+			
+			if (reprap.isCancelled())
+				break;
+			System.out.println("Commencing layer at " + z);
+
+			// Change Z height
+			reprap.moveTo(reprap.getX(), reprap.getY(), z, false, false);
+
+			// Layer cooling phase - after we've just raised the head.
+			//Only if we're not a null device.
+			if ((z != startZ && reprap.getExtruder().getCoolingPeriod() > 0)&&!(reprap instanceof NullCartesianMachine)) {
+				System.out.println("Starting a cooling period");
+				// Save where we are. We'll come back after we've cooled off.
+				double storedX=reprap.getX();
+				double storedY=reprap.getY();
+				reprap.getExtruder().setCooler(true);	// On with the fan.
+				//reprap.homeToZeroX();		// Seek (0,0)
+				//reprap.homeToZeroY();
+				reprap.setSpeed(reprap.getFastSpeed());
+				reprap.moveTo(0, 0, z, true, true);
+				Thread.sleep(1000 * reprap.getExtruder().getCoolingPeriod());
+				reprap.getExtruder().setCooler(false);
+				System.out.println("Brief delay for head to warm up.");
+				Thread.sleep(200 * reprap.getExtruder().getCoolingPeriod());
+				System.out.println("End of cooling period");
+				// TODO: BUG! Strangely, this only restores Y axis!
+				//System.out.println("stored X and Y: " + storedX + "   " + storedY);
+				
+				// The next layer will go where it wants to.
+				
+				//reprap.moveTo(storedX, storedY, z, true, true);
+				//reprap.setSpeed(reprap.getExtruder().getXYSpeed());
+				//reprap.moveTo(storedX, reprap.getY(), z, true, true);
+			}
+			
+			if (reprap.isCancelled())
+				break;
+
+			LayerProducer layer;
+//			if(testPiece)
+//			{
+//				layer = new LayerProducer(reprap, z, hex(), null,
+//						isEvenLayer?evenHatchDirection:oddHatchDirection);
+//			} else
+//			{
+				RrCSGPolygonList slice = stlc.slice(z+reprap.getExtruder().getExtrusionHeight()*0.5, 
+						LayerProducer.solidMaterial(), LayerProducer.gapMaterial());
+				BranchGroup lowerShell = stlc.getBelow();
+				if(slice.size() > 0)
+					layer = new LayerProducer(reprap, z, slice, lowerShell,
+						isEvenLayer?evenHatchDirection:oddHatchDirection);
+				else
+					layer = null;
+
+//			}
+			
+			if(layer != null)
+				layer.plot();
+		
+			isEvenLayer = !isEvenLayer;
+		}
+
+		if (subtractive)
+			reprap.moveTo(0, 0, startZ, true, true);
+		else
+			reprap.moveTo(0, 0, reprap.getZ(), true, true);
+		
+		reprap.terminate();
+
+	}
+
+	/**
+	 * The total distance moved is the total distance extruded plus 
+	 * plus additional movements of the extruder when no materials 
+	 * was deposited
+	 * 
+	 * @return total distance the extruder has moved 
+	 */
+	public double getTotalDistanceMoved() {
+		return reprap.getTotalDistanceMoved();
+	}
+	
+	/**
+	 * @return total distance that has been extruded in millimeters
+	 */
+	public double getTotalDistanceExtruded() {
+		return reprap.getTotalDistanceExtruded();
+	}
+	
+	/**
+	 * TODO: This figure needs to get added up as we go along to allow for different extruders
+	 * @return total volume that has been extruded
+	 */
+	public double getTotalVolumeExtruded() {
+		return reprap.getTotalDistanceExtruded() * reprap.getExtruder().getExtrusionHeight() * 
+		reprap.getExtruder().getExtrusionSize();
+	}
+	
+	/**
+	 * 
+	 */
+	public void dispose() {
+		reprap.dispose();
+	}
+
+	/**
+	 * @return total elapsed time in seconds between start and end of building the 3D object
+	 */
+	public double getTotalElapsedTime() {
+		return reprap.getTotalElapsedTime();
+	}
+	
+}
