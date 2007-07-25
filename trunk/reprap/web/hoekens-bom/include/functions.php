86f532d4d300b8e16b37a2afa48c3e826d6fe0f7
hoekstar <hoekstar@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2007-07-25 15:21:00 +0000

    created initial DB import framework

--- trunk/reprap/web/hoekens-bom/include/functions.php
+++ trunk/reprap/web/hoekens-bom/include/functions.php
@@ -3,7 +3,7 @@
 	{
 		$url = "http://spreadsheets.google.com/pub?key=pmEMxYRcQzzATwbOb71BmGA&output=csv&gid=$id";
 	
-		#echo "<!-- getting $url -->\n";
+		echo "<!-- getting $url -->\n";
 	
 		$fp = fopen($url, "r");
 		while ($ar = fgetcsv($fp))
