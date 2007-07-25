253ffb202ac5e2417927dcc5c1fec37a0c528cc4
hoekstar <hoekstar@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2007-07-25 17:54:30 +0000

    added the library directory

---
+++ trunk/reprap/web/hoekens-bom/lib/global.php
@@ -0,0 +1,8 @@
+<?
+	//simply include all our files...
+	include("application.php");
+	include("base-object.php");
+	include("base-controller.php");
+	include("collection.php");
+	include("db.php");
+?>
