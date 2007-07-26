044040c58880b6709fa6e154bbfd48935a6e26f4
hoekstar <hoekstar@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2007-07-26 14:56:39 +0000

    got a decent start on the framework for the site.

---
+++ trunk/reprap/web/hoekens-bom/framework/collection.php
@@ -0,0 +1,83 @@
+<?
+	class Collection
+	{
+		private $query;
+		private $map;
+		private $obj_types;
+		private $key;
+		private $expiration;
+	
+		public function __construct($query, $obj_types, $expiration = Cache::TIME_NEVER, $key = null)
+		{
+			$this->query = $query;
+			$this->map = array();
+			$this->reverse_map = array();
+			$this->obj_types = $obj_types;
+			$this->expiration = $expiration;
+		
+			if ($key === null)
+				$this->key = sha1($query);
+			else
+				$this->key = $key;
+	
+			$this->map = DB::getCachedArray($this->query, "{$key}.collection", $expiration);
+		}
+	
+		/**
+		* @TODO: fix this to make it work
+		*/
+		public function implodeMap($type)
+		{		 
+			if (is_array($this->map)) {
+				return implode(', ', $this->getMap($type));
+
+			return '';
+		}
+	
+		/**
+		* @TODO: make this work as well
+		*/
+		public function getMap($type = null)
+		{
+			return $this->map;
+		}
+		
+		public function count() 
+		{
+			if (is_array($this->map))
+				return count($this->map);
+			else
+				return 0;
+		}
+	
+		public function getAll() 
+		{
+			return $this->buildObjectArray();
+		}
+	
+		public function getRange($start, $end) 
+		{
+			if (is_array($this->map))
+				return $this->buildObjectArray(array_slice($this->map, $start, $end, false));
+			else
+				return array();
+		}
+	
+		private function buildObjectArray($map = null) 
+		{
+			if ($map === null)
+				$map = $this->map;
+			
+			if (!is_array($map))
+				return array();
+
+			//very simple load...  should be made more advanced.
+			$data = array();
+			foreach ($map AS $key => $row)
+				foreach ($this->obj_types AS $type => $id)
+					$data[$key][$type] = new $type($row[$type]);
+		
+			return $data;
+		}
+	}
+?>
