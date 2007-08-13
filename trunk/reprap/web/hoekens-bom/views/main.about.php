25ebb1e032a3e94b04578fd1c54a7b1121116471
hoekstar <hoekstar@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2007-08-13 04:53:17 +0000

    added subversion to the about page.

--- trunk/reprap/web/hoekens-bom/views/main.about.php
+++ trunk/reprap/web/hoekens-bom/views/main.about.php
@@ -1,11 +1,12 @@
 <h1>About the Parts Lister</h1>
 
 <p>
-	This website is a front-end for viewing Bill of Material information on the <a href="http://www.reprap.org">RepRap</a> project in an easy to manage, centralized, and database driven fashion.  The master data is stored in <a href="http://docs.google.com">Google Docs</a> to make data entry easy, to take advantage of their excellent UI, as well as author controls.
+	This website is a front-end for viewing Bill of Material information on the <a href="http://www.reprap.org">RepRap</a> project in an easy to manage, centralized, and database driven fashion.  The master data is stored in <a href="http://docs.google.com">Google Docs</a> to make data entry easy, to take advantage of their excellent UI, as well as author controls.  The code and the data are all licensed under the GPL.
 </p>
 
 <h2>Get the data!</h2>
 <ul>
 	<li><a href="http://spreadsheets.google.com/ccc?key=<?=RR_GOOGLE_DOC_KEY?>">Access the Google Spreadsheet</a></li>
 	<li><a href="/reprap-bill-of-materials.sql">Access the imported MySQL data</a></li>
+	<li><a href="http://reprap.svn.sourceforge.net/svnroot/reprap/trunk/reprap/web/hoekens-bom/">View the Subversion repository</a></li>
 </ul>
