9015af2858c58a5ebc39bb52d8fe612715cf726d
hoekstar <hoekstar@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2007-08-13 04:07:52 +0000

    more tidying housework stuff

--- trunk/reprap/web/hoekens-bom/extensions/global.php
+++ trunk/reprap/web/hoekens-bom/extensions/global.php
@@ -1,9 +1,6 @@
 <?
 	include("../framework/global.php");
 	include(EXTENSIONS_DIR . "config.php");
-	include(EXTENSIONS_DIR . "functions.php");
-	#include(EXTENSIONS_DIR . "classes.php");
-	#include(EXTENSIONS_DIR . "template.php");
 	
 	db()->selectDb('bom_dev');
 ?>
