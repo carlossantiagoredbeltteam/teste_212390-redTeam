0ca4c3c36ce6180aecf3cef21a340a02dc85c2e5
hoekstar <hoekstar@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2007-08-13 04:18:51 +0000

    added about page

---
+++ reprap/web/hoekens-bom/views/main.about.php
@@ -0,0 +1,11 @@
+<h1>About the Parts Lister</h1>
+
+<p>
+	This website is a front-end for viewing Bill of Material information on the <a href="http://www.reprap.org">RepRap</a> project in an easy to manage, centralized, and database driven fashion.  The master data is stored in <a href="http://docs.google.com">Google Docs</a> to make data entry easy, to take advantage of their excellent UI, as well as author controls.
+</p>
+
+<h2>Get the data!</h2>
+<ul>
+	<li><a href="http://spreadsheets.google.com/ccc?key=<?=RR_GOOGLE_DOC_KEY?>">Access the Google Spreadsheet</a></li>
+	<li><a href="/reprap-bill-of-materials.sql">Access the imported MySQL data</a></li>
+</ul>
