adc67534a5c5a35417e2538d9d1ae7cd1bea1bb8
hoekstar <hoekstar@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2007-08-13 04:07:52 +0000

    more tidying housework stuff

--- reprap/web/hoekens-bom/controllers/import.php
+++ reprap/web/hoekens-bom/controllers/import.php
@@ -33,6 +33,20 @@
 		}
 	}
 	
+	
+	function getRawSheetData($id)
+	{
+		$url = "http://spreadsheets.google.com/pub?key=pmEMxYRcQzzATwbOb71BmGA&output=csv&gid=$id";
+	
+		echo "<!-- getting $url -->\n";
+	
+		$fp = fopen($url, "r");
+		while ($ar = fgetcsv($fp))
+			$data[] = $ar;
+	
+		return $data;
+	}
+	
 	function loadLegend()
 	{
 		$legend = array();
