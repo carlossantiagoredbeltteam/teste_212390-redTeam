9cf1de9bd528d03f413370deaa193e3814b1c7e2
hoekstar <hoekstar@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2007-08-13 19:58:16 +0000

    added top level parts and supplier lists

---
+++ trunk/reprap/web/hoekens-bom/views/main.statistics.php
@@ -0,0 +1,5 @@
+<h1>Statistics</h1>
+
+<p>
+	<b>Coming soon.</b>
+</p>
---
+++ trunk/reprap/web/hoekens-bom/views/parts.all.php
@@ -0,0 +1,15 @@
+<h1>Parts</h1>
+<? if (!empty($parts)): ?>
+	<? foreach ($parts AS $row): ?>
+		<? $part = $row['UniquePart'] ?>
+		<? if ($part->get('type') != $type): ?>
+			<? $type = $part->get('type'); ?>
+			<h2><?=ucfirst($type)?> Parts (<a href="/type/<?=$type?>">see all</a>)</h2>
+		<? endif ?>
+		<p>
+			<b><a href="/uniquepart:<?=$part->id?>"><?=$part->get('name')?></a></b> <?=$part->get('description')?>
+		</p>
+	<? endforeach?>
+<? else: ?>
+	<b>No parts found.</b>
+<? endif ?>
