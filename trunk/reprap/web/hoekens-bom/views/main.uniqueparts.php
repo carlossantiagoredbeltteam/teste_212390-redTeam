1d337df521de2172da403ecc8336744a69bd2352
hoekstar <hoekstar@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2007-08-15 03:12:20 +0000

    fixed the quantity calculation stuff to work alot better now.

--- trunk/reprap/web/hoekens-bom/views/main.uniqueparts.php
+++ trunk/reprap/web/hoekens-bom/views/main.uniqueparts.php
@@ -16,35 +16,41 @@
 			<? foreach ($list->type_list AS $type => $data): ?>
 				<? foreach ($data AS $unique_id): ?>
 					<? $unique = $list->getUnique($unique_id); ?>
-					<tr>
-						<td>
-							<input type="checkbox" id="use_<?=$unique->id?>" name="use_part[<?=$unique->id?>]" value="1" checked="true"/>
-							<a href="/uniquepart:<?=$unique->id?>"><?=$unique->get('name')?></a>
+					<? $quantity = $list->getUniqueQuantity($unique->id); ?>
+					<? if ($quantity): ?>
+						<tr>
+							<td>
+								<input type="checkbox" id="use_<?=$unique->id?>" name="use_part[<?=$unique->id?>]" value="1" checked="true"/>
+								<a href="/uniquepart:<?=$unique->id?>"><?=$unique->get('name')?></a>
 						
-							<span onclick="Element.toggle('breakdown_<?=$unique->id?>')">(breakdown)</span>
-						</td>
-						<td><a href="/type/<?=$type?>"><?=$type?></a></td>
-						<td><input type="text" name="quantity[<?=$unique->id?>]" value="<?=$list->getUniqueQuantity($unique->id)?>" size="3"></td>
-						<td><?= Controller::byName('main')->renderView('part_suppliers', array('part' => $unique))?></td>
-					</tr>
-					<tr id="breakdown_<?=$unique->id?>" style="display: none;">
-						<td colspan="4">
-							<div style="margin-left: 20px">
-								<table>
-									<tr>
-										<td><b>Part</b></td>
-										<td><b>Quantity</b></td>
-									</tr>
-									<? foreach ($list->raw_list[$unique->id] AS $part): ?>
+								<span onclick="Element.toggle('breakdown_<?=$unique->id?>')">(breakdown)</span>
+							</td>
+							<td><a href="/type/<?=$type?>"><?=$type?></a></td>
+							<td>
+								<input type="text" name="quantity[<?=$unique->id?>]" value="<?=$quantity?>" size="3">
+								<?= $unique->get('units') ?>
+							</td>
+							<td><?= Controller::byName('main')->renderView('part_suppliers', array('part' => $unique))?></td>
+						</tr>
+						<tr id="breakdown_<?=$unique->id?>" style="display: none;">
+							<td colspan="4">
+								<div style="margin-left: 20px">
+									<table>
 										<tr>
-											<td><?=$part->get('raw_text')?></td>
-											<td><?=$part->get('quantity')?></td>
+											<td><b>Part</b></td>
+											<td><b>Quantity</b></td>
 										</tr>
-									<? endforeach ?>
-								</table>
-							</div>
-						</td>
-					</tr>
+										<? foreach ($list->raw_list[$unique->id] AS $part): ?>
+											<tr>
+												<td><?=$part->get('raw_text')?></td>
+												<td><?=$part->get('quantity')?></td>
+											</tr>
+										<? endforeach ?>
+									</table>
+								</div>
+							</td>
+						</tr>
+					<? endif ?>
 				<? endforeach ?>
 			<? endforeach ?>
 		</table>
