c2868529997a83a4125a0c7b1c3f26cc293cc010
hoekstar <hoekstar@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2007-08-19 22:01:54 +0000

    fixed a couple bugs in module selection

--- reprap/web/hoekens-bom/views/main.home.php
+++ reprap/web/hoekens-bom/views/main.home.php
@@ -69,48 +69,48 @@
 			<td>&nbsp;</td><td><input type="submit" name="submit" value="Next Step"></td>
 		</tr>
 	</table>
+</form>
 	
-	<h1>Detailed Part List Lookup</h1>
-	<form action="/uniqueparts">
-		<table width="100%">
-			<tr>
-				<th width="10%">Use?</th>
-				<th width="75%">Module</th>
-				<th width="15%">Quantity</th>
-			</tr>
-			<? foreach ($modules AS $row): ?>
-				<? $module = $row['UniquePart']; ?>
-				<tr class="module_row" id="module_<?=$module->id?>">
-					<td align="center">
-						<input type="checkbox" id="module_check_<?=$module->id?>" name="use_module[<?=$module->id?>]" value="1" onclick="selectModule(<?=$module->id?>)"/>
-					</td>
-					<td>
-						<input type="hidden" name="module_id[<?=$module->id?>]" value="<?=$module->id?>" />
-						<b><a href="/uniquepart:<?=$module->id?>"><?=$module->get('name')?></a></b>
-						<img src="/img/help-icon.gif" onclick="Element.toggle('description_<?=$module->id?>')">
-					</td>
-					<td align="center"><input type="text" name="quantities[<?=$module->id?>]" value="1" size="3"></td>
-				</tr>
-				<tr id="description_<?=$module->id?>" style="display: none">
-					<td>&nbsp;</td>
-					<td>
-						<?=$module->get('description')?>
-					</td>
-					<td>&nbsp;</td>
-				</tr>
-			<? endforeach ?>
-			<tr>
-				<th>Deep Lookup?</th>
-				<td>
-					<label>
-						<input type="checkbox" name="deep_lookup" value="1" checked="true"> Lookup all components for all sub-modules.
-					</label>
+<h1>Detailed Part List Lookup</h1>
+<form action="/uniqueparts" method="post">
+	<table width="100%">
+		<tr>
+			<th width="10%">Use?</th>
+			<th width="75%">Module</th>
+			<th width="15%">Quantity</th>
+		</tr>
+		<? foreach ($modules AS $row): ?>
+			<? $module = $row['UniquePart']; ?>
+			<tr class="module_row" id="module_<?=$module->id?>">
+				<td align="center">
+					<input type="checkbox" id="module_check_<?=$module->id?>" name="use_module[<?=$module->id?>]" value="1" onclick="selectModule(<?=$module->id?>)"/>
 				</td>
+				<td>
+					<input type="hidden" name="module_id[<?=$module->id?>]" value="<?=$module->id?>" />
+					<b><a href="/uniquepart:<?=$module->id?>"><?=$module->get('name')?></a></b>
+					<img src="/img/help-icon.gif" onclick="Element.toggle('description_<?=$module->id?>')">
+				</td>
+				<td align="center"><input type="text" name="quantities[<?=$module->id?>]" value="1" size="3"></td>
 			</tr>
-			<tr>
+			<tr id="description_<?=$module->id?>" style="display: none">
 				<td>&nbsp;</td>
-				<td><input type="submit" name="submit" value="Next Step" /></td>
+				<td>
+					<?=$module->get('description')?>
+				</td>
+				<td>&nbsp;</td>
 			</tr>
-		</table>
-	</form>
+		<? endforeach ?>
+		<tr>
+			<th>Deep Lookup?</th>
+			<td>
+				<label>
+					<input type="checkbox" name="deep_lookup" value="1" checked="true"> Lookup all components for all sub-modules.
+				</label>
+			</td>
+		</tr>
+		<tr>
+			<td>&nbsp;</td>
+			<td><input type="submit" name="submit" value="Next Step" /></td>
+		</tr>
+	</table>
 </form>
--- reprap/web/hoekens-bom/views/main.uniqueparts.php
+++ reprap/web/hoekens-bom/views/main.uniqueparts.php
@@ -1,133 +1,138 @@
-<script type="text/javascript">
-	function generateShoppingList()
-	{
-		$('shopping_list').innerHTML = 'Loading...';
-
-		var params = Form.serialize($('bom_form'));
-		new Ajax.Updater('shopping_list', '/partlist', {
-			parameters: params,
-			method: 'post'
-		});
-	}
-	
-	function toggleBreakdown(id)
-	{
-		Element.toggle('breakdown_' + id);
-		Element.toggle('plus_' + id);
-		Element.toggle('minus_' + id);
-	}
-</script>
-<? if ($module): ?>
-	<h1><a href="/uniquepart:<?=$module->id?>"><?=$module->get('name')?></a> - <?=date("M j, Y")?></h1>
+<? if ($error): ?>
+	<h1>Error:</h1>
+	<h3 style="font-color: red"><?=$error?></h3>
 <? else: ?>
-	<h1>Custom Part List - <?=date("M j, Y")?></h1>
-	<table>
-		<tr>
-			<td colspan="2"><b>Modules used:</b></td>
-		</tr>
-		<? foreach ($modules AS $module): ?>
+	<script type="text/javascript">
+		function generateShoppingList()
+		{
+			$('shopping_list').innerHTML = 'Loading...';
+
+			var params = Form.serialize($('bom_form'));
+			new Ajax.Updater('shopping_list', '/partlist', {
+				parameters: params,
+				method: 'post'
+			});
+		}
+	
+		function toggleBreakdown(id)
+		{
+			Element.toggle('breakdown_' + id);
+			Element.toggle('plus_' + id);
+			Element.toggle('minus_' + id);
+		}
+	</script>
+	<? if ($module): ?>
+		<h1><a href="/uniquepart:<?=$module->id?>"><?=$module->get('name')?></a> - <?=date("M j, Y")?></h1>
+	<? else: ?>
+		<h1>Custom Part List - <?=date("M j, Y")?></h1>
+		<table>
 			<tr>
-				<td><b><a href="/uniquepart:<?=$module->id?>"><?=$module->get('name')?></a></b></td>
-				<td>x <?=$quantities[$module->id]?></td>
+				<td colspan="2"><b>Modules used:</b></td>
 			</tr>
-		<? endforeach ?>
-	</table>
-	<br/>
-<? endif ?>
-<em>
-	<strong>Note:</strong> This list is reasonably accurate, but it is not guaranteed.  Please review it before you buy anything.<br/>
-	If you do happen to find an error, please report it in the <a href="http://forums.reprap.org">forums</a>.
-</em>
-<br/>
-<br/>
-<? if (!empty($list->uniques)): ?>
-
-	<?= Controller::byName('main')->renderView('global_suppliers', array('suppliers' => $list->getSupplierList())); ?>
-
-	<form onsubmit="return false;" method="POST" id="bom_form">
-		<input type="hidden" name="module_id" value="<?=$module->id?>" />
-		<table width="100%">
-			<? foreach ($list->type_list AS $type => $data): ?>
-				<? $data = $list->getTypeList($type)->getAll(); ?>
-				<? foreach ($data AS $row): ?>
-					<? $unique = $row['UniquePart']; ?>
-					<? $quantity = $list->getUniqueQuantity($unique->id); ?>
-					<? $type = $unique->get('type'); ?>
-					<? if ($unique->get('type') != $old_type): ?>
-						<tr>
-							<td colspan="3">
-								<br/>
-								<span style="font-size: 23px; margin: 15 0 5 0"><?=UniquePart::typeToEnglish($type)?></a></span>
-								<a href="/type/<?=$type?>">view all</a>
-							</td>
-						</tr>
-					<? endif ?>
-					<? $old_type = $type; ?>
-					<? if ($quantity): ?>
-						<tr>
-							<td>
-								<img src="/img/plus-icon.gif" id="plus_<?=$unique->id?>" onclick="toggleBreakdown(<?=$unique->id?>)" style="cursor: pointer;"/>
-								<img src="/img/minus-icon.gif" id="minus_<?=$unique->id?>" onclick="toggleBreakdown(<?=$unique->id?>)" style="cursor: pointer; display: none"/>
-								<input type="checkbox" id="use_<?=$unique->id?>" name="use_part[<?=$unique->id?>]" value="1" checked="true"/>
-								<a href="/uniquepart:<?=$unique->id?>"><?=$unique->get('name')?></a>
-								<img src="/img/help-icon.gif" onclick="Element.toggle('tooltip_<?=$unique->id?>')" style="cursor: pointer;">
-								<div id="tooltip_<?=$unique->id?>" class="tooltip" style="display: none"><?=$unique->get('description')?></div>
-							</td>
-							<td>
-								<input type="text" name="quantity[<?=$unique->id?>]" value="<?=$quantity?>" size="3">
-								<?= $unique->get('units') ?>
-							</td>
-							<td><?= Controller::byName('main')->renderView('part_suppliers', array('part' => $unique))?></td>
-						</tr>
-						<tr id="breakdown_<?=$unique->id?>" class="breakdown" style="display: none;">
-							<td colspan="4">
-								<div style="margin-left: 20px">
-									<table>
-										<tr>
-											<td><b>Part</b></td>
-											<td><b>Quantity</b></td>
-										</tr>
-										<? $raws = array(); ?>
-										<? foreach ($list->raw_list[$unique->id] AS $part): ?>
-											<?
-												$parent = new RawPart($part->get('parent_id'));
-												if ($parent->get('type') == 'assembly')
-												{
-													$assembly = $parent;
-													$parent = new RawPart($assembly->get('parent_id'));
-												}
-												else
-												{
-													$grandparent = new RawPart($parent->get('parent_id'));
-													$assembly = false;
-												}
-											?>
-											<tr>
-												<td><?=$part->get('raw_text')?> <span style="font-size: 90%">(from 
-													<? if ($assembly instanceOf RawPart): ?>
-														<a href="/uniquepart:<?=$parent->get('part_id')?>"><?=$parent->get('raw_text')?></a> / <?=$assembly->get('raw_text')?>)</span>
-													<? else: ?>
-														<!-- <a href="/uniquepart:<?=$grandparent->get('part_id')?>"><?=$grandparent->get('raw_text')?></a> / -->
-														<a href="/uniquepart:<?=$parent->get('part_id')?>"><?=$parent->get('raw_text')?></a>)</span>
-													<? endif ?>
-												</td>
-												<td><?=$part->get('quantity')?></td>
-											</tr>
-										<? endforeach ?>
-									</table>
-								</div>
-							</td>
-						</tr>
-					<? endif ?>
-				<? endforeach ?>
+			<? foreach ($modules AS $module): ?>
+				<tr>
+					<td><b><a href="/uniquepart:<?=$module->id?>"><?=$module->get('name')?></a></b></td>
+					<td>x <?=$quantities[$module->id]?></td>
+				</tr>
 			<? endforeach ?>
 		</table>
 		<br/>
-		<input type="button" style="font-size: 22px" value="Generate Shopping List" onclick="generateShoppingList()"/>
-	</form>
+	<? endif ?>
+	<em>
+		<strong>Note:</strong> This list is reasonably accurate, but it is not guaranteed.  Please review it before you buy anything.<br/>
+		If you do happen to find an error, please report it in the <a href="http://forums.reprap.org">forums</a>.
+	</em>
 	<br/>
+	<br/>
+	<? if (!empty($list->uniques)): ?>
+
+		<?= Controller::byName('main')->renderView('global_suppliers', array('suppliers' => $list->getSupplierList())); ?>
+
+		<form onsubmit="return false;" method="POST" id="bom_form">
+			<input type="hidden" name="module_id" value="<?=$module->id?>" />
+			<table width="100%">
+				<? foreach ($list->type_list AS $type => $data): ?>
+					<? $data = $list->getTypeList($type)->getAll(); ?>
+					<? foreach ($data AS $row): ?>
+						<? $unique = $row['UniquePart']; ?>
+						<? $quantity = $list->getUniqueQuantity($unique->id); ?>
+						<? $type = $unique->get('type'); ?>
+						<? if ($unique->get('type') != $old_type): ?>
+							<tr>
+								<td colspan="3">
+									<br/>
+									<span style="font-size: 23px; margin: 15 0 5 0"><?=UniquePart::typeToEnglish($type)?></a></span>
+									<a href="/type/<?=$type?>">view all</a>
+								</td>
+							</tr>
+						<? endif ?>
+						<? $old_type = $type; ?>
+						<? if ($quantity): ?>
+							<tr>
+								<td>
+									<img src="/img/plus-icon.gif" id="plus_<?=$unique->id?>" onclick="toggleBreakdown(<?=$unique->id?>)" style="cursor: pointer;"/>
+									<img src="/img/minus-icon.gif" id="minus_<?=$unique->id?>" onclick="toggleBreakdown(<?=$unique->id?>)" style="cursor: pointer; display: none"/>
+									<input type="checkbox" id="use_<?=$unique->id?>" name="use_part[<?=$unique->id?>]" value="1" checked="true"/>
+									<a href="/uniquepart:<?=$unique->id?>"><?=$unique->get('name')?></a>
+									<img src="/img/help-icon.gif" onclick="Element.toggle('tooltip_<?=$unique->id?>')" style="cursor: pointer;">
+									<div id="tooltip_<?=$unique->id?>" class="tooltip" style="display: none"><?=$unique->get('description')?></div>
+								</td>
+								<td>
+									<input type="text" name="quantity[<?=$unique->id?>]" value="<?=$quantity?>" size="3">
+									<?= $unique->get('units') ?>
+								</td>
+								<td><?= Controller::byName('main')->renderView('part_suppliers', array('part' => $unique))?></td>
+							</tr>
+							<tr id="breakdown_<?=$unique->id?>" class="breakdown" style="display: none;">
+								<td colspan="4">
+									<div style="margin-left: 20px">
+										<table>
+											<tr>
+												<td><b>Part</b></td>
+												<td><b>Quantity</b></td>
+											</tr>
+											<? $raws = array(); ?>
+											<? foreach ($list->raw_list[$unique->id] AS $part): ?>
+												<?
+													$parent = new RawPart($part->get('parent_id'));
+													if ($parent->get('type') == 'assembly')
+													{
+														$assembly = $parent;
+														$parent = new RawPart($assembly->get('parent_id'));
+													}
+													else
+													{
+														$grandparent = new RawPart($parent->get('parent_id'));
+														$assembly = false;
+													}
+												?>
+												<tr>
+													<td><?=$part->get('raw_text')?> <span style="font-size: 90%">(from 
+														<? if ($assembly instanceOf RawPart): ?>
+															<a href="/uniquepart:<?=$parent->get('part_id')?>"><?=$parent->get('raw_text')?></a> / <?=$assembly->get('raw_text')?>)</span>
+														<? else: ?>
+															<!-- <a href="/uniquepart:<?=$grandparent->get('part_id')?>"><?=$grandparent->get('raw_text')?></a> / -->
+															<a href="/uniquepart:<?=$parent->get('part_id')?>"><?=$parent->get('raw_text')?></a>)</span>
+														<? endif ?>
+													</td>
+													<td><?=$part->get('quantity')?></td>
+												</tr>
+											<? endforeach ?>
+										</table>
+									</div>
+								</td>
+							</tr>
+						<? endif ?>
+					<? endforeach ?>
+				<? endforeach ?>
+			</table>
+			<br/>
+			<input type="button" style="font-size: 22px" value="Generate Shopping List" onclick="generateShoppingList()"/>
+		</form>
+		<br/>
 	
-	<div id="shopping_list"></div>
-<? else: ?>
-	<b>No components found!</b>
-<? endif?>
+		<div id="shopping_list"></div>
+	<? else: ?>
+		<b>No components found!</b>
+	<? endif?>
+<? endif ?>
