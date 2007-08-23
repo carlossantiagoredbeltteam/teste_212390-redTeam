0c0df872013bb3b853effdae1b8e3b96c4e37a15
hoekstar <hoekstar@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2007-08-23 17:13:46 +0000

    made various pages look a bit better.  still needs real styles though.

--- trunk/reprap/web/part-lister/views/parts.all.php
+++ trunk/reprap/web/part-lister/views/parts.all.php
@@ -1,15 +1,25 @@
 <h1>Parts</h1>
 <? if (!empty($parts)): ?>
-	<? foreach ($parts AS $row): ?>
-		<? $part = $row['UniquePart'] ?>
-		<? if ($part->get('type') != $type): ?>
-			<? $type = $part->get('type'); ?>
-			<h2><?=UniquePart::typeToEnglish($type)?> Parts (<a href="/type/<?=$type?>">see all</a>)</h2>
-		<? endif ?>
-		<p>
-			<b><a href="<?=$part->getViewUrl()?>"><?=$part->get('name')?></a></b> <?=$part->get('description')?>
-		</p>
-	<? endforeach?>
+		<? foreach ($parts AS $row): ?>
+			<? $part = $row['UniquePart'] ?>
+			<? if ($part->get('type') != $type): ?>
+				<? if ($type): ?>
+					</table>
+				<? endif ?>
+				<? $type = $part->get('type'); ?>
+				<h2><?=UniquePart::typeToEnglish($type)?> Parts (<a href="/type/<?=$type?>">see all</a>)</h2>
+				<table width="100%">
+					<tr>
+						<th width="33%">Part</th>
+						<th>Description</th>
+					</tr>
+			<? endif ?>
+			<tr>
+				<td><b><a href="<?=$part->getViewUrl()?>"><?=$part->get('name')?></a></b></td>
+				<td><?=$part->get('description')?></td>
+			</tr>
+		<? endforeach?>
+	</table>
 <? else: ?>
 	<b>No parts found.</b>
 <? endif ?>
--- trunk/reprap/web/part-lister/views/supplier.all.php
+++ trunk/reprap/web/part-lister/views/supplier.all.php
@@ -1,11 +1,18 @@
 <h1>Suppliers</h1>
 <? if (!empty($suppliers)): ?>
-	<? foreach ($suppliers AS $row): ?>
-		<? $supplier = $row['Supplier'] ?>
-		<p>
-			<b><a href="<?=$supplier->getViewUrl()?>"><?=$supplier->get('name')?></a></b> <?=$supplier->get('description')?>
-		</p>
-	<? endforeach ?>
+	<table width="100%">
+		<tr>
+			<th width="33%">Name</th>
+			<th>Info</th>
+		</tr>
+		<? foreach ($suppliers AS $row): ?>
+			<? $supplier = $row['Supplier'] ?>
+			<tr>
+				<td><b><a href="<?=$supplier->getViewUrl()?>"><?=$supplier->get('name')?></a></b></td>
+				<td><?=$supplier->get('description')?></td>
+			</p>
+		<? endforeach ?>
+		</table>
 <? else: ?>
 	<b>No suppliers found.</b>
 <? endif ?>
