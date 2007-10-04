8fed70973d5b1ce3360e7a4c036321fed39cd76b
hoekstar <hoekstar@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2007-10-04 19:59:06 +0000

    added my fader code to my arduino library.

---
+++ users/hoeken/arduino-code/fader/fader.cpp
@@ -0,0 +1,44 @@
+
+#include "WProgram.h"
+#include "Fader.h"
+
+void function fadeUp(int pin, int delayTime)
+{
+	fadeTo(pin, 0, 255, delayTime);
+}
+
+void function fadeDown(int pin, int delayTime)
+{
+	fadeTo(pin, 255, 0, delayTime);
+}
+
+void function fadeTo(int pin, int start, int stop, int delayTime)
+{
+	int i;
+	
+	//do some error checking to make sure its within bounds
+	start = max(start, 0);
+	start = min(start, 255);
+	stop = max(stop, 0);
+	stop = min(stop, 255);
+	
+	//okay, fade up!
+	if (start < stop)
+	{
+		for (i=start; i<=stop; i++)
+		{
+			analogWrite(pin, i);
+			delay(delayTime);
+		}
+	}
+	//nope, fade down!
+	else if (stop < start)
+	{
+		for (i=start; i>=stop; i--)
+		{
+			analogWrite(pin, i);
+			delay(delayTime);
+		}
+	}
+	//otherwise, they're equal and we just fall through.
+}
