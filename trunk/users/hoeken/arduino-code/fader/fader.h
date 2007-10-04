acd6b66004428e100e11e0d6d53878999d644030
hoekstar <hoekstar@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2007-10-04 19:59:06 +0000

    added my fader code to my arduino library.

---
+++ trunk/users/hoeken/arduino-code/fader/fader.h
@@ -0,0 +1,6 @@
+#ifndef Fader_h
+#define Fader_h
+
+void function fadeUp(int pin, int delayTime);
+void function fadeDown(int pin, int delayTime);
+void function fadeTo(int pin, int start, int stop, int delayTime);
