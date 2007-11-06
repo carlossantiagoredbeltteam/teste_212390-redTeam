81ab3f0d1d855911f04a9041d868571adca4a462
hoekstar <hoekstar@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2007-11-06 04:20:25 +0000

    yay!!!  i got non blocking stepper motor control going!!!!

--- trunk/users/hoeken/arduino/library/RepStepper/RepStepper.cpp
+++ trunk/users/hoeken/arduino/library/RepStepper/RepStepper.cpp
@@ -11,7 +11,7 @@ RepStepper::RepStepper(int number_of_steps, int step_pin, int direction_pin)
 	this->direction = 1;
 	this->setSpeed(0);
 	this->last_step_time = 0;
-	this->next_step_time = 0;
+	this->next_step_time = 1;
 
 	//get our parameters
 	this->number_of_steps = number_of_steps;
@@ -31,7 +31,7 @@ void RepStepper::setSpeed(int speed)
 	this->step_delay = speed;
 	
 	if (this->step_delay > 0);
-		this->rpm = 15000000L / (this->step_delay * this->number_of_steps);
+		this->rpm = 15000000UL / (this->step_delay * this->number_of_steps);
 }
 
 /*
@@ -47,7 +47,7 @@ int RepStepper::getSpeed()
 */
 void RepStepper::setRPM(int rpm)
 {
-	if (rpm <= 0)
+	if (rpm == 0)
 	{
 		this->step_delay = 0;
 		this->rpm = 0;
@@ -58,7 +58,7 @@ void RepStepper::setRPM(int rpm)
 		
 		//our high precision time is measured in 4us ticks, so get the # of ticks in 1 minute / 
 		// number of steps per rev / number of revolutions per minute = ticks per step
-		this->step_delay = 15000000L / this->number_of_steps / this->rpm;
+		this->step_delay = (15000000UL / this->number_of_steps) / this->rpm;
 	}
 }
 
@@ -86,28 +86,32 @@ void RepStepper::setDirection(bool direction)
 	digitalWrite(this->direction_pin, this->direction);
 }
 
+bool RepStepper::getDirection()
+{
+	return this->direction;
+}
+
 bool RepStepper::canStep()
 {
 	//bail if we have no speed.
 	if (!this->step_delay)
 		return false;
 	
-	unsigned long now = this->hpticks();
+	this->now = this->hpticks();
 	
 	//if its like this, its normal.
-	if (last_step_time <= next_step_time)
+	if (this->last_step_time < this->next_step_time)
 	{
-		if (now >= this->next_step_time);
+		if (this->now > this->next_step_time)
 			return true;
 	}
 	//nope, we're probably dealing with overflow.
 	else
 	{
 		//is the time since our last step bigger than our delay?
-		if ((4294967295L - this->last_step_time + now) >= this->step_delay)
+		if ((4294967295UL - this->last_step_time + this->now) > this->step_delay)
 			return true;
-	}
-	
+	}	
 	//okay, nobody said they're ready, bail.
 	return false;
 }
@@ -119,7 +123,21 @@ void RepStepper::step()
 	
 	//calculate our next step.
 	this->last_step_time = this->hpticks();
-	this->next_step_time = this->last_step_time + $this->step_delay;
+	this->next_step_time = this->last_step_time + this->step_delay;
+}
+
+void RepStepper::blockingStep(int steps)
+{
+	int i;
+	
+	for (i=0; i<steps; i++)
+	{
+		//this->step();
+		digitalWrite(this->step_pin, HIGH);
+		digitalWrite(this->step_pin, LOW);
+		
+		delayMicroseconds(this->step_delay << 2);
+	}
 }
 
 int RepStepper::version(void)
@@ -127,7 +145,7 @@ int RepStepper::version(void)
   return 1;
 }
 
-unsigned long RepStepper::hpticks()
+unsigned long RepStepper::hpticks(void)
 {
 	return (timer0_overflow_count << 8) + TCNT0;
 }
--- trunk/users/hoeken/arduino/library/RepStepper/RepStepper.h
+++ trunk/users/hoeken/arduino/library/RepStepper/RepStepper.h
@@ -15,6 +15,9 @@
 
 #include "WConstants.h"
 
+extern volatile unsigned long timer0_overflow_count;
+
+
 #define RS_FORWARD 1
 #define RS_REVERSE 0
 
@@ -38,6 +41,7 @@ class RepStepper {
 	
     // mover method:
     void step();
+	void blockingStep(int steps);
 
 	// info stuff
 	bool canStep();
@@ -45,23 +49,23 @@ class RepStepper {
 	//random other functions
     int version();
 
+	//our time counter variables.
+	unsigned long last_step_time;			// time stamp in ticks of when our last step was.
+    unsigned long next_step_time;			// time stamp in ticks of when the next step can be.
+    unsigned long now;						// the 'current' timestamp in ticks of right now.
+	unsigned long hpticks(void);
+
   private:
 
-	unsigned int micros();
-    
 	//various internal variables
     bool direction;				// Direction of rotation
-	int rpm;					// Speed in RPMs
-	int step_delay;   			// delay between steps, in microseconds, based on speed
-    int number_of_steps;		// total number of steps this motor can take
+	unsigned int rpm;					// Speed in RPMs
+	unsigned int step_delay;   // delay between steps, in microseconds, based on speed
+    unsigned int number_of_steps;		// total number of steps this motor can take
 	
     // motor pin numbers:
     int step_pin;				//the step signal pin.
     int direction_pin;			//the direction pin.
-    
-	//our time counter variables.
-	unsigned long last_step_time;			// time stamp in ticks of when our last step was.
-    unsigned long next_step_time;			// time stamp in ticks of when the next step can be.
 };
 
 #endif
