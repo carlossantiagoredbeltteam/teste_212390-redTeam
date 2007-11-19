535a3a4a8ae9e381e5314d590f81d12de441d5fa
hoekstar <hoekstar@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2007-11-19 05:20:48 +0000

    re-ported the snap library from the main firmware trunk.

--- users/hoeken/arduino/library/SNAP/SNAP.cpp
+++ users/hoeken/arduino/library/SNAP/SNAP.cpp
@@ -1,198 +1,55 @@
-/*
- *
- * RepRap, The Replicating Rapid Prototyper Project
- *
- * http://reprap.org/
- *
- * RepRap is copyright (C) 2005-6 University of Bath, the RepRap
- * researchers (see the project's People webpage), and other contributors.
- *
- * RepRap is free; you can redistribute it and/or modify it under the
- * terms of the GNU Library General Public Licence as published by the
- * Free Software Foundation; either version 2 of the Licence, or (at your
- * option) any later version.
- *
- * RepRap is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
- * Licence for more details.
- *
- * For this purpose the words "software" and "library" in the GNU Library
- * General Public Licence are taken to mean any and all computer programs
- * computer files data results documents and other copyright information
- * available from the RepRap project.
- *
- * You should have received a copy of the GNU Library General Public
- * Licence along with RepRap (in reports, it will be one of the
- * appendices, for example); if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA, or see
- *
- * http://www.gnu.org/
- *
- */
 
-
-/**
- * Implementation of interrupt driven SNAP communications routines for
- * use in a token ring configuration.  Being completely interrupt
- * driven improves performance and simplifies development of the core
- * logic in each device.  This is not really a true token ring in that
- * there is no token frame and the procedure for ring insertion etc.
- * is trivial -- it is however a network with ring topology.
- *
- * Overview/notes:
- *
- * A receive buffer accepts payload data as it arrives.  Upon
- * completion, a global flag is set that acts as a lock to prevent
- * further receives occuring until the lock is removed.  If a receive
- * does occur, but it is for somebody else, it is passed onto the next
- * node in the loop.  If the receive is for ourself then we fail and
- * NAK the packet so that it will be re-sent at a time we can
- * hopefully act on it.
- *
- * The lock flag also indicates to the main loop that data is awaiting
- * and the main loop is responsible for calling any processing on the
- * data.  It is not called directly by the ISR to prevent re-entrancy
- * problems.  The act of receiving the byte will wake the CPU and
- * allow it to check for the present of the lock.  After processing is
- * complete it may sleep if it wishes.  It will be woken after every
- * byte is received but can just repeatedly sleep again if it likes.
- *
- * The main loop that is acting on the lock flag must process the
- * command and send any necessary data.  It must also wait for an ACK
- * or NAK before finally removing the lock and allowing further
- * receives.
- *
- * When sending any packet (including ACK or NAK packets), a timeout
- * is started.  If the timeout expires, the response is considered to
- * be a NAK and the ACK/NAK is resent.  The timeout should be generous
- * enough to allow for full ring propagation with worst-case delays.
- * If the packets comes back to the sender, it is also treated as a
- * NAK.  An error counter should limit the number of re-sends before
- * dropping the packet and returning an error.
- *
- * When data is received, only the payload is available to the main
- * loop.  A copy of the source address is also saved until the lock is
- * released.  This allows replies to be sent regardless of other
- * packets received or forwarded during processing.
- *
- * For the moment, data packets will not have ACK/NAK piggybacked
- * with them and each will be sent separately.  This is because
- * ACKs are automatically and immediately sent by the ISR routines
- * before a response is even computed.
- *
- * @todo NAKing a packet while busy should ideally send a special NAK
- * that indicates busy as opposed to failed CRC, etc.  This would allow
- * a small pause before re-sending, rather than resending immediately
- * and probably causing the same problem again.
- *
- * General API:
- *
- * - Main loop inspects processingLock flag.  If set, it actions the data
- *   in buffer.
- *
- * - A reply is optionally constructed by calling sendReply.  This uses
- *   the saved source address to send appropriate header bytes.  Nothing
- *   much happens here because the header can't be constructed until
- *   the packet is complete (length is unknown).
- *
- * - Packet payload is sent by repeatedly calling sendDataByte
- *
- * - The sending is completed by calling endMessage, which will
- *   send the actual packet by constructing a header, length, body and
- *   CRC for the message.
- *
- * - awaitDelivery is called to wait for a response. A duplicate of
- *   the entire packet is kept in an additional buffer so that if a NAK
- *   arrives the same data can be re-sent without bothering the client.
- *   This method should do very little as the handling of this is
- *   interrupt driven.  If called, it will block until the delivery is
- *   complete and return fail/success.
- *
- * - deliveryStatus returns the same information as awaitDelivery
- *   (except tristate values indicating still sending, success,
- *   failure).  This does not block however.
- *
- * - When sending a new message rather than a reply, the sendMessage
- *   function is called with the destination address.
- *
- * - Call releaseLock to indicate processing is complete amd allow
- *   any necessary cleanups.  If no ACK is received yet, this
- *   will block until it arrives.  If endMessage is not called,
- *   the packet is dropped.
- *
- * In order for the routines to work the ISR must call the interrupt
- * handler serialInterruptHandler()
- *
- * When we get a packet not destined for us or with headers we don't
- * understand, we just pass them on.  In theory, a corrupt packet
- * could therefore just be passed on by everybody, forever.  To get
- * around this we could buffer the packet and check the CRC, then only
- * send it on if all is well.  However in doing so we greatly increase
- * the latency.  To prevent the possible long-term buildup of rogue
- * packets it is assumed that there is a node in the ring (such as a
- * more powerful PC) that will check things more thoroughly and mop up
- * any problem packets that are cycling the network.  By only having
- * one such node in the network, the latency effects are minimised.
- *
- * @todo An enhancement that may be needed is something to deal with
- * too much data arriving.  eg if fully occupied with incoming data
- * and a local transmit is occasionally needed, eventually
- * transmitting will block while waiting for the TSR to become free
- * (it won't be able to contain all the outgoing data).  This will
- * mean received data is lost and the packet will become corrupted.
- * However at least the next packet will also become corrupted.  This
- * situation should be detected and if anything arrives during
- * blocking transmits, they should be cleanly dropped up until the
- * packet ending.  This improvement just decreases the number of lost
- * packets, but is a little complex so it may or may not be worth
- * doing.  Also in most cases for a local transmit to be needed, there
- * would also be a command received, which would be consumed leaving
- * more buffer space.  Also responses from slave devices are not
- * expected to overwhelm the network so badly.
- *
- *
- * Problems with SNAP:
- *
- * Error correction is optional.  That means the flag itself could be
- * corrupted and no error correction will take place.  It should
- * be mandatory and cover the header.
- *
- * The destination address should occur sooner so packets can be
- * passed on in the network as soon as possible to decrease latency (only
- * relevant in a token ring situation).
- *
- * The lengths are not continuous up to the sizes we want.
- *
- * A lot of the other stuff is superfluous.
- *
- * An ARP protocol like SMBus has might be nice.
- *
- */
-
-/// @bug There are problems with addresses 3,9,17,19,26 and 28 for
-/// some currently unknown reason.  If you send a message to one of
-/// these addresses, it will not be chained on correctly.
 
 #include "SNAP.h"
 #include "WConstants.h"
 
 SNAP::SNAP(void)
 {
-	uartState = SNAP_idle;
+	packetState = SNAP_idle;
 	processingLock = false;
 	deviceCount = 0;
+	serialStatus = 0;
+	crc = 0;
+}
+
+void SNAP::receiveError()
+{
+	byte i;
+
+	if (serialStatus & msgAbortedBit == 0)
+	{
+		//wipe the corrupt-message out of the receive-buffers of the nodes
+		for (i=0; i<8; i++)
+		{
+			//if we are sending too much for the transmit-buffer, it is discarded
+			this->transmit(0);
+		}
+	}
+	
+	//clear all bits except msgAbortedBit;
+	serialStatus = serialStatus & msgAbortedBit;
+	
+	packetState = SNAP_idle;
 }
 
 void SNAP::receiveByte(byte c)
 {
-	switch(this->uartState)
+	if (serialStatus & serialErrorBit)
+	{
+		this->receiveError();
+		return;
+	}
+  
+	switch(packetState)
 	{
 		case SNAP_idle:
-			// In the idle state, we wait for a sync byte.  If none is
-			// received, we remain in this state.
-			if (c == SNAP_SYNC)
-				uartState = SNAP_haveSync;
+		    // In the idle state, we wait for a sync byte.  If none is
+		    // received, we remain in this state.
+		    if (c == SNAP_SYNC)
+			{
+		      packetState = SNAP_haveSync;
+		      serialStatus &= ~msgAbortedBit; //clear
+		    }
 			break;
 
 		case SNAP_haveSync:
@@ -204,45 +61,45 @@ void SNAP::receiveByte(byte c)
 			in_hdb2 = c;
 			if ((c & B11111100) != B01010000)
 			{
-				// Unsupported header.  Just pass it on to the next node.
-				// We assume there are 1 byte addresses still, for simplicity.
-				this->transmit(SNAP_SYNC);
-				this->transmit(c);
-				uartState = SNAP_haveHDB2Pass;
+				// Unsupported header.  Drop it an reset
+				serialStatus |= serialErrorBit;  //set serialError
+				serialStatus |= wrongByteErrorBit; 
+				this->receiveError();
 			}
 			else
 			{
 				// All is well
 				if ((c & B00000011) == B00000001)
-					ackRequested = 1;
+					serialStatus |= ackRequestedBit;  //set ackRequested-Bit
 				else
-					ackRequested = 0;
+					serialStatus &= ~ackRequestedBit; //clear
 
 				crc = 0;
 				this->computeCRC(c);
-				uartState = SNAP_haveHDB2;
+
+				packetState = SNAP_haveHDB2;
 			}
 			break;
 
 		case SNAP_haveHDB2:
-			// For HDB1, we insist CMD = 0, EDM = 8-bit CRC.
+			// For HDB1, we insist on high bits are 0011 and low bits are the length 
+			//   of the payload.
 			in_hdb1 = c;
 			if ((c & B11110000) != B00110000)
 			{
-				// Bail out
-				this->transmit(SNAP_SYNC);
-				this->transmit(in_hdb2);
-				this->transmit(c);
-				uartState = SNAP_haveHDB1Pass;
+				serialStatus |= serialErrorBit;  //set serialError
+				serialStatus |= wrongByteErrorBit; 
+				this->receiveError();
 			}
 			else
 			{
 				packetLength = c & 0x0f;
-				if (packetLength & 8)
-					packetLength = 8 << (c & 7);
+				if (packetLength > RX_BUFFER_SIZE)
+					packetLength = RX_BUFFER_SIZE;
 				
 				this->computeCRC(c);
-				uartState = SNAP_haveHDB1;
+				
+				packetState = SNAP_haveHDB1;
 			}
 			break;
 
@@ -254,33 +111,66 @@ void SNAP::receiveByte(byte c)
 				this->transmit(in_hdb2);
 				this->transmit(in_hdb1);
 				this->transmit(c);
-				uartState = SNAP_haveDABPass;
+				packetState = SNAP_haveDABPass;
+				serialStatus &= ~ackRequestedBit; //clear
+				serialStatus |= inTransmitMsgBit; 
 			}
 			else
 			{
-				this->computeCRC(c);
-				uartState = SNAP_haveDAB;
+				//save our address, as we may have multiple addresses on one arduino.
 				destAddress = c;
+				
+				this->computeCRC(c);
+				packetState = SNAP_haveDAB;
 			}
 			break;
 
 		case SNAP_haveDAB:
-			sourceAddress = c;
-			rxBufferIndex = 0;
-			this->computeCRC(c);
-			uartState = SNAP_readingData;
+			// We should be reading the source address now
+			if (this->hasDevice(c))
+			{
+				// If we receive a packet from ourselves, that means it went
+				// around the ring and was never picked up, ie the device we
+				// sent to is off-line or unavailable.
+
+				/// @todo Deal with this situation
+			}
+			
+			if (serialStatus & processingLockBit)
+			{
+				//we have not finished the last order, reject
+				this->transmit(SNAP_SYNC);
+				crc = 0;
+				
+				this->transmit(computeCRC(B01010011));	//HDB2
+				this->transmit(computeCRC(B00110000));	// HDB1: 0 bytes, with 8 bit CRC
+				this->transmit(computeCRC(sourceAddress));	// Return to sender
+				this->transmit(computeCRC(destAddress));	// From us
+				this->transmit(crc);  // CRC
+
+				serialStatus &= ~ackRequestedBit; //clear
+				serialStatus |= msgAbortedBit; //set
+
+				packetState = SNAP_idle;
+			}
+			else
+			{
+				sourceAddress = c;
+				rxBufferIndex = 0;
+				this->computeCRC(c);
+
+				packetState = SNAP_readingData;
+			}
 			break;
 
 		case SNAP_readingData:
-			if (!processingLock)
-				rxBuffer[rxBufferIndex] = c;
-
+			rxBuffer[rxBufferIndex] = c;
 			rxBufferIndex++;
 
 			this->computeCRC(c);
 
 			if (rxBufferIndex == packetLength)
-				uartState = SNAP_dataComplete;
+				packetState = SNAP_dataComplete;
 			break;
 
 		case SNAP_dataComplete:
@@ -298,62 +188,56 @@ void SNAP::receiveByte(byte c)
 					// comms processing (such as passing other tokens around the
 					// ring) while we're actioning the command.
 
-					if (processingLock)
-					{
-						// If we're already processing, NAK and ignore
-						hdb2 |= B00000011;
-					}
-					else
-					{
-						// Otherwise ACK and set the flag to allow processing to start
-						hdb2 |= B00000010;
-						processingLock = true;
-						receivedSourceAddress = sourceAddress;
-					}
+					hdb2 |= B00000010;
+					serialStatus |= processingLockBit;  //set processingLockBit
+					receivedSourceAddress = sourceAddress;
 				}
 				else
 				{
 					// CRC mismatch, so we will NAK the packet
 					hdb2 |= B00000011;
 				}
-
-				if (ackRequested)
+				
+				if (serialStatus & ackRequestedBit)
 				{
 					// Send ACK or NAK back to source
 					this->transmit(SNAP_SYNC);
 					crc = 0;
-					this->transmit(computeCRC(hdb2));
-					// HDB1: 0 bytes, with 8 bit CRC
-					this->transmit(computeCRC(B00110000));
-					this->transmit(computeCRC(sourceAddress));  // Return to sender
-					this->transmit(computeCRC(destAddress));  // From us
-					this->transmit(crc);  // CRC
+					this->transmit(this->computeCRC(hdb2));
+					this->transmit(this->computeCRC(B00110000));	// HDB1: 0 bytes, with 8 bit CRC
+					this->transmit(this->computeCRC(sourceAddress));	// Return to sender
+					this->transmit(this->computeCRC(destAddress));	// From us
+					this->transmit(crc);  						// CRC
+					serialStatus &= ~ackRequestedBit;			//clear
 				}
 			}
-			uartState = SNAP_idle;
+			
+			packetState = SNAP_idle;
 			break;
 
 		case SNAP_haveHDB2Pass:
-			this->transmit(c);  // We will be reading HDB1; pass it on
-			packetLength = c & 0x0f;
-			if (packetLength & 8)
-				packetLength = 8 << (c & 7);
-			uartState = SNAP_haveHDB1Pass;
+		    this->transmit(c);  // We will be reading HDB1; pass it on
+
+		    packetLength = c & 0x0f;
+		    if (packetLength > RX_BUFFER_SIZE)
+		      packetLength = RX_BUFFER_SIZE;
+
+		    packetState = SNAP_haveHDB1Pass;
 			break;
 
 		case SNAP_haveHDB1Pass:
-			this->transmit(c);  // We will be reading dest addr; pass it on
-			uartState = SNAP_haveDABPass;
+		    this->transmit(c);  // We will be reading dest addr; pass it on
+		    packetState = SNAP_haveDABPass;
 			break;
 
 		case SNAP_haveDABPass:
-			this->transmit(c);  // We will be reading source addr; pass it on
+				this->transmit(c);  // We will be reading source addr; pass it on
 
-			// Increment data length by 1 so that we just copy the CRC
-			// at the end as well.
-			packetLength++;
+				// Increment data length by 1 so that we just copy the CRC
+				// at the end as well.
+				packetLength++;
 
-			uartState = SNAP_readingDataPass;
+				packetState = SNAP_readingDataPass;
 			break;
 
 		case SNAP_readingDataPass:
@@ -361,8 +245,16 @@ void SNAP::receiveByte(byte c)
 			if (packetLength > 1)
 				packetLength--;
 			else
-				uartState = SNAP_idle;
+			{
+				packetState = SNAP_idle;
+				serialStatus &= ~inTransmitMsgBit; //clear
+			}
 			break;
+			
+	  default:
+	    serialStatus |= serialErrorBit;  //set serialError
+	    serialStatus |= wrongStateErrorBit;  
+	    this->receiveError();
 	}
 }
 
@@ -387,17 +279,17 @@ void SNAP::sendDataByte(byte c)
 	// yet as we don't have complete information.
 
 	// Drop if trying to send too much
-	if (sendPacketLength >= TX_BUFFER_SIZE)
+	if (txLength >= TX_BUFFER_SIZE)
 		return;
 
-	sendPacket[sendPacketLength] = c;
-	sendPacketLength++;
+	txBuffer[txLength] = c;
+	txLength++;
 }
 
 void SNAP::sendMessage(byte dest)
 {
-	sendPacketDestination = dest;
-	sendPacketLength = 0;
+	txDestination = dest;
+	txLength = 0;
 }
 
 bool SNAP::packetReady()
@@ -457,31 +349,22 @@ byte SNAP::computeCRC(byte c)
 
 void SNAP::endMessage()
 {
-	byte length = sendPacketLength;
 	byte i;
+	
+    // init our CRC
+    crc = 0;
 
-	if (length > 7)
-	{
-		length = 8;
-		sendPacketLength = 16;
-	}
+	//here is our header.
+    this->transmit(SNAP_SYNC);									// our sync byte
+    this->transmit(this->computeCRC(B01010001));				// HDB2 - Request ACK
+    this->transmit(this->computeCRC(B00110000 | txLength));		// HDB1 
+    this->transmit(this->computeCRC(txDestination));			// Destination
+    this->transmit(this->computeCRC(destAddress));				// Source (us)
 
-	// Send the message
-	this->transmit(SNAP_SYNC);
-	crc = 0;
-	this->transmit(computeCRC(B01010001));   // Request ACK
-	this->transmit(computeCRC(B00110000 | length));
-	this->transmit(computeCRC(sendPacketDestination));
-	this->transmit(computeCRC(destAddress));
-	for(i = 0; i < sendPacketLength; i++)
-		this->transmit(computeCRC(sendPacket[i]));
-	this->transmit(crc); /// @todo crc here
+	//payload.
+    for(i=0; i<txLength; i++)
+      this->transmit(this->computeCRC(txBuffer[i]));
 
-	/// @bug Because interrupts are now disabled during sending
-	/// the buffer cannot empty, so if too much is sent, it
-	/// will probably cause a lock-up (I've not actually seen
-	/// this happen yet, but it probably will).  It should
-	/// be a bit smarter to prevent this or else enable
-	/// interrupts briefly during this->transmit if it is full and
-	/// then restore interrupts to their start state.
+	//our crc check.
+    this->transmit(crc); /// @todo crc here
 }
