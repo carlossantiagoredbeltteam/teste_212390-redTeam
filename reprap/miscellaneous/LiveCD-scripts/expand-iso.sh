e3fec11c2466844ce3615f7aa8eddc96614c08f8
reece-arnott <reece-arnott@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2007-12-08 02:50:49 +0000

    A first attempt at scripts useful for making and changing a ReprapLiveCD (based on Ubuntu LiveCDs)

---
+++ reprap/miscellaneous/LiveCD-scripts/expand-iso.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# This is designed to be run from the same folder you have the iso you want to expand in.
+
+echo "The current .iso files are:"
+echo
+
+ls -w 1  *.iso
+
+echo
+echo -n "Do you want to continue?"
+echo
+
+while [ "y" != "$answer" ] && [ "n" != "$answer" ];
+
+do
+        echo 'Enter y for "Yes" or  n for "No" '
+        read answer
+done
+
+
+if [ "$answer" == "n" ]
+then
+    echo "Ending Script"
+   exit 0
+
+else
+
+mkdir cd
+mkdir mnt
+mkdir squash
+mkdir source
+
+
+echo -n "What iso file did you want to expand? "
+read FILE_NAME
+
+echo "Mounting iso"
+sudo mount $FILE_NAME mnt -o loop
+
+echo "Copying mounted iso filesystem to cd folder"
+echo "This will take a few minutes"
+rsync -a mnt/ cd/
+
+echo "Mounting squashed filesystem within iso"
+sudo mount -t squashfs -o loop mnt/casper/filesystem.squashfs squash
+
+echo "Copying squashed filesystem to source folder"
+echo "This will take quite a long while"
+sudo cp -a squash/* source/
+
+echo "Finishing up"
+sudo umount squash
+sudo umount mnt
+
+fi
---
+++ reprap/miscellaneous/LiveCD-scripts/mk-iso.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# This is designed to be run as root from the folder above the cd and source folders created by running the expand-iso.sh 
+
+echo "Setting up the CD manifests"
+chmod +w cd/casper/filesystem.manifest
+chroot source dpkg-query -W --showformat '${Package} ${Version}\n' > cd/casper/filesystem.manifest
+cp cd/casper/filesystem.manifest cd/casper/filesystem.manifest-desktop
+
+sed -ie /ubiquity/d cd/casper/filesystem.manifest-desktop
+sed -ie /casper/d cd/casper/filesystem.manifest-desktop
+sed -ie /libdebian-installer4/d cd/casper/filesystem.manifest-desktop
+sed -ie /os-prober/d cd/casper/filesystem.manifest-desktop
+sed -ie /ubuntu-live/d cd/casper/filesystem.manifest-desktop
+sed -ie /user-setup/d cd/casper/filesystem.manifest-desktop
+
+echo "Making sure that the two manifests are different from each other"
+diff cd/casper/filesystem.manifest cd/casper/filesystem.manifest-desktop
+
+echo "Remove the old filesystem.squashfs"
+rm cd/casper/filesystem.squashfs
+
+echo "Making new filesystem.squashfs - takes a looooooong time"
+mksquashfs source cd/casper/filesystem.squashfs
+
+echo "Removing old md5sum"
+rm cd/md5sum.txt
+
+echo "Finally create the iso image."
+echo "You currently have the following ReprapLiveCD images:"
+echo
+
+ls -w 1  ReprapLiveCD-*
+echo
+echo -n "Do you want to continue?"
+echo
+
+while [ "y" != "$answer" ] && [ "n" != "$answer" ];
+
+do
+        echo 'Enter y for "Yes" or  n for "No" '
+        read answer
+done
+
+
+if [ "$answer" == "n" ]
+then
+    echo "Ending Script"
+   exit 0
+
+else
+
+echo -n "What version of the image is this? "
+read vnum
+
+IMAGE_NAME="ReprapLiveCD-"$vnum
+
+echo "Creating "$IMAGE_NAME".iso"
+cd cd && find . -type f -print0 |xargs -0 md5sum > md5sum.txt
+mkisofs -r -V "$IMAGE_NAME" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o ../$IMAGE_NAME.iso .
+
+fi
+
