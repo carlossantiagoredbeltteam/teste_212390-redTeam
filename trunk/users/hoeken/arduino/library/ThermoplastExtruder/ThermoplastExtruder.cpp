237ad5a74c9a7030565ac47f050b7be7a974230e
hoekstar <hoekstar@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2007-12-17 00:04:11 +0000

    got extruder code working.

--- trunk/users/hoeken/arduino/library/ThermoplastExtruder/ThermoplastExtruder.cpp
+++ trunk/users/hoeken/arduino/library/ThermoplastExtruder/ThermoplastExtruder.cpp
@@ -4,52 +4,51 @@
 #define NUMTEMPS  22
 short temptable[NUMTEMPS][2] = {
 // { adc ,  temp }
-   { 1 ,  608 } ,
-   { 60 ,  176 } ,
-   { 70 ,  166 } ,
-   { 80 ,  157 } ,
-   { 90 ,  150 } ,
-   { 100 ,  143 } ,
-   { 110 ,  137 } ,
-   { 120 ,  131 } ,
-   { 130 ,  125 } ,
-   { 140 ,  120 } ,
-   { 150 ,  115 } ,
-   { 160 ,  110 } ,
-   { 170 ,  105 } ,
-   { 180 ,  100 } ,
-   { 190 ,  95 } ,
-   { 200 ,  91 } ,
-   { 210 ,  86 } ,
-   { 220 ,  81 } ,
-   { 230 ,  75 } ,
-   { 240 ,  70 } ,
-   { 250 ,  64 } ,
-   { 300 ,  4 }
+	{ 1 ,  608 } ,
+	{ 60 ,  176 } ,
+	{ 70 ,  166 } ,
+	{ 80 ,  157 } ,
+	{ 90 ,  150 } ,
+	{ 100 ,  143 } ,
+	{ 110 ,  137 } ,
+	{ 120 ,  131 } ,
+	{ 130 ,  125 } ,
+	{ 140 ,  120 } ,
+	{ 150 ,  115 } ,
+	{ 160 ,  110 } ,
+	{ 170 ,  105 } ,
+	{ 180 ,  100 } ,
+	{ 190 ,  95 } ,
+	{ 200 ,  91 } ,
+	{ 210 ,  86 } ,
+	{ 220 ,  81 } ,
+	{ 230 ,  75 } ,
+	{ 240 ,  70 } ,
+	{ 250 ,  64 } ,
+	{ 300 ,  4 }
 };
 
 /*!
-  motor_pwm_pin and heater_pin must be PWM capable outputs.
-  thermistor_pin must be an analog input.
-  motor_dir_pin must be a digital output.
+	motor_dir_pin must be a digital output.
+	motor_pwm_pin and heater_pin must be PWM capable outputs.
+	thermistor_pin must be an analog input.
 */
-ThermoplastExtruder::ThermoplastExtruder(byte motor_dir_pin, byte motor_pwm_pin, 
-                                         byte heater_pin, byte thermistor_pin)
+ThermoplastExtruder::ThermoplastExtruder(byte motor_dir_pin, byte motor_pwm_pin, byte heater_pin, byte thermistor_pin)
 {
-  this->motor_dir_pin = motor_dir_pin;
-  this->motor_pwm_pin = motor_pwm_pin;
-  this->heater_pin = heater_pin;
-  this->thermistor_pin = thermistor_pin;
+	this->motor_dir_pin = motor_dir_pin;
+	this->motor_pwm_pin = motor_pwm_pin;
+	this->heater_pin = heater_pin;
+	this->thermistor_pin = thermistor_pin;
 
-  pinMode(this->motor_dir_pin, OUTPUT);
-  pinMode(this->motor_pwm_pin, OUTPUT);
-  pinMode(this->heater_pin, OUTPUT);
-	
-  this->readTemp();
-  this->setSpeed(0);
-  this->setDirection(true);
-  this->setTargetTemp(0);
-  this->setHeater(0);
+	pinMode(this->motor_dir_pin, OUTPUT);
+	pinMode(this->motor_pwm_pin, OUTPUT);
+	pinMode(this->heater_pin, OUTPUT);
+
+	this->readTemperature();
+	this->setSpeed(0);
+	this->setDirection(true);
+	this->setTargetTemperature(0);
+	this->setHeater(0);
 }
 
 /*!
@@ -57,8 +56,8 @@ ThermoplastExtruder::ThermoplastExtruder(byte motor_dir_pin, byte motor_pwm_pin,
 */
 void ThermoplastExtruder::setSpeed(byte speed)
 {
-  this->motor_pwm = speed;
-  analogWrite(this->motor_pwm_pin, this->motor_pwm);
+	this->motor_pwm = speed;
+	analogWrite(this->motor_pwm_pin, this->motor_pwm);
 }
 
 /*!
@@ -66,69 +65,92 @@ void ThermoplastExtruder::setSpeed(byte speed)
 */
 void ThermoplastExtruder::setDirection(bool dir)
 {
-  this->motor_dir = dir;
-  digitalWrite(this->motor_dir_pin, this->motor_dir);
+	this->motor_dir = dir;
+	digitalWrite(this->motor_dir_pin, this->motor_dir);
 }
 
-void ThermoplastExtruder::setTargetTemp(byte target)
+void ThermoplastExtruder::setTargetTemperature(int target)
 {
-  this->target_celcius = target;
+	this->target_celsius = target;
 }
 
 void ThermoplastExtruder::setHeater(byte heat)
 {
-  this->heater_pwm = heat;
+	this->heater_pwm = heat;
 }
 
 byte ThermoplastExtruder::getHeater()
 {
-  return this->heater_pwm;
+	return this->heater_pwm;
 }
 
 byte ThermoplastExtruder::getSpeed()
 {
-  return this->motor_pwm;
+	return this->motor_pwm;
 }
 
 bool ThermoplastExtruder::getDirection()
 {
-  return this->motor_dir;
+	return motor_dir;
 }
 
-byte ThermoplastExtruder::getTemp()
+int ThermoplastExtruder::getTemperature()
 {
-  return this->current_celcius;
+	return this->current_celsius;
 }
 
-byte ThermoplastExtruder::getTargetTemp()
+int ThermoplastExtruder::getTemperatureInFahrenheit()
 {
-  return this->target_celcius;
+	return (((this->current_celsius * 9) / 5) + 32);
 }
 
-/*!
-  Samples the temperature and converts it to degrees Celcius.
-  Returns degrees Celcius.
+int ThermoplastExtruder::getTargetTemperature()
+{
+	return target_celsius;
+}
+
+/**
+*  Samples the temperature and converts it to degrees celsius.
+*  Returns degrees celsius.
 */
-byte ThermoplastExtruder::readTemp()
+int ThermoplastExtruder::readTemperature()
 {
-  int rawtemp = analogRead(this->thermistor_pin);
-  this->rawtmp = rawtemp;
-  byte i;
-  for (i=1;i<NUMTEMPS;i++) {
-    if (temptable[i][0] > rawtemp) {
-      int realtemp  = temptable[i-1][1] +
-        (rawtemp - temptable[i-1][0]) * (temptable[i][1] - temptable[i-1][1]) /
-        (temptable[i][0] - temptable[i-1][0]);
-      if (realtemp > 255) realtemp = 255; 
-      this->current_celcius = realtemp;
-      break;
-    }
-  }
-  if (i == NUMTEMPS) {
-    // Overflow: We just clamp to 0 degrees celcius
-    this->current_celcius = 0;
-  }
-  return this->current_celcius;
+	this->raw_temperature = this->getRawTemperature();
+	this->current_celsius = this->calculateTemperatureFromRaw(this->raw_temperature);
+	
+	return this->current_celsius;
+}
+
+/**
+*  Samples the temperature and converts it to degrees celsius.
+*  Returns degrees celsius.
+*/
+int ThermoplastExtruder::calculateTemperatureFromRaw(int raw)
+{
+	int celsius = 0;
+	byte i;
+	
+	for (i=1; i<NUMTEMPS; i++)
+	{
+		if (temptable[i][0] > raw)
+		{
+			celsius  = temptable[i-1][1] + 
+				(raw - temptable[i-1][0]) * 
+				(temptable[i][1] - temptable[i-1][1]) /
+				(temptable[i][0] - temptable[i-1][0]);
+			
+			if (celsius > 255)
+				celsius = 255; 
+
+			break;
+		}
+	}
+
+	// Overflow: We just clamp to 0 degrees celsius
+	if (i == NUMTEMPS)
+		celsius = 0;
+		
+	return celsius;
 }
 
 /*!
@@ -136,24 +158,29 @@ byte ThermoplastExtruder::readTemp()
   o If temp is too low, don't start the motor
   o Adjust the heater power to keep the temperature at the target
  */
-void ThermoplastExtruder::manageTemp()
+void ThermoplastExtruder::manageTemperature()
 {
-  // Stop the motor and start the heater if temp is too low
-  if (this->current_celcius < this->target_celcius) {
-    analogWrite(this->motor_pwm_pin, 0);
-    analogWrite(this->heater_pin, this->heater_pwm);
-  }
-  // Start the motor again and stop the heater if temp is high enough
-  else {
-    analogWrite(this->motor_pwm_pin, this->motor_pwm);
-    analogWrite(this->heater_pin, 0);
-  }
+	//make sure we know what our temp is.
+	this->readTemperature();
+	
+	// Stop the motor and start the heater if temp is too low
+	if (current_celsius < target_celsius)
+	{
+		analogWrite(motor_pwm_pin, 0);
+		analogWrite(heater_pin, heater_pwm);
+	}
+	// Start the motor again and stop the heater if temp is high enough
+	else
+	{
+		analogWrite(motor_pwm_pin, motor_pwm);
+		analogWrite(heater_pin, 0);
+	}
 }
 
 /*!
-  Raw temperature reading - just for debugging.
+  Raw temperature reading
  */
-int ThermoplastExtruder::getRaw()
+int ThermoplastExtruder::getRawTemperature()
 {
-  return this->rawtmp;
+	return analogRead(thermistor_pin);
 }
--- trunk/users/hoeken/arduino/library/ThermoplastExtruder/ThermoplastExtruder.h
+++ trunk/users/hoeken/arduino/library/ThermoplastExtruder/ThermoplastExtruder.h
@@ -1,57 +1,60 @@
 /*
-  ThermoplastExtruder.h - RepRap Thermoplastic Extruder library for Arduino - Version 0.1
+	ThermoplastExtruder.h - RepRap Thermoplastic Extruder library for Arduino - Version 0.1
 
-  This library is used to read, control, and handle a thermoplastic extruder.
+	This library is used to read, control, and handle a thermoplastic extruder.
 
-  History:
-  * Created intial library (0.1) by Zach Smith.
-  * Initial rework (0.2) by Marius Kintel <kintel@sim.no>
+	History:
+	* Created intial library (0.1) by Zach Smith.
+	* Initial rework (0.2) by Marius Kintel <kintel@sim.no>
 
-  */
+*/
 
 #ifndef THERMOPLASTEXTRUDER_H
 #define THERMOPLASTEXTRUDER_H
 
 class ThermoplastExtruder
 {
- public:
-  ThermoplastExtruder(byte motor_dir_pin, byte motor_pwm_pin, 
-                      byte heater_pin, byte thermistor_pin);
+	public:
+		ThermoplastExtruder(byte motor_dir_pin, byte motor_pwm_pin, byte heater_pin, byte thermistor_pin);
 
-  // various setters methods:
-  void setSpeed(byte speed);
-  void setDirection(bool dir);
-  void setTargetTemp(byte targettemp);
-  void setHeater(byte heat);
+		// various setters methods:
+		void setSpeed(byte speed);
+		void setDirection(bool dir);
+		void setTargetTemperature(int targettemp);
+		void setHeater(byte heat);
 
-  //get various info things.
-  byte getSpeed();
-  bool getDirection();
-  byte getTargetTemp();
-  byte getTemp();
-  byte getHeater();
+		//get various info things.
+		byte getSpeed();
+		bool getDirection();
+		int getTargetTemperature();
+		int getTemperature();
+		int getTemperatureInFahrenheit();
+		int getRawTemperature();
+		int calculateTemperatureFromRaw(int raw);
 
-  //manage the extruder
-  byte readTemp();
-  void manageTemp();
+		byte getHeater();
 
-  int getRaw();
- private:
+		//manage the extruder
+		int readTemperature();
+		void manageTemperature();
 
-  //pin numbers:
-  byte motor_pwm_pin;     // motor PWM pin
-  byte motor_dir_pin;     // motor direction pin
-  byte heater_pin;        // heater PWM pin
-  byte thermistor_pin;    // thermistor analog input pin
 
-  //extruder variables
-  bool motor_dir;        // Motor direction (true = forward, false = backward)
-  byte motor_pwm;        // Speed in PWM, 0-255
-  byte heater_pwm;       // Heater PWM, 0-255
-  byte target_celcius;    // Our target temperature
-  byte current_celcius;   // Our current temperature
 
-  int rawtmp;
+	private:
+
+		//pin numbers:
+		byte motor_pwm_pin;		// motor PWM pin
+		byte motor_dir_pin;		// motor direction pin
+		byte heater_pin;		// heater PWM pin
+		byte thermistor_pin;	// thermistor analog input pin
+
+		//extruder variables
+		bool motor_dir;			// Motor direction (true = forward, false = backward)
+		byte motor_pwm;			// Speed in PWM, 0-255
+		byte heater_pwm;		// Heater PWM, 0-255
+		int target_celsius;		// Our target temperature
+		int current_celsius;	// Our current temperature
+		int raw_temperature;	// our raw temperature reading.
 };
 
 #endif
