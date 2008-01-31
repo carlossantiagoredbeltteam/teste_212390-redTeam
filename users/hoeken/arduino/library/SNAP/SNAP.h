289f410e1d3b04039a1d876d99409396548b0da8
hoekstar <hoekstar@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2008-01-31 19:19:08 +0000

    audited and fixed indentation on the SNAP library.  getting ready to make it awesome.

--- users/hoeken/arduino/library/SNAP/SNAP.h
+++ users/hoeken/arduino/library/SNAP/SNAP.h
@@ -22,8 +22,8 @@
 
 //how many devices we have on this meta device
 #define MAX_DEVICE_COUNT 5		// size of our array to store virtual addresses
-#define TX_BUFFER_SIZE 16		// Transmit buffer size.
-#define RX_BUFFER_SIZE 16		// Receive buffer size.
+#define TX_BUFFER_SIZE 8		// Transmit buffer size.
+#define RX_BUFFER_SIZE 8		// Receive buffer size.
 #define HOST_ADDRESS 0			// address of the host.
 
 //our sync packet value.
@@ -52,88 +52,88 @@
 
 //these are the states for processing a packet.
 enum SNAP_states {
-  SNAP_idle = 0x30,
-  SNAP_haveSync,
-  SNAP_haveHDB2,
-  SNAP_haveHDB1,
-  SNAP_haveDAB,
-  SNAP_readingData,
-  SNAP_dataComplete,
+	SNAP_idle = 0x30,
+	SNAP_haveSync,
+	SNAP_haveHDB2,
+	SNAP_haveHDB1,
+	SNAP_haveDAB,
+	SNAP_readingData,
+	SNAP_dataComplete,
 
-  // The *Pass states below represent states where
-  // we should just be passing the data on to the next node.
-  // This is either because we bailed out, or because the
-  // packet wasn't destined for us.
-  SNAP_haveHDB2Pass,
-  SNAP_haveHDB1Pass,
-  SNAP_haveDABPass,
-  SNAP_readingDataPass
+	// The *Pass states below represent states where
+	// we should just be passing the data on to the next node.
+	// This is either because we bailed out, or because the
+	// packet wasn't destined for us.
+	SNAP_haveHDB2Pass,
+	SNAP_haveHDB1Pass,
+	SNAP_haveDABPass,
+	SNAP_readingDataPass
 };
 
 class SNAP
 {
- public:
-  SNAP();
-                
-  void addDevice(byte b);
+	public:
+		SNAP();
+            
+		void addDevice(byte b);
 
-  void receivePacket();
-  void receiveByte(byte b);
+		void receivePacket();
+		void receiveByte(byte b);
 
-  bool packetReady();
+		bool packetReady();
 
-  byte getDestination();
-  byte getSource();
-  byte getPacketLength();
-  byte getByte(byte index);
-  unsigned int getInt(byte index); // get 16 bits
+		byte getDestination();
+		byte getSource();
+		byte getPacketLength();
+		byte getByte(byte index);
+		unsigned int getInt(byte index); // get 16 bits
 
-  void startMessage(byte from);
-  void sendReply();
-  void sendMessage(byte dest);
-  void sendDataByte(byte c);
-  void sendDataInt(int data);
-  void endMessage();
+		void startMessage(byte from);
+		void sendReply();
+		void sendMessage(byte dest);
+		void sendDataByte(byte c);
+		void sendDataInt(int data);
+		void endMessage();
 
-  void releaseLock();
+		void releaseLock();
 
- private:
-  void receiveError();
-  bool hasDevice(byte b);
-  void transmit(byte c);
-  byte computeCRC(byte c);
-                
-  byte packetState;               // Current SNAP packet state
-  byte in_hdb1;                   // Temporary buffers needed to
-  byte in_hdb2;                   // pass packets on from various states
-  byte packetLength;              // Length of packet being received
-  byte destAddress;               // Destination of packet being received
-  byte sourceAddress;             // Source of packet being received
-  byte crc;                       // Incrementally calculated CRC value
-  byte serialStatus;              // flags for checking status of the serial-communication
+	private:
+		void receiveError();
+		bool hasDevice(byte b);
+		void transmit(byte c);
+		byte computeCRC(byte c);
+            
+		byte packetState;               // Current SNAP packet state
+		byte in_hdb1;                   // Temporary buffers needed to
+		byte in_hdb2;                   // pass packets on from various states
+		byte packetLength;              // Length of packet being received
+		byte destAddress;               // Destination of packet being received
+		byte sourceAddress;             // Source of packet being received
+		byte crc;                       // Incrementally calculated CRC value
+		byte serialStatus;              // flags for checking status of the serial-communication
 
-  byte rxBufferIndex;                                // Current receive buffer index
-  byte rxBuffer[RX_BUFFER_SIZE];        // Receive buffer
-                
-  // This buffer stores the last complete packet body (not the headers
-  // as they can be reconstructed).  This is to allow automatic re-sending
-  // if a NAK is received.
-  byte txBuffer[TX_BUFFER_SIZE];        // Last packet data, for auto resending on a NAK
-  byte txDestination;                                // Last packet destination
-  byte txLength;                                        // Last packet length
-                
-  // When sending a packet this is set to 0 and incremented for
-  // every NAK.  After too many have occurred, the packet is just
-  // dropped.
-  byte nakCount;
+		byte rxBufferIndex;                                // Current receive buffer index
+		byte rxBuffer[RX_BUFFER_SIZE];        // Receive buffer
+            
+		// This buffer stores the last complete packet body (not the headers
+		// as they can be reconstructed).  This is to allow automatic re-sending
+		// if a NAK is received.
+		byte txBuffer[TX_BUFFER_SIZE];        // Last packet data, for auto resending on a NAK
+		byte txDestination;                                // Last packet destination
+		byte txLength;                                        // Last packet length
+            
+		// When sending a packet this is set to 0 and incremented for
+		// every NAK.  After too many have occurred, the packet is just
+		// dropped.
+		byte nakCount;
 
-  // General flags:
-  // @bug these should be "sbit" rather than "byte" but sdcc is breaking a bit
-  bool ackRequested;
+		// General flags:
+		// @bug these should be "sbit" rather than "byte" but sdcc is breaking a bit
+		bool ackRequested;
 
-  // the address of our internal device sending message
-  byte deviceAddresses[MAX_DEVICE_COUNT];
-  byte deviceCount;
+		// the address of our internal device sending message
+		byte deviceAddresses[MAX_DEVICE_COUNT];
+		byte deviceCount;
 };
 
 //global variable declaration.
