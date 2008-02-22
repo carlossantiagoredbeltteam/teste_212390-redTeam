800b870cef889ded45b582562b3f7a6667bb24a0
hoekstar <hoekstar@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2008-02-22 23:08:10 +0000

    added the recent changes from the stl stuff.

--- users/hoeken/povray/stl2pov.pl
+++ users/hoeken/povray/stl2pov.pl
@@ -84,20 +84,61 @@ sub create_scene_file
 	chomp $line;
 	if ($line =~ m/\/\/ Name of the solid: (.*)/)
 	{
-		my $include = `basename $inc_file`;
-		chomp $include;
-		
+		my $max_x = 0.0;
+		my $max_y = 0.0;
+		my $max_z = 0.0;
+
+		#figoure out the max dimensions
+		while ($new_line = <INC_FP>)
+		{
+			#if ($new_line =~ m/([0-9]?.[0-9]?), ([0-9]?.[0-9]?), ([0-9]?.[0-9]?)/)
+			if ($new_line =~ m/(\d*\.?\d*), (\d*\.?\d*), (\d*\.?\d*)/)
+			{
+				chomp $new_line;
+				my $x = $1;
+				my $y = $2;
+				my $z = $3;
+
+				if ($x > $max_x)
+				{
+					$max_x = $x;
+				}
+				if ($y > $max_y)
+				{
+					$max_y = $y;
+				}
+				if ($z > $max_z)
+				{
+					$max_z = $z;
+				}
+			}
+		}
+		print "max: $max_x, $max_y, $max_z\n";
 		my $obj_name = $1;
 		chomp $obj_name;
+		$obj_name = "m_" . $obj_name;
 		
+		my $cam_x = $max_x * 2;
+		my $cam_y = $max_y * 2;
+		my $cam_z = $max_z * 2;
+
+		my $look_x = $max_x;
+		my $look_y = $max_y;
+		my $look_z = $max_z;
+
+		my $light_x = ($max_x+1) / 2;
+		my $light_y = ($max_y+1) / 2;
+		my $light_z = $max_z * 2;
+
 		#create the .pov scene file.
 		print SCENE_FP "//$scene_file\n\n";
-		print SCENE_FP "#include $include\n\n";
-		print SCENE_FP "background{color rgb 1}\n\n";
-		print SCENE_FP "object{ $obj_name\n\n";
+		print SCENE_FP "#include \"$inc_file\"\n\n";
+		print SCENE_FP "background {color rgb <0.9, 0.9, 0.9>}\n\n";
+		print SCENE_FP "object { $obj_name\n\n";
 		print SCENE_FP "\trotate 90*x\n";
+		print SCENE_FP "\trotate 90*y\n";
 		print SCENE_FP "\ttexture {\n";
-		print SCENE_FP "\t\tpigment {color rgb <1,0.5,0> }\n";
+		print SCENE_FP "\t\tpigment {color rgb <0.1, 0.6, 0.1> }\n";
 		print SCENE_FP "\t\tfinish {\n";
 		print SCENE_FP "\t\t\tambient 0.15\n";
 		print SCENE_FP "\t\t\tdiffuse 0.85\n";
@@ -105,12 +146,12 @@ sub create_scene_file
 		print SCENE_FP "\t\t}\n";
 		print SCENE_FP "\t}\n\n";
 		print SCENE_FP "}\n\n";
-		print SCENE_FP "light_source { <-20, 100, 20> color rgb 2 }\n\n";
+		print SCENE_FP "light_source { <$light_x, $light_y, $light_z> color rgb 2 }\n\n";
+		print SCENE_FP "light_source { <-$light_x, -$light_y, -$light_z> color rgb 2 }\n\n";
 		print SCENE_FP "camera {\n";
 		print SCENE_FP "\tperspective\n";
-		print SCENE_FP "\tangle 35\n";
-		print SCENE_FP "\tright x*image_width/image_height\n";
-		print SCENE_FP "\tlocation <-3,7,15>\n";
+		print SCENE_FP "\tlocation <$cam_x, $cam_y, $cam_z>\n";
+		print SCENE_FP "\tlook_at <$look_x, $look_y, $look_z>\n";
 		print SCENE_FP "\t";
 		print SCENE_FP "}\n";
 	}
@@ -140,6 +181,8 @@ sub render_scene_file
 		mkdir $render_dir;
 	}
 	
-	$cmd = "povray +I$scene_file +O$render_name +FN +W1200 +H1024 +Q9 +A +AM2 -D -V +WL0";
+	$cmd = "povray +I$scene_file +Otemp.png +FN +W1200 +H1024 +Q9 +A +AM2 -D -V +WL0";
+	print $cmd . "\n";
 	print `$cmd` . "\n";
+	`cp temp.png $render_name`;
 }
