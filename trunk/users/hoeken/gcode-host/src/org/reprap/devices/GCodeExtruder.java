41e7bba3d87a9003547a3666afd3eea6e9e4a9fa
hoekstar <hoekstar@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2008-03-07 19:57:17 +0000

    did some more work.  feeling good and close.

--- trunk/users/hoeken/gcode-host/src/org/reprap/devices/GCodeExtruder.java
+++ trunk/users/hoeken/gcode-host/src/org/reprap/devices/GCodeExtruder.java
@@ -8,17 +8,22 @@ import org.reprap.ReprapException;
 import javax.media.j3d.Appearance;
 import javax.vecmath.Color3f;
 import javax.media.j3d.Material;
+import org.reprap.comms.GCodeWriter;
+
 
 public class GCodeExtruder extends GenericExtruder
 {
+	GCodeWriter gcode;
+	
 	/**
 	 * @param prefs
 	 * @param extruderId
 	 */
-	public GCodeExtruder(Preferences prefs, int extruderId)
+	public GCodeExtruder(GCodeWriter writer, Preferences prefs, int extruderId)
 	{
 		super(prefs, extruderId);
 
+		gcode = writer;
 		isCommsAvailable = true;
 	}
 	
