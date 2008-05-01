b52e4add41c13f6ffad413a4eeb585a4bbd5a0f9
greenarrow <greenarrow@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2008-05-01 14:00:08 +0000

    Added pen calibration tool, lots of bugs fixed, incorperated dxf plotting to gui program

--- users/stef/pyRepRap/setup.py
+++ users/stef/pyRepRap/setup.py
@@ -9,5 +9,8 @@ setup(
 	author_email='greenarrow@users.sourceforge.net',
 	description='Python library to control RepRap firmware using the SNAP protocol.',
 	packages=['reprap'],
-	scripts=['scripts/rrplotdxf', 'scripts/reprapcontrol', 'scripts/rrplotgerber']
+	scripts=['scripts/reprapcontrol', 'scripts/reprapplot'],
+	data_files = [( 'share/reprap/icons', ['reprap/graphics/reprap.png', 'reprap/graphics/connect_established.png', 'reprap/graphics/media-playback-stop.png', 'reprap/graphics/connect_no.png', 'reprap/graphics/go-home.png', 'reprap/graphics/document-open.png', 'reprap/graphics/media-playback-start.png'] )]
 )
+
+
