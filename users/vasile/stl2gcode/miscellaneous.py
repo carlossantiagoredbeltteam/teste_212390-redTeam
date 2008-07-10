0bdb1714990da8c6d44ce8057a109a7856844627
jvasile <jvasile@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2008-07-10 19:15:04 +0000

    linking didn't work, so here's the actual file

---
+++ users/vasile/stl2gcode/miscellaneous.py
@@ -0,0 +1,24 @@
+## miscellaneous.py contains functinos of general application.
+## It's an Oft-used hodgepodge of functions.
+
+## Copyright (C) 2008 James Vasile <james@hackervisions.org>.  This is
+## free software; you can redistribute it and/or modify it under the
+## terms of the GNU General Public License as published by the Free
+## Software Foundation; either version 3 of the License, or any later
+## version.
+
+## This is distributed in the hope that it will be useful, but WITHOUT
+## ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+## or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+## License for more details.  You should have received a copy of the
+## GNU General Public License along with image-to-gcode; if not, write
+## to the Free Software Foundation, Inc., 51 Franklin Street 5th
+## Floor, Boston, MA 02110-1301 USA
+##
+## Not tested on Windows or Mac boxes.
+
+def slurp_file(fname):
+    "Returns an entire file as one string."
+    #TODO: error handling
+    file_contents = file(fname).read()
+    return file_contents
