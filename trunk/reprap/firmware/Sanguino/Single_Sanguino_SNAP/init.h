865d8a45a9c11cd14a00f32a76dbfea3eef4775c
adrian-bowyer <adrian-bowyer@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2008-09-24 12:43:36 +0000

    Provisional pin allocations for the Sanguino in init.dist.h

--- trunk/reprap/firmware/Sanguino/Single_Sanguino_SNAP/init.h
+++ trunk/reprap/firmware/Sanguino/Single_Sanguino_SNAP/init.h
@@ -1,38 +1,39 @@
+// This version is for the Sanguino.
+
 // THIS IS THE DISTRIBUTION HEADER FILE.
 // Please rename to init.h and make local customizations there.
 // Thanks -- Zach Hoeken
 
 /****************************************************************************************
 * digital i/o pin assignment
-*
-* this uses the undocumented feature of Arduino - pins 14-19 correspond to analog 0-5
+
 ****************************************************************************************/
 
 //cartesian bot pins
-#define X_STEP_PIN 2
-#define X_DIR_PIN 3
-#define X_MIN_PIN 4
-#define X_MAX_PIN 9
+#define X_STEP_PIN 1
+#define X_DIR_PIN 2
+#define X_MIN_PIN 3
+#define X_MAX_PIN 4
 #define X_ENABLE_PIN 15
-#define Y_STEP_PIN 10
-#define Y_DIR_PIN 7
-#define Y_MIN_PIN 8
-#define Y_MAX_PIN 13
+#define Y_STEP_PIN 26
+#define Y_DIR_PIN 27
+#define Y_MIN_PIN 28
+#define Y_MAX_PIN 10
 #define Y_ENABLE_PIN 15
-#define Z_STEP_PIN 19
-#define Z_DIR_PIN 18
-#define Z_MIN_PIN 17
-#define Z_MAX_PIN 16
+#define Z_STEP_PIN 11
+#define Z_DIR_PIN 29
+#define Z_MIN_PIN 30
+#define Z_MAX_PIN 18
 #define Z_ENABLE_PIN 15
 
 //extruder pins
-#define EXTRUDER_MOTOR_SPEED_PIN  11
-#define EXTRUDER_MOTOR_DIR_PIN    12
-#define EXTRUDER_HEATER_PIN       6
-#define EXTRUDER_FAN_PIN          5
-#define EXTRUDER_THERMISTOR_PIN   0
-#define VALVE_DIR_PIN             15
-#define VALVE_ENABLE_PIN          16  //NB: Conflicts with Max Z!!!!
+#define EXTRUDER_MOTOR_SPEED_PIN  13
+#define EXTRUDER_MOTOR_DIR_PIN    20
+#define EXTRUDER_HEATER_PIN       12
+#define EXTRUDER_FAN_PIN          19
+#define EXTRUDER_THERMISTOR_PIN   24 // Should this be 0?
+#define VALVE_DIR_PIN             21
+#define VALVE_ENABLE_PIN          22  
 
 // how many steps do our motors have?
 #define X_MOTOR_STEPS 400
