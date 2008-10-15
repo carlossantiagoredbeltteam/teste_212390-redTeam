bd8fbba0e6382c085460d45ab5f41212e5009bb2
greenarrow <greenarrow@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2008-10-15 21:48:19 +0000

    pyRepRap reorganise

--- trunk/users/stef/pyRepRap/reprap/shapeplotter.py
+++ trunk/users/stef/pyRepRap/reprap/shapeplotter.py
@@ -1,29 +1,29 @@
-#######################################################################################################################################################
-# This module plots various shapes to polgons                                  .                                                                      #
-#######################################################################################################################################################
 """
-Licenced under GNU v2 and the 'I'm not going to help you kill people licence'. The latter overrules the former.
-        
-I'm not going to help you kill people licence v1:
-The use of this software in any form for any purposes relating to any form of military activity or
-research either directly or via subcontracts is strictly prohibited.
-Any company or organisation affiliated with any military organisations either directly or through
-subcontracts are strictly prohibited from using any part of this software.
+This module plots many shapes a polygon objects containing a series of lines
+The Polygon object can be found in reprap.toolpath
+"""
 
-GNU licence:        
-RepRap Gerber Plotter is free software; you can redistribute it and/or modify it 
-under the terms of the GNU General Public License as published by the Free Software Foundation; 
-either version 2 of the License, or (at your option) any later version.
+# Python module properties
+__author__ = "Stefan Blanke (greenarrow) (greenarrow@users.sourceforge.net)"
+__license__ = "GPL 3.0"
+__credits__ = "Author of potrace"
+__licence__ = """
+pyRepRap is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-RepRap Gerber Plotter is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
-without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
-See the GNU General Public License for more details. You should have received a copy of 
-the GNU General Public License along with File Hunter; if not, write to 
-the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+pyRepRap is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with pyRepRap.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import math, os
-import polygon
+import toolpath
 
 # Fill modes
 FILL_LOCUS = 1
@@ -31,42 +31,42 @@ FILL_LINES = 2
 
 debug = False
 
-# Returns polygon for line
-def line(line, keepToolOn = False ):
-	poly = polygon.Polygon()
+def line(line):
+	"""Returns polygon for line (x1, y2, x2, y2) as a Polygon object"""
+	poly = toolpath.Polygon()
 	x1, y1, x2, y2 = line
-	poly.addPoint( (x1, y1) )
-	poly.addPoint( (x2, y2) )
+	poly.addPoint( toolpath.Point(x1, y1) )
+	poly.addPoint( toolpath.Point(x2, y2) )
 	return poly
 
-# Returns polygon for point
-def point(point, keepToolOn = False ):
-	poly = polygon.Polygon()
+def point(point):
+	"""Returns polygon for point (x, y) as a Polygon Object"""
+	poly = toolpath.Polygon()
 	x, y = point
-	poly.addPoint( (x, y) )
+	poly.addPoint( toolpath.Point(x, y) )
 	return poly
 
-# Return polygon for arc with defined position, radius, start angle, end angle     resolution in lines per mm on circumference.
-# This function works in degrees but takes parameters in radians
-def arc(x, y, radius, startAngle, endAngle, resolution, fillDensity = False):
-	poly = polygon.Polygon()
+def arc(x, y, radius, startAngle, endAngle, resolution):
+	"""Returns polygon an arc with x, y, radius, start angle (radians), end engle (radians) and resolution (lines per mm) a Polygon object"""
+	# This function works in degrees but takes parameters in radians
+	poly = toolpath.Polygon()
 	if debug: print "Plotting arc at", x, y, "from", startAngle, "(", math.degrees(startAngle), ") to", endAngle, "(", math.degrees(endAngle), ")"
 	startAngle, endAngle = math.degrees(startAngle), math.degrees(endAngle)
 	circumference = float(2) * math.pi * float(radius)
 	angleDiv = ( float(360) / float( circumference * resolution ) )# + ( 360 % int( circumference * resolution ) )
-	lastX, lastY = calcCircle( startAngle, radius )
+	lastX, lastY = _calcCircle( startAngle, radius )
 	# compensate for arc going beyond 360 deg
 	if startAngle > endAngle:
 		endAngle += 360								
 	# make detail proportional to radius to always give good resolution
-	for theta in frange( startAngle, endAngle + angleDiv, angleDiv ):
-		cx, cy = calcCircle(theta, radius)
-		poly.addPoint( (x + cx, y + cy) )
+	for theta in _frange( startAngle, endAngle + angleDiv, angleDiv ):
+		cx, cy = _calcCircle(theta, radius)
+		poly.addPoint( toolpath.Point(x + cx, y + cy) )
 	return poly
 
-# Return polygon for filled circle with defined position, radius.
-def circle(x, y, radius, resolution, fillDensity = False ):
-	poly = polygon.Polygon()
+def circle(x, y, radius, resolution, fillDensity = False):
+	"""Returns polygon for a filled circle with x, y, radius, resolution (lines per mm) and fill density (lines per mm) as a Polygon object"""
+	poly = toolpath.Polygon()
 	if fillDensity:
 		numFills = int( float(fillDensity) * float(radius) )
 	else:
@@ -77,9 +77,9 @@ def circle(x, y, radius, resolution, fillDensity = False ):
 		poly.addPolygon( arc( x, y, r, math.radians(0), math.radians(360), resolution ) )
 	return poly
 
-# Return polygon for filled ellipse with defined position, radius.
 def ellipse(x, y, a, b, resolution, fillDensity = False):
-	poly = polygon.Polygon()
+	"""Returns polygon for a filled ellipse with x, y, a, b, resolution (lines per mm) and fill density (lines per mm) as a Polygon object"""
+	poly = toolpath.Polygon()
 	if not resolution:
 		resolution = self.circleResolution
 	if fillDensity:
@@ -94,22 +94,23 @@ def ellipse(x, y, a, b, resolution, fillDensity = False):
 	# Not really cicumference but will do?
 	circumference = float(2) * math.pi * float( float( a + b ) / 2 )
 	angleDiv = ( float(360) / float( circumference * resolution ) ) # + ( 360 % int( circumference * resolution ) )
-	lastX, lastY = calcEllipse( startAngle, a, b )
+	lastX, lastY = _calcEllipse( startAngle, a, b )
 	# Compensate for arc going beyond 360 deg
 	if startAngle > endAngle:
 		endAngle += 360
 	for d in range( 1, numFills + 1 ):
 		ra = ( float(d) / float(numFills) ) * float(a)
 		rb = ( float(d) / float(numFills) ) * float(b)
-		for theta in frange( startAngle, endAngle + angleDiv, angleDiv ):
-			newX, newY = calcEllipse( theta, ra, rb )
-			aLine = poly.addPoint(newX + x , newY + y)
+		for theta in _frange( startAngle, endAngle + angleDiv, angleDiv ):
+			newX, newY = _calcEllipse( theta, ra, rb )
+			aLine = poly.addPoint( toolpath.Point(newX + x , newY + y) )
 			if debug: print "aLine", aLine
 	return poly
 
 # Return polygon for a filled rectangle
 def rectangle(x, y, width, height, fillDensity = False):
-	poly = polygon.Polygon()
+	"""Returns polygon for line x, y, width, height and fill density (lines per mm) as a Polygon object"""
+	poly = toolpath.Polygon()
 	numFillsY = int( float(fillDensity) * float(height) )
 	cornerX, cornerY = x - ( width / 2 ), y - ( height / 2 )
 	invert = False
@@ -119,21 +120,21 @@ def rectangle(x, y, width, height, fillDensity = False):
 		line1 = (cornerX, cornerY + ry, cornerX + width, cornerY + ry )
 		line2 = (cornerX, cornerY + ry, cornerX + width, cornerY - ry )
 		if invert:
-			line1 = reverseLine(line1)
+			line1 = _reverseLine(line1)
 		invert = not invert
 		x1, y1, x2, y2 = line1
-		poly.addPoint( (x1, y1) )
-		poly.addPoint( (x2, y2) )
+		poly.addPoint( toolpath.Point(x1, y1) )
+		poly.addPoint( toolpath.Point(x2, y2) )
 		
-	poly.addPoint( (cornerX, cornerY) )
-	poly.addPoint( (cornerX, cornerY + height) )
-	poly.addPoint( (cornerX + width, cornerY) )
-	poly.addPoint( (cornerX + width, cornerY + height) )
+	poly.addPoint( toolpath.Point(cornerX, cornerY) )
+	poly.addPoint( toolpath.Point(cornerX, cornerY + height) )
+	poly.addPoint( toolpath.Point(cornerX + width, cornerY) )
+	poly.addPoint( toolpath.Point(cornerX + width, cornerY + height) )
 	return poly
 
-# Return polygon for filled photoplotter movement with circle aperture
-def circleTrace(x1, y1, x2, y2, radius, resolution, fillDensity = False ):
-	poly = polygon.Polygon()
+def circleStroke(x1, y1, x2, y2, radius, resolution, fillDensity = False ):
+	"""Returns polygon for a photoplotter moving stroke using a circular aperture with x, y, radius, resolution (lines per mm) and fill density (lines per mm) as a Polygon object"""
+	poly = toolpath.Polygon()
 	deltaY = y2 - y1
 	deltaX = x2 - x1
 	if x1 == x2 and y1 == y2:
@@ -142,14 +143,14 @@ def circleTrace(x1, y1, x2, y2, radius, resolution, fillDensity = False ):
 	else:
 		if debug: print "PMWC, fill density", fillDensity
 		#Plot central line
-		poly.addPoint( (x1, y1) )
-		poly.addPoint( (x2, y2) )
+		poly.addPoint( toolpath.Point(x1, y1) )
+		poly.addPoint( toolpath.Point(x2, y2) )
 		# For each locus
 		numFills = int( float(fillDensity) * float(radius) )
 		for d in range( 1, numFills + 1 ):
 			r = ( float(d) / float(numFills) ) * float(radius)
 			if debug: print "using r", r, "mm"
-			theta = angleFromDeltas( deltaX, deltaY )
+			theta = _angleFromDeltas( deltaX, deltaY )
 			rsintheta = r * math.sin( theta )
 			rcostheta = r * math.cos( theta )
 			# Makes sure angle is in correct quadrant
@@ -164,17 +165,22 @@ def circleTrace(x1, y1, x2, y2, radius, resolution, fillDensity = False ):
 				endOffset = -endOffset
 			# Plot side lines and end arcs (simi-circles) of locus
 			# reversing these point lets locus be drawn in one continual motion
-			poly.addPoint( (x2 - rsintheta, y2 + rcostheta) )
-			poly.addPoint( (x1 - rsintheta, y1 + rcostheta) )
+			poly.addPoint( toolpath.Point(x2 - rsintheta, y2 + rcostheta) )
+			poly.addPoint( toolpath.Point(x1 - rsintheta, y1 + rcostheta) )
 			poly.addPolygon( arc(x1, y1, r, theta + startOffset, theta + endOffset, resolution, fillDensity = False) )
-			poly.addPoint( (x1 + rsintheta, y1 - rcostheta) )
-			poly.addPoint( (x2 + rsintheta, y2 - rcostheta) )
+			poly.addPoint( toolpath.Point(x1 + rsintheta, y1 - rcostheta) )
+			poly.addPoint( toolpath.Point(x2 + rsintheta, y2 - rcostheta) )
 			poly.addPolygon( arc(x2, y2, r, theta - startOffset, theta - endOffset, resolution, fillDensity = False) )
 	return poly
 
-# Use potrace to convert raster to polygon(s)
-def rasterToPolygon(fileName, originalWidth, originalHeight, svg = False):
-	poly = polygon.Polygon()
+def fill(polygon, fillDensity):
+	pass
+
+def raster(fileName, originalWidth, originalHeight, svg = False):
+	"""Returns polygon for a vectorised raster as a Polygon object.
+	Uses external potrace program to convert raster file into polygon(s)
+	"""
+	poly = toolpath.Polygon()
 	if svg:
 		os.system("potrace --svg --output " + fileName[ :-3 ] + "svg " + fileName)
 	os.system("potrace --alphamax 0 --turdsize 5 --backend gimppath --output " + fileName[ :-3 ] + "gimppath " + fileName)
@@ -197,7 +203,7 @@ def rasterToPolygon(fileName, originalWidth, originalHeight, svg = False):
 			elif p == 'Y:':
 				y = float(parts[i + 1]) * scale
 		if isPoint:
-			poly.addPoint( (x, y) )
+			poly.addPoint( toolpath.Point(x, y) )
 			#print "NEW POINT", x, y, ptype
 	# This should not be assumed?
 	poly.closed = True
@@ -227,24 +233,24 @@ def rasterToPolygon(fileName, originalWidth, originalHeight, svg = False):
 ############# General Maths Functions #############
 
 # Return the coordinates of a point on a circle at theta (rad) with radius.
-def calcCircle(theta, radius):
+def _calcCircle(theta, radius):
 	x = math.cos( math.radians(theta) ) * radius
 	y = math.sin( math.radians(theta) ) * radius
 	return x, y
 
 # Return the coordinates of a point on an ellipse at theta (rad) with a and b.
-def calcEllipse(theta, a, b):
+def _calcEllipse(theta, a, b):
 	x = math.cos( math.radians(theta) ) * a
 	y = math.sin( math.radians(theta) ) * b
 	return x, y
 
 # Reverse line (swap x1, y1 and x2, y2)
-def reverseLine( line ):
+def _reverseLine( line ):
 	x1, y1, x2, y2 = line
 	return x2, y2, x1, y1
 
 # Return the angle between the line between two points (2D coordinates) and vetical?
-def angleFromDeltas( dx, dy ):
+def _angleFromDeltas( dx, dy ):
 	radius = math.sqrt( ( dx * dx ) + ( dy * dy ) )
 	#if radius != 0:
 	dx, dy = dx / radius, dy / radius
@@ -267,10 +273,10 @@ def angleFromDeltas( dx, dy ):
 	else:
 		return math.radians(-90)	# i think this should really be 90, it just makes thae program work wen its -90 :)
 	#else:
-	#	print "Radius cannot be zero!, returning 0 (angleFromDeltas, shapeplotter.py)"
+	#	print "Radius cannot be zero!, returning 0 (_angleFromDeltas, shapeplotter.py)"
 
 # Range function accepting floats (by Dinu Gherman)
-def frange(start, end=None, inc=None):
+def _frange(start, end=None, inc=None):
 	if end == None:
 		end = start + 0.0
 		start = 0.0
@@ -287,7 +293,7 @@ def frange(start, end=None, inc=None):
 	return L
 
 # Return length of vector
-def calcVectorLength(line):
+def _calcVectorLength(line):
 	x1, y1, x2, y2 = line
 	deltaX = max(x1, x2) - min(x1, x2)
 	deltaY = max(y1, y2) - min(y1, y2)
