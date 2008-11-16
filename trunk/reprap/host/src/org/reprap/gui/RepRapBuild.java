ba5e24b03983d3385e18000d2d3db12f1515233d
adrian-bowyer <adrian-bowyer@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2008-11-16 11:23:55 +0000

    Bug where objects stayed selected on hitting Print fixed.

--- trunk/reprap/host/src/org/reprap/gui/RepRapBuild.java
+++ trunk/reprap/host/src/org/reprap/gui/RepRapBuild.java
@@ -218,7 +218,7 @@ public class RepRapBuild extends Panel3D implements MouseListener {
 	private PickCanvas pickCanvas = null; // The thing picked by a mouse click
 	private STLObject lastPicked = null; // The last thing picked
 	private java.util.List<STLObject> stls = new ArrayList<STLObject>(); // All the STLObjects to be built
-	private int objectIndex = 0; // Counter for STLs as they are loaded
+	//private int objectIndex = 0; // Counter for STLs as they are loaded
 
 	// Constructors
 	public RepRapBuild() throws Exception {
@@ -387,11 +387,12 @@ public class RepRapBuild extends Panel3D implements MouseListener {
 	public void anotherSTLFile(String s) {
 		if (s == null)
 			return;
-		objectIndex++;
+		//objectIndex++;
 		STLObject stl = new STLObject();
 		Attributes att = stl.addSTL(s, null, Preferences.unselectedApp(), lastPicked);
 		if(att != null)
 		{
+			// New separate object, or just appended to lastPicked?
 			if(stl.numChildren() > 0)
 			{
 				wv_and_stls.addChild(stl.top());
