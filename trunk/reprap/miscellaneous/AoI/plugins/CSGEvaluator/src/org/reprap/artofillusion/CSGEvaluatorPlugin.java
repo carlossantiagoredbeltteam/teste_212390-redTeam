7d45919c617b507296c4791d6a5b6f8fa1f473d3
kintel <kintel@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2009-01-14 21:57:36 +0000

    small bugfix: forgot a package rename

--- trunk/reprap/miscellaneous/AoI/plugins/CSGEvaluator/src/org/reprap/artofillusion/CSGEvaluatorPlugin.java
+++ trunk/reprap/miscellaneous/AoI/plugins/CSGEvaluator/src/org/reprap/artofillusion/CSGEvaluatorPlugin.java
@@ -6,10 +6,6 @@ public class CSGEvaluatorPlugin implements Plugin
 {
   public void processMessage(int message, Object args[]) {
     if (message == Plugin.APPLICATION_STARTING) {
-//      KeystrokeManager.addRecord(new KeystrokeRecord(KeyEvent.VK_C, 0, "CSG Evaluator",
-//          "ModellingTool plugin = (ModellingTool)" +
-//          "PluginRegistry.getPluginObject(\"at.metalab.artofillusion.CSGEvaluatorTool\");" +
-//          "plugin.commandSelected(window);"));
     }
     else if (message == Plugin.SCENE_WINDOW_CREATED) {
     }
