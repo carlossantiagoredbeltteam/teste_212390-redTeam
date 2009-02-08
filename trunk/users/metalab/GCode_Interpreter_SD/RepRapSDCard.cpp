2f7015515d885184cbb34addfe2f58a3b767e649
kintel <kintel@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2009-02-08 17:31:45 +0000

    Testing SD-card support

---
+++ trunk/users/metalab/GCode_Interpreter_SD/RepRapSDCard.cpp
@@ -0,0 +1,121 @@
+#include <HardwareSerial.h>
+#include "RepRapSDCard.h"
+#include "fat.h"
+#include "sd_raw.h"
+#include "partition.h"
+#include <string.h>
+
+#if !USE_DYNAMIC_MEMORY
+  struct partition_struct partition_handles[PARTITION_COUNT];
+#endif
+
+void RepRapSDCard::print_disk_info()
+{
+  if (!fs) return;
+
+  struct sd_raw_info disk_info;
+  if (!sd_raw_get_info(&disk_info)) return;
+
+    Serial.print("manuf:  0x"); 
+    Serial.println(disk_info.manufacturer, HEX);
+    Serial.print("oem:    ");
+    Serial.println((char*) disk_info.oem);
+    Serial.print("prod:   ");
+    Serial.println((char*) disk_info.product);
+    Serial.print("rev:    ");
+    Serial.println(disk_info.revision, HEX);
+    Serial.print("serial: 0x");
+    Serial.println(disk_info.serial, HEX);
+    Serial.print("date:   ");
+    Serial.print(disk_info.manufacturing_month, DEC);
+    Serial.println(disk_info.manufacturing_year);
+    Serial.print("size:   ");
+    Serial.print(disk_info.capacity / 1024 / 1024, DEC);
+    Serial.println("MB");
+    Serial.print("copy:   ");
+    Serial.println(disk_info.flag_copy, DEC);
+    Serial.print("wr.pr.: ");
+    Serial.print(disk_info.flag_write_protect_temp, DEC);
+    Serial.println(disk_info.flag_write_protect);
+    Serial.print("format: ");
+    Serial.println(disk_info.format, DEC);
+    Serial.print("free:   ");
+    Serial.print(fat_get_fs_free(fs));
+    Serial.print("/");
+    Serial.println(fat_get_fs_size(fs), DEC);
+}
+
+bool RepRapSDCard::open_partition(void)
+{
+  // open first partition
+  partition = partition_open(sd_raw_read,
+                             sd_raw_read_interval,
+                             sd_raw_write,
+                             sd_raw_write_interval,
+                             0);
+
+  if (!partition) {
+    // If the partition did not open, assume the storage device
+    // is a "superfloppy", i.e. has no MBR.
+    partition = partition_open(sd_raw_read,
+                               sd_raw_read_interval,
+                               sd_raw_write,
+                               sd_raw_write_interval,
+                               -1);
+  }
+
+  if (!partition) return false;
+  return true;
+}
+
+bool RepRapSDCard::open_filesys(void)
+{
+  fs = fat_open(partition);
+  if (!fs) return false;
+  return true;
+}
+
+bool RepRapSDCard::open_dir(char *path)
+{
+  struct fat_dir_entry_struct dir;
+
+  fat_get_dir_entry_of_path(fs, path, &dir);
+  dd = fat_open_dir(fs, &dir);
+  if (!dd) return false;
+  return true;
+}
+
+
+uint8_t find_file_in_dir(struct fat_fs_struct* fs, struct fat_dir_struct* dd, const char* name, struct fat_dir_entry_struct* dir_entry)
+{
+  while (fat_read_dir(dd, dir_entry))
+  {
+    if (strcmp(dir_entry->long_name, name) == 0)
+    {
+      fat_reset_dir(dd);
+      return 1;
+    }
+  }
+
+  return 0;
+}
+
+struct fat_file_struct* open_file_in_dir(struct fat_fs_struct* fs, struct fat_dir_struct* dd, const char* name)
+{
+  struct fat_dir_entry_struct file_entry;
+
+  if (!find_file_in_dir(fs, dd, name, &file_entry))
+  {
+    //Serial.println("File not found");
+    return 0;
+  }
+
+  return fat_open_file(fs, &file_entry);
+}
+
+
+uint8_t RepRapSDCard::create_file(char *name)
+{
+  struct fat_dir_entry_struct file_entry;
+  return fat_create_file(dd, name, &file_entry);
+}
---
+++ trunk/users/metalab/GCode_Interpreter_SD/RepRapSDCard.h
@@ -0,0 +1,47 @@
+#ifndef _REPRAPSDCARD_H_
+#define _REPRAPSDCARD_H_
+
+#include "sd_raw.h"
+#include "sd_raw_config.h"
+#include "sd-reader_config.h"
+#include "partition.h"
+#include "partition_config.h"
+#include "fat.h"
+
+typedef struct fat_file_struct * File;
+
+class RepRapSDCard
+{
+  //private?
+  struct partition_struct *partition;
+  struct fat_fs_struct* fs;
+  struct fat_dir_struct* dd;
+  struct fat_dir_entry_struct file_entry;
+
+ public:
+  RepRapSDCard(void) { }
+  void print_disk_info();
+  bool isAvailable(void) { return sd_raw_available(); }
+  bool isLocked(void) { return sd_raw_locked(); }
+  bool init_card(void) { return sd_raw_init(); }
+  bool open_partition(void);
+  bool open_filesys(void);
+  bool open_dir(char *path);
+
+  char *get_next_name_in_dir(void);
+  File open_file(char *name);
+  void close_file(File f) { fat_close_file(f); }
+  uint8_t create_file(char *name);
+  uint8_t seek_file(File f, int32_t *offset, uint8_t whence) {
+    return fat_seek_file(f, offset, whence);
+  }
+  uint8_t reset_file(File f) { return fat_seek_file(f, 0, FAT_SEEK_SET); }
+  uint16_t read_file(File f, uint8_t* buffer, uint16_t buffer_len) {
+    return fat_read_file(f, buffer, buffer_len);
+  }
+  uint8_t write_file(File f, uint8_t *buff, uint8_t num) {
+    return fat_write_file(f, buff, num);
+  }
+};
+
+#endif
