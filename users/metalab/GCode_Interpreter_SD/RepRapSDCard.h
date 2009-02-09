927f2ef23edf486c415946553c676e519d24a794
kintel <kintel@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2009-02-09 02:38:52 +0000

    Some SD card restructuring

--- users/metalab/GCode_Interpreter_SD/RepRapSDCard.h
+++ users/metalab/GCode_Interpreter_SD/RepRapSDCard.h
@@ -8,40 +8,53 @@
 #include "partition_config.h"
 #include "fat.h"
 
-typedef struct fat_file_struct * File;
+typedef struct fat_file_struct File;
 
 class RepRapSDCard
 {
  public:
-  struct partition_struct *partition;
-  struct fat_fs_struct *filesystem;
-  struct fat_dir_struct *cwd;
-  struct fat_dir_entry_struct file_entry;
+  enum SDStatus {
+    SDOK = 0,
+    INIT_FAILED,
+    NO_CARD,
+    NO_PARTITION,
+    NO_FILESYS,
+    NO_ROOT
+  };
+  partition_struct *partition;     // Current partition
+  fat_fs_struct *filesystem;       // Current filesystem
+  fat_dir_struct *cwd;             // Current open directory
 
  public:
-  RepRapSDCard(void) { }
-  void print_disk_info();
-  bool isAvailable(void) { return sd_raw_available(); }
-  bool isLocked(void) { return sd_raw_locked(); }
-  bool init_card(void) { return sd_raw_init(); }
-  bool open_partition(void);
-  bool open_filesys(void);
-  bool open_dir(char *path);
+  RepRapSDCard() { }
 
-  char *get_next_name_in_dir(void);
-  File open_file(char *name);
-  void close_file(File f) { fat_close_file(f); }
-  uint8_t create_file(char *name);
-  uint8_t seek_file(File f, int32_t *offset, uint8_t whence) {
-    return fat_seek_file(f, offset, whence);
-  }
-  uint8_t reset_file(File f) { return fat_seek_file(f, 0, FAT_SEEK_SET); }
-  uint16_t read_file(File f, uint8_t* buffer, uint16_t buffer_len) {
+  uint8_t init();
+
+  void printInfo();
+  bool getNextEntry(char *name);
+
+
+
+  bool openDir(const char *path);
+  File *openFile(const char *name);
+  void closeFile(File *f) { fat_close_file(f); }
+  uint16_t readFile(File *f, uint8_t* buffer, uint16_t buffer_len) {
     return fat_read_file(f, buffer, buffer_len);
   }
-  uint8_t write_file(File f, uint8_t *buff, uint8_t num) {
+
+
+  uint8_t create_file(char *name);
+  uint8_t seek_file(File *f, int32_t *offset, uint8_t whence) {
+    return fat_seek_file(f, offset, whence);
+  }
+  uint8_t reset_file(File *f) { return fat_seek_file(f, 0, FAT_SEEK_SET); }
+  uint8_t write_file(File *f, uint8_t *buff, uint8_t num) {
     return fat_write_file(f, buff, num);
   }
+
+private:
+  bool open_partition();
+  bool open_filesys();
 };
 
 #endif
