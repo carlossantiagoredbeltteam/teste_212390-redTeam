59561353fd7b1761e665298ebe57d551bc9b5426
wizard23 <wizard23@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2009-02-22 22:40:01 +0000

    Hacked a prototype for loop...very messy...needs cleanup

---
+++ trunk/users/metalab/AoI/plugins/CSGEvaluator/src/org/reprap/artofillusion/CSGHelper.java
@@ -0,0 +1,56 @@
+package org.reprap.artofillusion;
+
+import artofillusion.math.CoordinateSystem;
+import artofillusion.object.CSGObject;
+import artofillusion.object.Object3D;
+import artofillusion.object.ObjectInfo;
+
+public class CSGHelper {
+	int count;
+	int operation;
+	ObjectInfo buffer;
+	CSGObject sum;
+	ObjectInfo sumInfo;
+
+	public CSGHelper(int op) {
+		operation = op;
+		count = 0;
+		sum = null;
+		buffer = null;
+	}
+
+	public void Add(ObjectInfo obj) {
+		if (count == 0)
+			buffer = obj;
+		else {
+			if (sum == null) {
+				sum = new CSGObject(buffer, obj, operation);
+				sumInfo = new ObjectInfo(sum,
+						new CoordinateSystem(), "tmp");
+			} else {
+				sum = new CSGObject(sumInfo, obj, operation);
+				sumInfo = new ObjectInfo(sum, new CoordinateSystem(), "tmp");
+			}
+		}
+		count++;
+	}
+
+	public ObjectInfo GetObjectInfo() {
+		if (count == 0)
+			return null;
+		if (count == 1)
+			return buffer;
+
+		return sumInfo;
+	}
+
+	public Object3D GetObject() {
+		if (count == 0)
+			return null;
+		if (count == 1)
+			return buffer.object;
+
+		return sum;
+	}
+
+}
