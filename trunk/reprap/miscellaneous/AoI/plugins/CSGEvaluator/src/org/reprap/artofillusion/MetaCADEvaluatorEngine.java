c4aa56d66d7cea7910b0f0dfbb71fcdafc380eb5
kintel <kintel@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2009-03-11 01:53:39 +0000

    Refactored coordsys handling, coordsys is now a separate expression

--- trunk/reprap/miscellaneous/AoI/plugins/CSGEvaluator/src/org/reprap/artofillusion/MetaCADEvaluatorEngine.java
+++ trunk/reprap/miscellaneous/AoI/plugins/CSGEvaluator/src/org/reprap/artofillusion/MetaCADEvaluatorEngine.java
@@ -1,8 +1,6 @@
 package org.reprap.artofillusion;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.util.Enumeration;
 
 import org.cheffo.jeplite.JEP;
 
@@ -10,7 +8,6 @@ import artofillusion.LayoutWindow;
 import artofillusion.UndoRecord;
 import artofillusion.math.CoordinateSystem;
 import artofillusion.math.Vec3;
-import artofillusion.object.CSGObject;
 import artofillusion.object.Cube;
 import artofillusion.object.Curve;
 import artofillusion.object.Cylinder;
@@ -53,6 +50,7 @@ public class MetaCADEvaluatorEngine extends CSGEvaluatorEngine {
     return evaluateLines(getParameters());
   }
 
+
   public void evaluate() {
     // only evaluate selected objects if the parameters could be evaluated
     if (readParameters())
@@ -77,31 +75,30 @@ public class MetaCADEvaluatorEngine extends CSGEvaluatorEngine {
     if (booleanOp == -1)
       return false;
 
-    MetaCADParser coordExpr = null;
+    MetaCADParser coordExpr = new MetaCADParser(line);
 
-    String[] subParam = line.split("\\)\\s*\\(");
-    if (subParam.length == 2) {
-      coordExpr = new MetaCADParser("dummy(" + subParam[1], ",");
-      line = subParam[0] + ")";
-    }
+    if (!coordExpr.parse()) return false;
 
-    MetaCADParser forExpr = new MetaCADParser(line, ";");
-
-    // no loop but a simple boolean operation let base class do that
-    if (forExpr.parseError || forExpr.parameters.length != 3) {
-      forExpr = new MetaCADParser("dummyDummy(dummyDummy=0; dummyDummy<1; dummyDummy=dummyDummy+1)", ";");
+    CoordinateSystem coordsys = evaluateCoordSys(coordExpr.getParameters("cs"));
+    
+    String[] parameters = coordExpr.getParameters(this.opToString(booleanOp));
+    if (parameters == null || parameters.length != 3) {
+      // no loop but a simple boolean operation let base class do that
+      MetaCADParser forExpr = new MetaCADParser("dummyDummy(dummyDummy=0; dummyDummy<1; dummyDummy=dummyDummy+1)");
+      forExpr.parse();
+      parameters = forExpr.getParameters("dummyDummy");
     }
 
     CSGHelper helper = new CSGHelper(booleanOp);
 
     // evaluate first part of for loop i.e. i=0
-    evaluateAssignment(forExpr.parameters[0]);
+    evaluateAssignment(parameters[0]);
 
     // security count to exit loop even if we fuck up exit condition
     int count = 0;
 
     // condition loop evaluate the condition i.e. i < 10
-    while (evaluateExpression(forExpr.parameters[1]) != 0 && count < 100) {
+    while (evaluateExpression(parameters[1]) != 0 && count < 100) {
       ObjectInfo[] objects = parent.children;
 
       for (int i = 0; i < objects.length; i++) {
@@ -109,7 +106,7 @@ public class MetaCADEvaluatorEngine extends CSGEvaluatorEngine {
       }
 
       // "increment" evaluate 3rd for parameter i.e. i=i+1
-      evaluateAssignment(forExpr.parameters[2]);
+      evaluateAssignment(parameters[2]);
       count++;
     }
 
@@ -117,21 +114,7 @@ public class MetaCADEvaluatorEngine extends CSGEvaluatorEngine {
       Texture tex = parent.object.getTexture();
       TextureMapping map = parent.object.getTextureMapping();
 
-      // use coordinate system paramaters from loop if user gave one
-      if (coordExpr != null && !coordExpr.parseError) {
-        double x, y, z, rotx, roty, rotz;
-
-        x = evaluateExpression(coordExpr.parameters[0]);
-        y = evaluateExpression(coordExpr.parameters[1]);
-        z = evaluateExpression(coordExpr.parameters[2]);
-
-        rotx = evaluateExpression(coordExpr.parameters[3]);
-        roty = evaluateExpression(coordExpr.parameters[4]);
-        rotz = evaluateExpression(coordExpr.parameters[5]);
-
-        parent.setCoords(new CoordinateSystem(new Vec3(x, y, z), rotx, roty,
-            rotz));
-      }
+      if (coordsys != null) parent.setCoords(coordsys);
 
       Object3D test = sanitizeObject3D(helper.sum);
 
@@ -172,136 +155,126 @@ public class MetaCADEvaluatorEngine extends CSGEvaluatorEngine {
     
     String line = parent.name;
 
-    MetaCADParser objExpr = new MetaCADParser(line, ",");
-
-    if (objExpr.parseError) {
+    MetaCADParser objExpr = new MetaCADParser(line);
+    if (!objExpr.parse()) {
       // FIXME: Notify user? 
       return false;
     }
 
     Object3D obj3D = null;
     CoordinateSystem coordsys = null;
-    
-    if (objExpr.name.startsWith("poly"))
-    {
-      if (objExpr.parameters.length == 4 && objExpr.parameters[0].startsWith("star"))
-      {
-        
-        double inner  = evaluateExpression(objExpr.parameters[2]); //innerValueField.getValue();
-        double outer = evaluateExpression(objExpr.parameters[3]); //outerValueField.getValue();
-        int n = (int)evaluateExpression(objExpr.parameters[1]); //(int) Math.round( nValueField.getValue() );
-  
-        Vec3[] v = new Vec3[2*n];
-        float[] smoothness = new float[2*n];
-        int index = 0;
-        for (int i = 0; i < n; i++)
-        {
-          v[index] = new Vec3( Math.cos( Math.PI * index / (double) n ),
-            Math.sin( Math.PI * index / (double) n ), 0 );
-          v[index].scale( inner );
-          v[index+1] = new Vec3( Math.cos( Math.PI * (index + 1) / (double) n ),
-            Math.sin( Math.PI * (index + 1) / (double) n ), 0 );
-          v[index+1].scale( outer );
-          smoothness[index]=0;
-          smoothness[index+1]=0;
-          index += 2;
-        }
-  
-        obj3D = new Curve( v, smoothness, Mesh.NO_SMOOTHING, true).convertToTriangleMesh(0);
+
+    Enumeration<String> iter = objExpr.getNames();
+    while (iter.hasMoreElements()) {
+      String name = iter.nextElement();
+      String[] parameters = objExpr.getParameters(name);
+
+      if (name.equals("cs")) {
+        coordsys = evaluateCoordSys(parameters);
       }
-    }
+      else if (name.startsWith("poly")) {
+        if (parameters.length == 4 && parameters[0].startsWith("star")) {
+          double inner  = evaluateExpression(parameters[2]); //innerValueField.getValue();
+          double outer = evaluateExpression(parameters[3]); //outerValueField.getValue();
+          int n = (int)evaluateExpression(parameters[1]); //(int) Math.round( nValueField.getValue() );
 
-    if (objExpr.name.startsWith("extrude")) {
-      // Three first parameters define the extrusion vector
-      Vec3 dir = Vec3.vz();
-      if (objExpr.parameters.length >= 3) {
-        dir = new Vec3(evaluateExpression(objExpr.parameters[0]),
-            evaluateExpression(objExpr.parameters[1]),
-            evaluateExpression(objExpr.parameters[2]));
-      }
-      // 4. parameter is num segments
-      int numsegments = 1;
-      if (objExpr.parameters.length >= 4) {
-        numsegments = (int)evaluateExpression(objExpr.parameters[3]);
-        if (numsegments < 2) numsegments = 1;
-      }
-      // 5. parameter is twist degrees
-      double twist = 0.0;
-      if (objExpr.parameters.length >= 5) {
-        twist = evaluateExpression(objExpr.parameters[4]);
-      }
-
-      // First child is the base mesh
-      // FIXME: Find all extrudable child objects instead
-      // FIXME: Recursively evaluate children
-      ObjectInfo[] children = parent.getChildren();
-      for (int i=0;i<children.length;i++) {
-        evaluateNode(children[i], undo);
-      }
-      ObjectInfo profile = parent.getChildren()[0];
-      Object3D profileobj = profile.getObject();
-      if (profileobj.canConvertToTriangleMesh() != Object3D.CANT_CONVERT) {
-        profileobj = profileobj.convertToTriangleMesh(0.1);
-      }
-
-      if (profileobj instanceof TriangleMesh) {
-        // Build extrusion curve from vector and segments
-        Vec3 v[] = new Vec3[numsegments+1];
-        float smooth[] = new float[v.length];
-        for (int i = 0; i < v.length; i++) {
-          v[i] = new Vec3(dir);
-          v[i].scale(1.0*i/numsegments);
-          smooth[i] = 1.0f;
-        }
-        Curve path = new Curve(v, smooth, Mesh.NO_SMOOTHING, false);
-        CoordinateSystem pathCoords = new CoordinateSystem();
-
-        obj3D = ExtrudeTool.extrudeMesh((TriangleMesh)profileobj, path, profile.getCoords(), pathCoords, twist*Math.PI/180.0, true);
-      }
-      for (int i=0;i<children.length;i++) children[i].setVisible(false);
-    }
-    else {
-      // First part of expression is always the coordinate system (transform)
-      if (objExpr.parameters.length >= 6) {
-        double x, y, z, rotx, roty, rotz;
-
-        x = evaluateExpression(objExpr.parameters[0]);
-        y = evaluateExpression(objExpr.parameters[1]);
-        z = evaluateExpression(objExpr.parameters[2]);
-
-        rotx = evaluateExpression(objExpr.parameters[3]);
-        roty = evaluateExpression(objExpr.parameters[4]);
-        rotz = evaluateExpression(objExpr.parameters[5]);
-        coordsys = new CoordinateSystem(new Vec3(x, y, z), rotx, roty, rotz);
-      }
-
-      // Typically, the three next parameters are common object properties
-      if (objExpr.parameters.length >= 9) {
-        double a, b, c;
-
-        a = evaluateExpression(objExpr.parameters[6]);
-        b = evaluateExpression(objExpr.parameters[7]);
-        c = evaluateExpression(objExpr.parameters[8]);
-
-        if (objExpr.name.startsWith("cube")) {
-          obj3D = new Cube(a, b, c);
-        }
-        if (objExpr.name.startsWith("sphere")) {
-          obj3D = new Sphere(a, b, c);
-        }
-        if (objExpr.name.startsWith("cylinder")) {
-          double ratio = 1;
-          // Cylinder takes an optional fourth parameter
-          if (objExpr.parameters.length >= 10) {
-            ratio = evaluateExpression(objExpr.parameters[9]);
-            if (ratio > 1) ratio = 1;
-            if (ratio < 0) ratio = 0;
+          Vec3[] v = new Vec3[2*n];
+          float[] smoothness = new float[2*n];
+          int index = 0;
+          for (int i = 0; i < n; i++)
+          {
+            v[index] = new Vec3( Math.cos( Math.PI * index / (double) n ),
+                Math.sin( Math.PI * index / (double) n ), 0 );
+            v[index].scale( inner );
+            v[index+1] = new Vec3( Math.cos( Math.PI * (index + 1) / (double) n ),
+                Math.sin( Math.PI * (index + 1) / (double) n ), 0 );
+            v[index+1].scale( outer );
+            smoothness[index]=0;
+            smoothness[index+1]=0;
+            index += 2;
           }
-          obj3D = new Cylinder(a, b, c, ratio);
+
+          obj3D = new Curve(v, smoothness, Mesh.NO_SMOOTHING, true).convertToTriangleMesh(0);
+        }
+      }
+      else if (name.startsWith("extrude")) {
+        // Three first parameters define the extrusion vector
+        Vec3 dir = Vec3.vz();
+        if (parameters.length >= 3) {
+          dir = new Vec3(evaluateExpression(parameters[0]),
+              evaluateExpression(parameters[1]),
+              evaluateExpression(parameters[2]));
+        }
+        // 4. parameter is num segments
+        int numsegments = 1;
+        if (parameters.length >= 4) {
+          numsegments = (int)evaluateExpression(parameters[3]);
+          if (numsegments < 2) numsegments = 1;
+        }
+        // 5. parameter is twist degrees
+        double twist = 0.0;
+        if (parameters.length >= 5) {
+          twist = evaluateExpression(parameters[4]);
+        }
+
+        // First child is the base mesh
+        // FIXME: Find all extrudable child objects instead
+        // FIXME: Recursively evaluate children
+        ObjectInfo[] children = parent.getChildren();
+        for (int i=0;i<children.length;i++) {
+          evaluateNode(children[i], undo);
+        }
+        ObjectInfo profile = parent.getChildren()[0];
+        Object3D profileobj = profile.getObject();
+        if (profileobj.canConvertToTriangleMesh() != Object3D.CANT_CONVERT) {
+          profileobj = profileobj.convertToTriangleMesh(0.1);
+        }
+
+        if (profileobj instanceof TriangleMesh) {
+          // Build extrusion curve from vector and segments
+          Vec3 v[] = new Vec3[numsegments+1];
+          float smooth[] = new float[v.length];
+          for (int i = 0; i < v.length; i++) {
+            v[i] = new Vec3(dir);
+            v[i].scale(1.0*i/numsegments);
+            smooth[i] = 1.0f;
+          }
+          Curve path = new Curve(v, smooth, Mesh.NO_SMOOTHING, false);
+          CoordinateSystem pathCoords = new CoordinateSystem();
+
+          obj3D = ExtrudeTool.extrudeMesh((TriangleMesh)profileobj, path, profile.getCoords(), pathCoords, twist*Math.PI/180.0, true);
+        }
+        for (int i=0;i<children.length;i++) children[i].setVisible(false);
+      }
+      else {
+        // Typically, the three next parameters are common object properties
+        if (parameters.length >= 3) {
+          double a, b, c;
+
+          a = evaluateExpression(parameters[0]);
+          b = evaluateExpression(parameters[1]);
+          c = evaluateExpression(parameters[2]);
+
+          if (name.startsWith("cube")) {
+            obj3D = new Cube(a, b, c);
+          }
+          if (name.startsWith("sphere")) {
+            obj3D = new Sphere(a, b, c);
+          }
+          if (name.startsWith("cylinder")) {
+            double ratio = 1;
+            // Cylinder takes an optional fourth parameter
+            if (parameters.length >= 4) {
+              ratio = evaluateExpression(parameters[3]);
+              if (ratio > 1) ratio = 1;
+              if (ratio < 0) ratio = 0;
+            }
+            obj3D = new Cylinder(a, b, c, ratio);
+          }
         }
       }
     }
-    
+
     if (obj3D != null) {
       Texture tex = parent.object.getTexture();
       TextureMapping map = parent.object.getTextureMapping();
@@ -317,10 +290,9 @@ public class MetaCADEvaluatorEngine extends CSGEvaluatorEngine {
     } else {
       return false;
     }
-
     return true;
   }
-
+  
   // Evaluates an Expression like 3*x+sin(a) and returns the value of it or 0 if
   // any error occurred
   double evaluateExpression(String expr) throws Exception {
@@ -369,6 +341,24 @@ public class MetaCADEvaluatorEngine extends CSGEvaluatorEngine {
     }
   }
 
+  CoordinateSystem evaluateCoordSys(String[] parameters) throws Exception {
+    CoordinateSystem coordsys = null;
+    if (parameters != null) {
+      coordsys = new CoordinateSystem();
+      if (parameters.length >= 3) {
+        coordsys.setOrigin(new Vec3(evaluateExpression(parameters[0]),
+            evaluateExpression(parameters[1]),
+            evaluateExpression(parameters[2])));
+      }
+      if (parameters.length >= 6) {
+        coordsys.setOrientation(evaluateExpression(parameters[3]),
+             evaluateExpression(parameters[4]),
+             evaluateExpression(parameters[5]));
+      }
+    }
+    return coordsys;
+  }
+  
   void showMessage(String text)  {
     new MessageDialog(this.window, text);
   }
