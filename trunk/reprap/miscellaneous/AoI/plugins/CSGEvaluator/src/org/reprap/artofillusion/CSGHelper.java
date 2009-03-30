a5e726e0d589056a0b15fd6ae2b6843e4b0b92fe
kintel <kintel@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2009-03-30 00:55:47 +0000

    Just coding style unification

--- trunk/reprap/miscellaneous/AoI/plugins/CSGEvaluator/src/org/reprap/artofillusion/CSGHelper.java
+++ trunk/reprap/miscellaneous/AoI/plugins/CSGEvaluator/src/org/reprap/artofillusion/CSGHelper.java
@@ -5,54 +5,52 @@ import artofillusion.object.CSGObject;
 import artofillusion.object.Object3D;
 import artofillusion.object.ObjectInfo;
 
-// Allows easy boolean modelling of multiple objects 
-// takes 
+/**
+ *  Allows easy boolean modeling of multiple objects 
+ *
+ */
 public class CSGHelper {
-	int count;
-	int operation;
-	ObjectInfo buffer;
-	CSGObject sum;
-	ObjectInfo sumInfo;
+  int count;
+  int operation;
+  ObjectInfo buffer;
+  CSGObject sum;
+  ObjectInfo sumInfo;
 
-	public CSGHelper(int op) {
-		operation = op;
-		count = 0;
-		sum = null;
-		buffer = null;
-	}
+  public CSGHelper(int op) {
+    this.operation = op;
+    this.count = 0;
+    this.sum = null;
+    this.buffer = null;
+  }
 
-	public void Add(ObjectInfo obj) {
-		if (count == 0)
-			buffer = obj;
-		else {
-			if (sum == null) {
-				sum = new CSGObject(buffer, obj, operation);
-				sumInfo = new ObjectInfo(sum,
-						new CoordinateSystem(), "tmp");
-			} else {
-				sum = new CSGObject(sumInfo, obj, operation);
-				sumInfo = new ObjectInfo(sum, new CoordinateSystem(), "tmp");
-			}
-		}
-		count++;
-	}
+  public void Add(ObjectInfo obj) {
+    if (this.count == 0) {
+      this.buffer = obj;
+    }
+    else {
+      if (this.sum == null) {
+        this.sum = new CSGObject(this.buffer, obj, this.operation);
+        this.sumInfo = new ObjectInfo(this.sum, new CoordinateSystem(), "tmp");
+      } else {
+        this.sum = new CSGObject(this.sumInfo, obj, this.operation);
+        this.sumInfo = new ObjectInfo(this.sum, new CoordinateSystem(), "tmp");
+      }
+    }
+    this.count++;
+  }
 
-	public ObjectInfo GetObjectInfo() {
-		if (count == 0)
-			return null;
-		if (count == 1)
-			return buffer;
+  public ObjectInfo GetObjectInfo() {
+    if (this.count == 0) return null;
+    if (this.count == 1) return this.buffer;
 
-		return sumInfo;
-	}
+    return this.sumInfo;
+  }
 
-	public Object3D GetObject() {
-		if (count == 0)
-			return null;
-		if (count == 1)
-			return buffer.object;
+  public Object3D GetObject() {
+    if (this.count == 0) return null;
+    if (this.count == 1) return this.buffer.object;
 
-		return sum;
-	}
+    return this.sum;
+  }
 
 }
