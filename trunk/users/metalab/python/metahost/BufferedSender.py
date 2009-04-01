db222e624ddd099a788336ead7f1f671b27bcf30
kintel <kintel@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2009-04-01 21:15:22 +0000

    initial implementation of simple sender

--- trunk/users/metalab/python/metahost/BufferedSender.py
+++ trunk/users/metalab/python/metahost/BufferedSender.py
@@ -1,57 +1,61 @@
-class MetaHost:
-    def __init__(self, serialPort, verbose=False):
+import sys
+import SerialFactory
+
+# The iterator should produce lines of gcode
+class BufferedSender:
+    def __init__(self, iterator, verbose=False):
         """
             Opens the serial port and prepares for writing.
             port MUST be set, and values are operating system dependant.
         """
-        self._verbose = verbose
-        self.lines = []
-        # pointing to the command that will be sent next
-        self.sendIndex = 0
-        # pointing to the command which will be acknoledged by the next ok
-        self.okIndex = 0
-        self.sentBufferSize = 0
+        self.iterator = iterator
+        self.verbose = verbose
+        self.bufferedlengths = []
+        self.bufferedlines = []     # for debugging
+        self.bufferavail = 128
+        self.nextline = ""
         self.BUFFERMAX = 128
 
-        self.ser = serialPort
+        self.serial = SerialFactory.createSerialPort(verbose=True)
 
-        if self._verbose:
-            print >> sys.stdout, "Serial Open?: " + str(self.ser.isOpen())
-            print >> sys.stdout, "Baud Rate: " + str(self.ser.baudrate)
+        while self.serial.inWaiting(): self.serial.read()
 
-        
+        if self.verbose:
+            print >> sys.stdout, "Serial Open?: " + str(self.serial.isOpen())
+            print >> sys.stdout, "Baud Rate: " + str(self.serial.baudrate)
 
-    def write(self, line):
-        self.lines.appen(line)
-
-    def send(self):
-        while (self.sendIndex < len(self.lines)):
-            # fill buffer
-            while (self.sendIndex < len(self.lines) && self.bufferSize < self.BUFFERMAX):
-                line = self.lines[self.sendIndex];
-                if len(line) < self.BUFFERMAX - self.bufferSize:
-                    self.ser.send(line)
-                    self.sendIndex += 1
-                    self.bufferSize += len(l)
-                    if self._verbose:
-                        print "sent: ", line
-                else:
-                    break # dont block anymore when buffer is full
+    def play(self):
+        try:
+            while True:
+                # fill buffer
+                while len(self.nextline) == 0 or self.nextline[0] == '(':
+                    self.nextline = self.iterator.next().strip();
+                # while next line fits in buffer
+                while self.bufferavail >= (len(self.nextline) + 1):
+                    self.serial.write(self.nextline + "\n")
+                    length = len(self.nextline) + 1
+                    self.bufferedlengths.append(length)
+                    self.bufferedlines.append(self.nextline)
+                    self.bufferavail -= length
+                    if self.verbose: print("sent: ", self.nextline)
+                    self.nextline = ""
+                    while len(self.nextline) == 0 or self.nextline[0] == '(':
+                        self.nextline = self.iterator.next().strip();
                 
-            if (self.sendIndex >= len(self.lines):
-                break #dont block if there is nothing to send
-                    
-            while (self.ser.available()):
-                line = self.ser.readLine()
-                if self._verbose:
-                print "received: ", line
+                while self.serial.inWaiting():
+                    recvline = self.serial.readline().strip()
+                    if self.verbose: print("received: ", recvline)
                 
-                if (line.startsWith("echo:")):
-                echo = line.sub(5)
-                if (echo != self.lines[self.okIndex]):
-                    print "Mismatch at index: ", self.okIndex, "sent: ", self.lines[self.okIndex], " but got: ", echo
-                else if (line.startsWith("ok")):
-                    self.sentBufferSize -= len(self.lines[self.okIndex])
-                    self.okIndex += 1
-                else:
-                print "unexpected serial line: ", line
+                    if recvline.startswith("echo: "):
+                        echo = recvline[6:]
+                        if (echo != self.bufferedlines[0]):
+                            print "Mismatch: sent: ", self.bufferedlines[0], " but got: ", echo
+                    elif recvline.startswith("ok"):
+                        self.bufferedlines.pop(0)
+                        size = self.bufferedlengths.pop(0)
+                        self.bufferavail += size
+                    else:
+                        print "unexpected serial line: ", recvline
+        except StopIteration:
+            print("EOF")
+
