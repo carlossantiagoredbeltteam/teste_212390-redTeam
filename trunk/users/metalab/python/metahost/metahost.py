b0f8ca879827803289cb8254de16f0a389a15c92
kintel <kintel@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2009-04-02 21:07:09 +0000

    added -b parameter

--- trunk/users/metalab/python/metahost/metahost.py
+++ trunk/users/metalab/python/metahost/metahost.py
@@ -5,12 +5,17 @@ import sys
 import getopt
 from BufferedSender import BufferedSender
 
+def printUsage(err = None):
+    if err and len(err.msg): print("Error: " + err.msg)
+    print("Usage: " + sys.argv[0] + " [<args>] <file.gcode>")
+    print("  FIXME: Print help about options here");
+    exit(1)
+
 if __name__ == "__main__":
     try:
-        opts, argv = getopt.getopt(sys.argv[1:], "vqnhp:", ["verbose","port="])
-    except getopt.error, msg:
-        raise Usage(msg)
-
+        opts, argv = getopt.getopt(sys.argv[1:], "vb:p:", ["verbose", "port=", "baud="])
+    except getopt.error, err:
+        printUsage(err)
 
     # option processing
     verbose = False
@@ -20,16 +25,18 @@ if __name__ == "__main__":
             verbose = True
             print "You have requested that verbosity be set to True"
             print "All communication with the arduino will be printed"
+        elif option in ( "-b" , "--baud" ):
+            baudrate = value
         elif option in ( "-p" , "--port" ):
             port = value
+        else:
+            print("Unhandled option: " + option + " " + value)
     
-    if len(argv) < 1:
-        print("Usage: " + sys.argv[0] + " <file.gcode>")
-        exit(1)
+    if len(argv) < 1: printUsage()
 
     infile = argv[0]
     print("Printing " + infile + "...")
     f = open(infile)
-    sender = BufferedSender(f, port = port, baudrate = 115200, verbose = verbose)
+    sender = BufferedSender(f, port = port, baudrate = baudrate, verbose = False)
     sender.play()
     f.close()
