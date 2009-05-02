b60f984e8ecf16db634914721e5d69f2b4c3628d
kintel <kintel@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2009-05-02 23:45:29 +0000

    abstract, evaluateChildren()

--- trunk/reprap/miscellaneous/AoI/plugins/CSGEvaluator/src/org/reprap/artofillusion/ParsedTree.java
+++ trunk/reprap/miscellaneous/AoI/plugins/CSGEvaluator/src/org/reprap/artofillusion/ParsedTree.java
@@ -1,6 +1,5 @@
 package org.reprap.artofillusion;
 
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -8,15 +7,15 @@ import java.util.List;
 
 import artofillusion.object.ObjectInfo;
 
-public class ParsedTree {
+public abstract class ParsedTree {
   public String name;
   public List<String> parameters;
   public List<ParsedTree> children;
   ObjectInfo aoiobj;
   
-  public ObjectInfo evaluate(MetaCADContext ctx) throws Exception {
-    ObjectInfo result = null;    
+  public abstract ObjectInfo evaluate(MetaCADContext ctx) throws Exception;
 
+  public List<ObjectInfo> evaluateChildren(MetaCADContext ctx) throws Exception {
     List<ObjectInfo> resultchildren = new LinkedList<ObjectInfo>();
     Iterator<ParsedTree> iter = this.children.iterator();
     while (iter.hasNext()) {
@@ -24,16 +23,7 @@ public class ParsedTree {
       resultchildren.add(treeobject.evaluate(ctx));
     }
     
-    Method meth;
-    try {
-      meth = ParsedTree.class.getMethod(this.name, List.class);
-      result = (ObjectInfo)meth.invoke(this, resultchildren);
-    } catch (Exception e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-
-    return result;
+    return resultchildren;
   }
 
   public ParsedTree() {
