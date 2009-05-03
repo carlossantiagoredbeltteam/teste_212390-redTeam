ce540b8bb69f9a84d34a1cbf75748111869260dd
kintel <kintel@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2009-05-03 00:53:07 +0000

    cleanup and first working sphere

--- trunk/reprap/miscellaneous/AoI/plugins/CSGEvaluator/src/org/reprap/artofillusion/ParsedTree.java
+++ trunk/reprap/miscellaneous/AoI/plugins/CSGEvaluator/src/org/reprap/artofillusion/ParsedTree.java
@@ -6,6 +6,8 @@ import java.util.LinkedList;
 import java.util.List;
 
 import artofillusion.object.ObjectInfo;
+import artofillusion.texture.Texture;
+import artofillusion.texture.TextureMapping;
 
 public abstract class ParsedTree {
   public String name;
@@ -13,22 +15,42 @@ public abstract class ParsedTree {
   public List<ParsedTree> children;
   ObjectInfo aoiobj;
   
-  public abstract ObjectInfo evaluate(MetaCADContext ctx) throws Exception;
+  public ParsedTree() {
+    this.parameters =  new ArrayList<String>();
+    this.children = new ArrayList<ParsedTree>();
+  }
+
+  public ObjectInfo evaluate(MetaCADContext ctx) throws Exception {
+    ObjectInfo result = evaluateObject(ctx);
+    updateAOI(result);
+    return result;
+  }
+
+  public abstract ObjectInfo evaluateObject(MetaCADContext ctx) throws Exception;
 
   public List<ObjectInfo> evaluateChildren(MetaCADContext ctx) throws Exception {
     List<ObjectInfo> resultchildren = new LinkedList<ObjectInfo>();
     Iterator<ParsedTree> iter = this.children.iterator();
     while (iter.hasNext()) {
       ParsedTree treeobject = iter.next();
-      resultchildren.add(treeobject.evaluate(ctx));
+      resultchildren.add(treeobject.evaluateObject(ctx));
     }
     
     return resultchildren;
   }
 
-  public ParsedTree() {
-    this.parameters =  new ArrayList<String>();
-    this.children = new ArrayList<ParsedTree>();
+  public void updateAOI(ObjectInfo newinfo) {
+    if (this.aoiobj != null) {
+      Texture tex = this.aoiobj.object.getTexture();
+      TextureMapping map = this.aoiobj.object.getTextureMapping();
+
+      this.aoiobj.setObject(newinfo.object);
+      this.aoiobj.setCoords(newinfo.coords);
+      this.aoiobj.object.setTexture(tex, map);
+      this.aoiobj.setVisible(true);
+
+      this.aoiobj.clearCachedMeshes();
+    }
   }
-    
+
 }
