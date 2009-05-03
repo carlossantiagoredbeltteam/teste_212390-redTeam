acc1b934893f3194781573abf69519ffdda5375a
kintel <kintel@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2009-05-03 02:32:53 +0000

    Implemented difference and intersection

--- trunk/reprap/miscellaneous/AoI/plugins/CSGEvaluator/src/org/reprap/artofillusion/BooleanObj.java
+++ trunk/reprap/miscellaneous/AoI/plugins/CSGEvaluator/src/org/reprap/artofillusion/BooleanObj.java
@@ -1,16 +1,6 @@
 package org.reprap.artofillusion;
 
-import java.util.List;
 
-import artofillusion.object.ObjectInfo;
+public abstract class BooleanObj extends ParsedTree {
 
-public class BooleanObj extends ParsedTree {
-
-  public ObjectInfo evaluateObject(MetaCADContext ctx) throws Exception {
-    List<ObjectInfo> children = evaluateChildren(ctx);
-    
-    
-    
-    return null;
-  }
 }
