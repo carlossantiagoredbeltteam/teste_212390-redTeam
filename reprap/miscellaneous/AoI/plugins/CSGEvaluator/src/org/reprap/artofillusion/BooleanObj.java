5d54acfa96cef3774e684ace28aae34ef1d81e4a
kintel <kintel@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2009-05-03 03:05:52 +0000

    Refactored boolean objects into BooleanObj

--- reprap/miscellaneous/AoI/plugins/CSGEvaluator/src/org/reprap/artofillusion/BooleanObj.java
+++ reprap/miscellaneous/AoI/plugins/CSGEvaluator/src/org/reprap/artofillusion/BooleanObj.java
@@ -1,6 +1,24 @@
 package org.reprap.artofillusion;
 
+import java.util.List;
 
-public abstract class BooleanObj extends ParsedTree {
+import artofillusion.object.ObjectInfo;
+
+
+public class BooleanObj extends ParsedTree {
+  int type;
+
+  public BooleanObj(int type) {
+    this.type = type;
+  }
+  
+  public ObjectInfo evaluateObject(MetaCADContext ctx) throws Exception {
+    
+    CSGHelper helper = new CSGHelper(this.type);
+    List<ObjectInfo> children = evaluateChildren(ctx);
+    helper.addAll(children.iterator());
+    
+    return helper.GetObjectInfo();
+  }
 
 }
