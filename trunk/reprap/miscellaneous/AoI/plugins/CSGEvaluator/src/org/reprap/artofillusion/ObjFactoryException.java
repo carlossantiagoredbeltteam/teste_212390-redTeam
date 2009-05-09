403b317bb8bfcb063702525bf85139253d2d93af
kintel <kintel@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2009-05-09 06:35:57 +0000

    Let ObjFactory throw ObjFactoryException

---
+++ trunk/reprap/miscellaneous/AoI/plugins/CSGEvaluator/src/org/reprap/artofillusion/ObjFactoryException.java
@@ -0,0 +1,12 @@
+package org.reprap.artofillusion;
+
+import org.reprap.artofillusion.parser.ParseException;
+
+public class ObjFactoryException extends ParseException {
+
+  private static final long serialVersionUID = -5731809677962819119L;
+
+  public ObjFactoryException(String message) {
+    super(message);
+  }
+}
