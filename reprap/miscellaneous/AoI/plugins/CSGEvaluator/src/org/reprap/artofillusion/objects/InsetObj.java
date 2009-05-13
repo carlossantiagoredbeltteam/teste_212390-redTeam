e28f2cfdd7bf4fb8c26637243706889a2056ab66
wizard23 <wizard23@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2009-05-13 04:04:47 +0000

    cleanup,removed old duplicate code

--- reprap/miscellaneous/AoI/plugins/CSGEvaluator/src/org/reprap/artofillusion/objects/InsetObj.java
+++ reprap/miscellaneous/AoI/plugins/CSGEvaluator/src/org/reprap/artofillusion/objects/InsetObj.java
@@ -45,64 +45,6 @@ public class InsetObj extends MetaCADObject
     }
     return result;
   }
-  
-  Vec3[] createRoll(int n, double big, double small, double small2) {
-    Vec3[] v;
-    v = new Vec3[n];
-    int index = 0;
-    for (int i = 0; i < n; i++)
-    {
-      double biga=(2*Math.PI*i)/n;
-      double len = big*biga;
-      double smalla=len/small;
-      double x,y;
-      
-      x = (big+small)*Math.cos(biga) + (small2)*Math.cos(smalla);
-      y = (big+small)*Math.sin(biga) + (small2)*Math.sin(smalla);
-      
-      v[index] = new Vec3(x, y, 0 );
-      
-      index++;
-    }
-    return v;
-  }
-
-  Vec3[] createRegular(int n, double radiusx, double radiusy) {
-    Vec3[] v;
-    v = new Vec3[n];
-    int index = 0;
-    for (int i = 0; i < n; i++)
-    {
-      v[index] = new Vec3( radiusx*Math.cos( 2*Math.PI * index / (double) n ),
-          radiusy*Math.sin(2*Math.PI * index / (double) n ), 0 );
-      index++;
-    }
-    return v;
-  }
-
-  Vec3[] createStar(int n, double inner, double outer) {
-    Vec3[] v;
-    v = new Vec3[2*n];
-    int index = 0;
-    for (int i = 0; i < n; i++) {
-      v[index] = new Vec3(Math.cos(Math.PI * index / (double) n),
-                          Math.sin(Math.PI * index / (double) n), 0);
-      v[index].scale(inner);
-      v[index+1] = new Vec3(Math.cos(Math.PI * (index + 1) / (double) n),
-                            Math.sin(Math.PI * (index + 1) / (double) n), 0);
-      v[index+1].scale(outer);
-      index += 2;
-    }
-    return v;
-  }
-
-  Object3D createPolygon(Vec3[] v) {
-    Object3D obj3D;
-    float smoothness[] = new float[v.length];
-    Arrays.fill(smoothness, 0.0f);
-    obj3D = new Curve(v, smoothness, Mesh.NO_SMOOTHING, true).convertToTriangleMesh(0);
-    return obj3D;
-  }
 
 ///////////////////////////////////////////
 //math helpers
