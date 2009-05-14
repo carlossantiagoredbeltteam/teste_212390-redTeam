4f13fcc7955a8e45efe3d42c07cce2c07b94195d
kintel <kintel@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2009-05-14 05:03:43 +0000

    added rotate

---
+++ reprap/miscellaneous/AoI/plugins/MetaCAD/src/org/reprap/artofillusion/metacad/objects/RotateObj.java
@@ -0,0 +1,36 @@
+package org.reprap.artofillusion.metacad.objects;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.reprap.artofillusion.metacad.MetaCADContext;
+import org.reprap.artofillusion.metacad.ParsedTree;
+
+import artofillusion.math.CoordinateSystem;
+import artofillusion.object.ObjectInfo;
+
+public class RotateObj extends MetaCADObject {
+
+  public List<ObjectInfo> evaluateObject(MetaCADContext ctx, 
+                                         List<String> parameters, 
+                                         List<ParsedTree> children) throws Exception {
+
+    assert(children.size() == 1);
+    List<ObjectInfo> chlist = ParsedTree.evaluate(ctx, children);
+    
+    CoordinateSystem coordsys = new CoordinateSystem();
+    if (parameters.size() >= 3) {
+      coordsys.setOrientation(ctx.evaluateExpression(parameters.get(3)),
+                              ctx.evaluateExpression(parameters.get(4)),
+                              ctx.evaluateExpression(parameters.get(5)));
+    }
+    
+    Iterator<ObjectInfo> iter = chlist.iterator();
+    while (iter.hasNext()) {
+      ObjectInfo info = iter.next();
+      info.coords.transformCoordinates(coordsys.fromLocal());
+    }
+    return chlist;
+  }
+
+}
