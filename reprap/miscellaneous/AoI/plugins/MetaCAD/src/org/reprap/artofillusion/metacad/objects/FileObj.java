c5da6ccbeea8590cfdc03087305c03817f4366dd
kintel <kintel@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2009-06-02 07:18:52 +0000

    STL support in =file() function

--- reprap/miscellaneous/AoI/plugins/MetaCAD/src/org/reprap/artofillusion/metacad/objects/FileObj.java
+++ reprap/miscellaneous/AoI/plugins/MetaCAD/src/org/reprap/artofillusion/metacad/objects/FileObj.java
@@ -1,13 +1,19 @@
 package org.reprap.artofillusion.metacad.objects;
 
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.reprap.artofillusion.metacad.MetaCADContext;
 import org.reprap.artofillusion.metacad.ParsedTree;
 import org.reprap.artofillusion.metacad.translators.DXFImporter;
 
+import artofillusion.PluginRegistry;
+import artofillusion.Scene;
 import artofillusion.object.ObjectInfo;
+import artofillusion.object.TriangleMesh;
+import artofillusion.ui.MessageDialog;
 
 public class FileObj extends MetaCADObject
 {
@@ -20,18 +26,35 @@ public class FileObj extends MetaCADObject
       filename = parameters.get(0);
       if (filename.charAt(0) == '"') filename = filename.substring(1, filename.length()-1);
     }
-    
+    // Append filename to directory of the current AoI scene if it's not an absolute path
+    if (!new File(filename).exists()) {
+      filename = ctx.scene.getDirectory() + File.separator + filename;
+    }
+
     List<ObjectInfo> objects = null;
-    
-    // lets see if the file exists
-    boolean absExists = (new File(filename)).exists();
-    if (absExists) {
+
+    if (filename.toLowerCase().endsWith(".dxf")) {
       objects = DXFImporter.importFile(ctx.scene, filename, false);
-    } 
-    // otherwise lets try to append the path of the aoi scene
-    else {
-      String dir = ctx.scene.getDirectory();
-      objects = DXFImporter.importFile(ctx.scene, dir + File.separator + filename, false);
+    }
+    else if (filename.toLowerCase().endsWith(".stl")) {
+      try {
+        Scene scene = (Scene)PluginRegistry.invokeExportedMethod("nik777.STLTranslator.import", new File(filename));
+        // Only import triangle meshes (filters away cameras, lights)
+        objects = new LinkedList<ObjectInfo>();
+        for (ObjectInfo stlinfo : scene.getAllObjects()) {
+          if (stlinfo.object instanceof TriangleMesh) {
+            objects.add(stlinfo);
+          }
+        }
+      }
+      catch (NoSuchMethodException nsme) {
+        nsme.printStackTrace();
+        new MessageDialog(null, "Unable to access STL Translator.\nVerify that the STL Translator plugin is installed.");
+      }
+      catch (InvocationTargetException ite) {
+        ite.printStackTrace();
+        new MessageDialog(null, "Unable to access STL Translator.\nVerify that the STL Translator plugin is installed.");
+      }
     }
   
     return objects;
