c61c37483367232eaa5096fb0328a2d943f5ac55
adrian-bowyer <adrian-bowyer@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2009-07-27 21:07:47 +0000

    Code added optionally to build a nozzle-wipe shield alongside the part(s) being laid down.

--- trunk/reprap/host/src/org/reprap/geometry/LayerProducer.java
+++ trunk/reprap/host/src/org/reprap/geometry/LayerProducer.java
@@ -299,15 +299,17 @@ public class LayerProducer {
 		if(borderPolygons != null)
 		{
 			borderPolygons.middleStarts(hatchedPolygons, layerConditions);
-
-//			RrRectangle rr = lc.getBox();
-//			Rr2Point corner = Rr2Point.sub(rr.sw(), new Rr2Point(-3, -3));
-//			RrPolygon ell = new RrPolygon(borderPolygons.polygon(0).getAttributes(), false);
-//			ell.add(corner);
-//			ell.add(Rr2Point.add(corner, new Rr2Point(-2, 10)));
-//			ell.add(Rr2Point.add(corner, new Rr2Point(-2, -2)));
-//			ell.add(Rr2Point.add(corner, new Rr2Point(20, -2)));
-//			borderPolygons.add(0, ell);
+			if(Preferences.loadGlobalBool("Shield"))
+			{
+				RrRectangle rr = lc.getBox();
+				Rr2Point corner = Rr2Point.add(rr.sw(), new Rr2Point(-3, -3));
+				RrPolygon ell = new RrPolygon(borderPolygons.polygon(0).getAttributes(), false);
+				ell.add(corner);
+				ell.add(Rr2Point.add(corner, new Rr2Point(-2, 10)));
+				ell.add(Rr2Point.add(corner, new Rr2Point(-2, -2)));
+				ell.add(Rr2Point.add(corner, new Rr2Point(20, -2)));
+				borderPolygons.add(0, ell);
+			}
 		}
 		
 		if(simulationPlot != null)
