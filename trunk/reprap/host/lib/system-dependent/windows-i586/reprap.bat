467a8f3211f3f1e7fd4ed20f493327817508f3f9
adrian-bowyer <adrian-bowyer@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2010-03-08 16:56:28 +0000

    Tidying release files.

--- trunk/reprap/host/lib/system-dependent/windows-i586/reprap.bat
+++ trunk/reprap/host/lib/system-dependent/windows-i586/reprap.bat
@@ -1,7 +1,7 @@
 rem reprap-host -- runs Reprap Java host code with an appropriate classpath
 
 rem Amount of RAM to allow Java VM to use
-set RAM_SIZE=384M
+set REPRAP_RAM_SIZE=1024M
 
 rem reprap.jar file and stl file
 set REPRAP_DIR=%ProgramFiles%\Reprap
@@ -12,6 +12,6 @@ rem set JAVA_LIBRARY_DIR=%ProgramFiles%\Reprap
 
 rem cd so we can find the reprap-wv.stl file.  Can we avoid this??
 IF NOT EXIST reprap-wv.stl cd "%REPRAP_DIR%"
-java -cp ".\reprap.jar;.\RXTXcomm.jar;.\j3dcore.jar;.\j3d-org-java3d-all.jar;.\j3dutils.jar;.\swing-layout-1.0.3.jar;.\vecmath.jar;." -Xmx%RAM_SIZE% org/reprap/Main
+java -cp ".\reprap.jar;.\RXTXcomm.jar;.\j3dcore.jar;.\j3d-org-java3d-all.jar;.\j3dutils.jar;.\swing-layout-1.0.3.jar;.\vecmath.jar;." -Xmx%REPRAP_RAM_SIZE% org/reprap/Main
 if ERRORLEVEL 1 pause
 
